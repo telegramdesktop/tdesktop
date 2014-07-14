708267b8cac0b3375df2204775979410713670a7
John Preston <johnprestonmail@gmail.com>
2014-07-14 17:59:11 +0400

    prepare shell script for mac works from Telegram dir now

--- Telegram/Prepare.sh
+++ Telegram/Prepare.sh
@@ -1,32 +1,53 @@
-AppVersionStr=0.5.5
-AppVersion=5005
+AppVersionStr=0.5.7
+AppVersion=5007
 
-if [ -d "deploy/$AppVersionStr" ]; then
+if [ -d "./../Mac/Release/deploy/$AppVersionStr" ]; then
   echo "Deploy folder for version $AppVersionStr already exists!"
   exit 1
 fi
 
-if [ -f "tupdate$AppVersion" ]; then
+if [ -f "./../Mac/Release/tupdate$AppVersion" ]; then
   echo "Update file for version $AppVersion already exists!"
   exit 1
 fi
 
-if [ ! -d "Telegram.app" ]; then
+if [ ! -d "./../Mac/Release/Telegram.app" ]; then
   echo "Telegram.app not found!"
   exit 1
 fi
+
+if [ ! -f "./../Mac/Release/Telegram.app/Contents/Resources/Icon.icns" ]; then
+  echo "Icon.icns not found in Resources!"
+  exit 1
+fi
+
+if [ ! -f "./../Mac/Release/Telegram.app/Contents/MacOS/Telegram" ]; then
+  echo "Telegram not found in MacOS!"
+  exit 1
+fi
+
+if [ ! -f "./../Mac/Release/Telegram.app/Contents/Frameworks/Updater" ]; then
+  echo "Icon.icns not found in Resources!"
+  exit 1
+fi
+
+if [ ! -f "./../Mac/Release/Telegram.dmg" ]; then
+  echo "Telegram.dmg not found!"
+  exit 1
+fi
+
 echo "Preparing version $AppVersionStr, executing Packer.."
-./Packer.app/Contents/MacOS/Packer -path Telegram.app -version $AppVersion
+cd ./../Mac/Release && ./Packer.app/Contents/MacOS/Packer -path Telegram.app -version $AppVersion && cd ./../../Telegram
 echo "Packer done!"
 
-if [ ! -d "deploy/" ]; then
-  mkdir "deploy"
+if [ ! -d "./../Mac/Release/deploy/" ]; then
+  mkdir "./../Mac/Release/deploy"
 fi
 echo "Copying Telegram.app and tmacupd$AppVersion to deploy/$AppVersionStr..";
-mkdir "deploy/$AppVersionStr"
-mkdir "deploy/$AppVersionStr/Telegram"
-mv Telegram.app deploy/$AppVersionStr/Telegram/
-mv tmacupd$AppVersion deploy/$AppVersionStr/
-mv Telegram.dmg deploy/$AppVersionStr/
+mkdir "./../Mac/Release/deploy/$AppVersionStr"
+mkdir "./../Mac/Release/deploy/$AppVersionStr/Telegram"
+mv ./../Mac/Release/Telegram.app ./../Mac/Release/deploy/$AppVersionStr/Telegram/
+mv ./../Mac/Release/tmacupd$AppVersion ./../Mac/Release/deploy/$AppVersionStr/
+mv ./../Mac/Release/Telegram.dmg ./../Mac/Release/deploy/$AppVersionStr/tsetup.$AppVersionStr.dmg
 echo "Version $AppVersionStr prepared!";
 
