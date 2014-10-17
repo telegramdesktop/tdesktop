6377f59e23c3b9dcde036550e6ecf0702950af95
John Preston <johnprestonmail@gmail.com>
2014-10-17 17:27:05 +0400

    added linux deploy script to git

---
+++ Telegram/DeployLinux.sh
@@ -0,0 +1,16 @@
+AppVersionStr=0.6.3
+AppVersion=6003
+
+if [ ! -f "./../Linux/Release/deploy/$AppVersionStr/tlinuxupd$AppVersion" ]; then
+    echo "tlinuxupd$AppVersion not found!";
+    exit 1
+fi
+
+if [ ! -f "./../Linux/Release/deploy/$AppVersionStr/tsetup.$AppVersionStr.tar.xz" ]; then
+    echo "tsetup.$AppVersionStr.tar.xz not found!"
+    exit 1
+fi
+
+scp ./../Linux/Release/deploy/$AppVersionStr/tlinuxupd$AppVersion tupdates:tdesktop/static/tlinux/
+scp ./../Linux/Release/deploy/$AppVersionStr/tsetup.$AppVersionStr.tar.xz tupdates:tdesktop/static/tlinux/
+
