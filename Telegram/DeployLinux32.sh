87d795807da8c7c8485596fbac9ef0f3b79c8741
John Preston <johnprestonmail@gmail.com>
2014-10-17 17:27:48 +0400

    added linux deploy script to git

---
+++ Telegram/DeployLinux32.sh
@@ -0,0 +1,16 @@
+AppVersionStr=0.6.3
+AppVersion=6003
+
+if [ ! -f "./../Linux/Release/deploy/$AppVersionStr/tlinux32upd$AppVersion" ]; then
+    echo "tlinux32upd$AppVersion not found!"
+    exit 1
+fi
+
+if [ ! -f "./../Linux/Release/deploy/$AppVersionStr/tsetup32.$AppVersionStr.tar.xz" ]; then
+    echo "tsetup32.$AppVersionStr.zip not found!"
+    exit 1
+fi
+
+scp ./../Linux/Release/deploy/$AppVersionStr/tlinux32upd$AppVersion tupdates:tdesktop/static/tlinux32/
+scp ./../Linux/Release/deploy/$AppVersionStr/tsetup32.$AppVersionStr.tar.xz tupdates:tdesktop/static/tlinux32/
+
