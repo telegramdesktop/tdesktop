2940c922a7a18b51eec73a242775d29977d5bfcd
John Preston <johnprestonmail@gmail.com>
2014-11-28 14:49:13 +0300

    started mac store prepare

--- Telegram/PrepareMac.sh
+++ Telegram/PrepareMac.sh
@@ -10,58 +10,41 @@ if [ -d "./../Mac/Release/deploy/$AppVersionStr" ]; then
   exit 1
 fi
 
-if [ -f "./../Mac/Release/tupdate$AppVersion" ]; then
-  echo "Update file for version $AppVersion already exists!"
+if [ ! -d "./../Mac/Release/Telegram Desktop.app" ]; then
+  echo "Telegram Desktop.app not found!"
   exit 1
 fi
 
-if [ ! -d "./../Mac/Release/Telegram.app" ]; then
-  echo "Telegram.app not found!"
-  exit 1
-fi
-
-if [ ! -f "./../Mac/Release/Telegram.app/Contents/Resources/Icon.icns" ]; then
+if [ ! -f "./../Mac/Release/Telegram Desktop.app/Contents/Resources/Icon.icns" ]; then
   echo "Icon.icns not found in Resources!"
   exit 1
 fi
 
-if [ ! -f "./../Mac/Release/Telegram.app/Contents/MacOS/Telegram" ]; then
+if [ ! -f "./../Mac/Release/Telegram Desktop.app/Contents/MacOS/Telegram" ]; then
   echo "Telegram not found in MacOS!"
   exit 1
 fi
 
-if [ ! -f "./../Mac/Release/Telegram.app/Contents/Frameworks/Updater" ]; then
+if [ ! -f "./../Mac/Release/Telegram Desktop.app/Contents/Frameworks/Updater" ]; then
   echo "Updater not found in Frameworks!"
   exit 1
 fi
 
-cd ./../Mac/Release && codesign --force --deep --sign "Developer ID Application: John Preston" Telegram.app && cd ./../../Telegram
+cd ./../Mac/Release && codesign --force --deep --sign "Mac Developer: Peter Iakovlev (88P695A564)" "Telegram Desktop.app" && cd ./../../Telegram
 
-if [ ! -d "./../Mac/Release/Telegram.app/Contents/_CodeSignature" ]; then
+if [ ! -d "./../Mac/Release/Telegram Desktop.app/Contents/_CodeSignature" ]; then
   echo "Telegram signature not found!"
   exit 1
 fi
-
-cd ./../Mac/Release
-temppath=`hdiutil attach -readwrite tsetup.dmg | awk -F "\t" 'END {print $3}'`
-cp -R ./Telegram.app "$temppath/"
-bless --folder "$temppath/" --openfolder "$temppath/"
-hdiutil detach "$temppath"
-hdiutil convert tsetup.dmg -format UDZO -imagekey zlib-level=9 -ov -o tsetup.$AppVersionStr.dmg
-cd ./../../Telegram
-cd ./../Mac/Release && ./Packer.app/Contents/MacOS/Packer -path Telegram.app -version $AppVersion && cd ./../../Telegram
 
 if [ ! -d "./../Mac/Release/deploy/" ]; then
   mkdir "./../Mac/Release/deploy"
 fi
 
-echo "Copying Telegram.app and tmacupd$AppVersion to deploy/$AppVersionStr..";
+echo "Copying Telegram Desktop.app to deploy/$AppVersionStr..";
 mkdir "./../Mac/Release/deploy/$AppVersionStr"
-mkdir "./../Mac/Release/deploy/$AppVersionStr/Telegram"
-cp -r ./../Mac/Release/Telegram.app ./../Mac/Release/deploy/$AppVersionStr/Telegram/
+cp -r ./../Mac/Release/Telegram.app ./../Mac/Release/deploy/$AppVersionStr/
 rm ./../Mac/Release/Telegram.app/Contents/MacOS/Telegram
 rm ./../Mac/Release/Telegram.app/Contents/Frameworks/Updater
-mv ./../Mac/Release/tmacupd$AppVersion ./../Mac/Release/deploy/$AppVersionStr/
-mv ./../Mac/Release/tsetup.$AppVersionStr.dmg ./../Mac/Release/deploy/$AppVersionStr/tsetup.$AppVersionStr.dmg
 echo "Version $AppVersionStr prepared!";
 
