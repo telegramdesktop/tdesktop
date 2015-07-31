f5259b58e5b8b7a4252b48ac4ba41d5619bfdc6b
John Preston <johnprestonmail@gmail.com>
2015-07-31 13:32:33 +0300

    fixed bug in dev -> stable update, version 0.8.42.dev

--- Telegram/SourceFiles/config.h
+++ Telegram/SourceFiles/config.h
@@ -17,9 +17,9 @@ Copyright (c) 2014 John Preston, https://desktop.telegram.org
 */
 #pragma once
 
-static const int32 AppVersion = 8043;
-static const wchar_t *AppVersionStr = L"0.8.43";
-static const bool DevVersion = false;
+static const int32 AppVersion = 8042;
+static const wchar_t *AppVersionStr = L"0.8.42";
+static const bool DevVersion = true;
 
 static const wchar_t *AppNameOld = L"Telegram Win (Unofficial)";
 static const wchar_t *AppName = L"Telegram Desktop";
