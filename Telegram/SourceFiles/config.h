a0dc865bdcc611c7ec3419f2ad516a43ce87511b
John Preston <johnprestonmail@gmail.com>
2015-08-02 21:07:46 +0300

    version 0.8.45 stable

--- Telegram/SourceFiles/config.h
+++ Telegram/SourceFiles/config.h
@@ -17,9 +17,9 @@ Copyright (c) 2014 John Preston, https://desktop.telegram.org
 */
 #pragma once
 
-static const int32 AppVersion = 8044;
-static const wchar_t *AppVersionStr = L"0.8.44";
-static const bool DevVersion = true;
+static const int32 AppVersion = 8045;
+static const wchar_t *AppVersionStr = L"0.8.45";
+static const bool DevVersion = false;
 
 static const wchar_t *AppNameOld = L"Telegram Win (Unofficial)";
 static const wchar_t *AppName = L"Telegram Desktop";
