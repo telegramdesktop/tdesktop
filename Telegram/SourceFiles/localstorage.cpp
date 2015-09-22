7a74c5a3114f1bdfba618d6acb8b10778a9df7e2
John Preston <johnprestonmail@gmail.com>
2015-09-22 13:26:39 +0300

    new channel blank photos, 0.8.58.dev version

--- Telegram/SourceFiles/localstorage.cpp
+++ Telegram/SourceFiles/localstorage.cpp
@@ -2946,7 +2946,7 @@ namespace Local {
 			channel->input = MTP_inputPeerChannel(MTP_int(peerToChannel(channel->id)), MTP_long(access));
 			channel->inputChannel = MTP_inputChannel(MTP_int(peerToChannel(channel->id)), MTP_long(access));
 
-			channel->photo = photoLoc.isNull() ? ImagePtr(chatDefPhoto(channel->colorIndex)) : ImagePtr(photoLoc);
+			channel->photo = photoLoc.isNull() ? ImagePtr(channelDefPhoto(channel->colorIndex)) : ImagePtr(photoLoc);
 		}
 		App::markPeerUpdated(result);
 		emit App::main()->peerPhotoChanged(result);
