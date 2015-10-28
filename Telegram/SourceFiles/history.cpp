63dbf7e9ef9b4653bcd1b2c1f0d9fd32fa5e79cd
John Preston <johnprestonmail@gmail.com>
2015-10-27 23:47:55 -0400

    improved emoji strings in sticker description

--- Telegram/SourceFiles/history.cpp
+++ Telegram/SourceFiles/history.cpp
@@ -4380,6 +4380,10 @@ HistorySticker::HistorySticker(DocumentData *document) : HistoryMedia()
 	data->thumb->load();
 	if (!data->sticker()->alt.isEmpty()) {
 		_emoji = data->sticker()->alt;
+		int32 elen = 0;
+		if (EmojiPtr e = emojiFromText(_emoji.constData(), _emoji.constEnd(), elen)) {
+			_emoji = emojiString(e);
+		}
 	}
 }
 
