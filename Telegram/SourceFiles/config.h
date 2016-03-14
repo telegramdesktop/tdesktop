3e37be5d5c3f512a5fe2f027cfa925acd5883dc6
John Preston <johnprestonmail@gmail.com>
2016-03-14 13:34:32 +0300

    version 0.9.31 stable

--- Telegram/SourceFiles/config.h
+++ Telegram/SourceFiles/config.h
@@ -20,10 +20,10 @@ Copyright (c) 2014-2016 John Preston, https://desktop.telegram.org
 */
 #pragma once
 
-static const int32 AppVersion = 9030;
-static const wchar_t *AppVersionStr = L"0.9.30";
+static const int32 AppVersion = 9031;
+static const wchar_t *AppVersionStr = L"0.9.31";
 static const bool DevVersion = false;
-#define BETA_VERSION (9030002ULL) // just comment this line to build public version
+//#define BETA_VERSION (9030002ULL) // just comment this line to build public version
 
 static const wchar_t *AppNameOld = L"Telegram Win (Unofficial)";
 static const wchar_t *AppName = L"Telegram Desktop";
