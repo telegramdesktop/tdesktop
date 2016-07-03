c40ffd14e3762be5a2909c7b9eefc81b877f5d24
Christoph <auer.chrisi@gmx.net>
2016-07-03 16:06:27 +0200

    Test

--- .travis/check.sh
+++ .travis/check.sh
@@ -13,11 +13,16 @@ checkCommitMessage() {
 		if [[  $TRAVIS_COMMIT_MSG != *"Signed-off-by: "* ]];then
 			error_msg "The commit message does not contain the signature!"
 			error_msg "More information: https://github.com/telegramdesktop/tdesktop/blob/master/.github/CONTRIBUTING.md#sign-your-work"
+			addCommentToGitHubPR
 			exit 1
 		else
 			success_msg "Commit message contains signature"
 		fi
 	fi
+}
+
+addCommentToGitHubPR() {
+    curl -H "Authorization: token ${GH_AUTH_TOKEN}" --data "{\"body\": \"TEST\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 }
 
 source ./.travis/common.sh
