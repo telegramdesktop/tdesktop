1f64f21dcb33b3bed31bd9aed9563d1080bb1d34
Christoph <auer.chrisi@gmx.net>
2016-07-03 16:33:52 +0200

    Test

--- .travis/check.sh
+++ .travis/check.sh
@@ -13,7 +13,7 @@ checkCommitMessage() {
 		if [[  $TRAVIS_COMMIT_MSG != *"Signed-off-by: "* ]];then
 			error_msg "The commit message does not contain the signature!"
 			error_msg "More information: https://github.com/telegramdesktop/tdesktop/blob/master/.github/CONTRIBUTING.md#sign-your-work"
-			addCommentToGitHubPR
+			addMissingSignatureComment
 			exit 1
 		else
 			success_msg "Commit message contains signature"
@@ -21,11 +21,23 @@ checkCommitMessage() {
 	fi
 }
 
-addCommentToGitHubPR() {
+addMissingSignatureComment() {
 	if [[ $BUILD_VERSION == "" ]]; then
-		info_msg "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
-		curl -H "Authorization: token ${GH_AUTH_TOKEN}" --data "{\"body\": \"TEST\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+		local TEXT="Hi,\n\
+thanks for the pull request!\n\
+\n\
+Please read our [contributing policy](https://github.com/telegramdesktop/tdesktop/blob/master/.github/CONTRIBUTING.md). You'll need to make a pull request with the \"Signed-off-by:\" signature being the last line of your commit message, like it is described in [sign your work](https://github.com/telegramdesktop/tdesktop/blob/master/.github/CONTRIBUTING.md#sign-your-work) section. That will grant your work into the public domain.\n\
+\n\
+(See [travis build](https://travis-ci.org/telegramdesktop/tdesktop/jobs/${TRAVIS_JOB_ID}))"
+		addCommentToGitHubPR "${TEXT}"
 	fi
+	
+	
+}
+
+addCommentToGitHubPR() {
+	local BODY=$1
+	curl -H "Authorization: token ${GH_AUTH_TOKEN}" --data "{\"body\": \"${BODY}\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 }
 
 source ./.travis/common.sh
