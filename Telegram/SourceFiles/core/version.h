c8b89620f5557038a49609f5453ce99e4956b85a
John Preston <johnprestonmail@gmail.com>
2016-07-27 21:22:15 +0300

    Private beta version 9061001

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -22,7 +22,7 @@ Copyright (c) 2014-2016 John Preston, https://desktop.telegram.org
 
 #include "core/basic_types.h"
 
-#define BETA_VERSION_MACRO (0ULL)
+#define BETA_VERSION_MACRO (9061001ULL)
 
 constexpr int AppVersion = 9061;
 constexpr str_const AppVersionStr = "0.9.61";
