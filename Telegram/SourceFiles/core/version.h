75c12dda31032fa8bf45f9e65d0217edba09d618
John Preston <johnprestonmail@gmail.com>
2016-09-20 16:29:34 +0300

    Version 0.10.7: fix of settings saving, fix of OpenSSL in Linux build.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -24,7 +24,7 @@ Copyright (c) 2014-2016 John Preston, https://desktop.telegram.org
 
 #define BETA_VERSION_MACRO (0ULL)
 
-constexpr int AppVersion = 10006;
-constexpr str_const AppVersionStr = "0.10.6";
+constexpr int AppVersion = 10007;
+constexpr str_const AppVersionStr = "0.10.7";
 constexpr bool AppAlphaVersion = false;
 constexpr uint64 AppBetaVersion = BETA_VERSION_MACRO;
