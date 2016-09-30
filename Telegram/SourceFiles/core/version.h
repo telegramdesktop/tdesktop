3d2f025e10dea7ae82860b4fff4064344827c30c
John Preston <johnprestonmail@gmail.com>
2016-09-30 21:18:00 +0300

    Version 0.10.9: games.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -22,9 +22,9 @@ Copyright (c) 2014-2016 John Preston, https://desktop.telegram.org
 
 #include "core/utils.h"
 
-#define BETA_VERSION_MACRO (10008005ULL)
+#define BETA_VERSION_MACRO (0ULL)
 
-constexpr int AppVersion = 10008;
-constexpr str_const AppVersionStr = "0.10.8";
+constexpr int AppVersion = 10009;
+constexpr str_const AppVersionStr = "0.10.9";
 constexpr bool AppAlphaVersion = false;
 constexpr uint64 AppBetaVersion = BETA_VERSION_MACRO;
