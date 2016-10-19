9752224449cadcfc5948b7748f4707100e5e47dd
John Preston <johnprestonmail@gmail.com>
2016-10-19 21:47:45 +0300

    Version 0.10.16 stable: new media player + notifications update.

--- Telegram/SourceFiles/application.cpp
+++ Telegram/SourceFiles/application.cpp
@@ -1087,8 +1087,8 @@ void AppClass::checkMapVersion() {
 		if (Local::oldMapVersion()) {
 			QString versionFeatures;
 			if ((cAlphaVersion() || cBetaVersion()) && Local::oldMapVersion() < 10014) {
-				versionFeatures = QString::fromUtf8("\xe2\x80\x94 New audio player design\n\xe2\x80\x94 Moved to Qt library version 5.6.2\n\nWindows 10:\n\xe2\x80\x94 Respecting quiet hours for the notifications");
-			} else if (!(cAlphaVersion() || cBetaVersion()) && Local::oldMapVersion() < 10005) {
+				versionFeatures = langNewVersionText(); //QString::fromUtf8("\xe2\x80\x94 New audio player design\n\xe2\x80\x94 Moved to Qt library version 5.6.2\n\nWindows 10:\n\xe2\x80\x94 Respecting quiet hours for the notifications");
+			} else if (!(cAlphaVersion() || cBetaVersion()) && Local::oldMapVersion() < 10016) {
 				versionFeatures = langNewVersionText();
 			} else {
 				versionFeatures = lang(lng_new_version_minor).trimmed();
