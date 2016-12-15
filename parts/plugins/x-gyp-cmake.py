94243b7838f376e175c90df15c914e3ff9c35ca4
Marco Trevisan (Treviño) <mail@3v1n0.net>
2016-12-15 20:31:44 +0100

    gyp-cmake: inherit from CMakePlugin reusing artifacts + organize

--- parts/plugins/x-gyp-cmake.py
+++ parts/plugins/x-gyp-cmake.py
@@ -1,22 +1,15 @@
 import os
 import snapcraft
 
+from snapcraft.plugins import cmake
 
-class GypCMakePlugin(snapcraft.BasePlugin):
+
+class GypCMakePlugin(cmake.CMakePlugin):
     """A basic plugin for snapcraft that generates CMake files from gyp"""
 
     @classmethod
     def schema(cls):
         schema = super().schema()
-        schema['properties']['configflags'] = {
-            'type': 'array',
-            'minitems': 1,
-            'uniqueItems': True,
-            'items': {
-                'type': 'string',
-            },
-            'default': [],
-        }
 
         schema['properties']['gyp-file'] = {
             'type': 'string'
@@ -28,64 +21,44 @@ class GypCMakePlugin(snapcraft.BasePlugin):
             'enum': ['Debug', 'Release'],
         }
 
-        schema['properties']['built-binaries'] = {
-            'type': 'array',
-            'minitems': 0,
-            'uniqueItems': True,
-            'items': {
-                'type': 'string',
-            },
-            'default': [],
-        }
-
         schema['required'].append('gyp-file')
 
-        return schema
-
-    @classmethod
-    def get_build_properties(cls):
-        return [
+        schema['build-properties'].extend([
             'build-type',
-            'configflags',
             'gyp-file',
-        ]
+        ])
+
+        return schema
 
     def __init__(self, name, options, project):
         super().__init__(name, options, project)
         self.build_packages.extend([
             'binutils',
-            'cmake',
             'python',
         ])
+        self.builddir = os.path.join(
+            self.build_basedir, 'out', self.options.build_type)
 
     def build(self):
-        super().build()
-
+        env = self._build_environment()
         gyp_path = os.path.join(self.sourcedir, os.path.dirname(self.options.gyp_file))
-        build_dir = os.path.join(self.build_basedir, 'out', self.options.build_type)
 
-        if not os.path.exists(os.path.join(build_dir, 'CMakeLists.txt')):
+        if not os.path.exists(os.path.join(self.builddir, 'CMakeLists.txt')):
             gyp_command = ['gyp'] + self.options.configflags + ['--format=cmake']
             gyp_command.append('--generator-output={}'.format(self.build_basedir))
             gyp_command.append(os.path.basename(self.options.gyp_file))
-
             self.run(gyp_command, cwd=gyp_path)
 
-        if not os.path.exists(os.path.join(build_dir, 'Makefile')):
-            self.run(['cmake', '.'], cwd=build_dir)
+        if not os.path.exists(os.path.join(self.builddir, 'Makefile')):
+            self.run(['cmake', '.'], env=env)
 
-        self.run(['make', '-j{}'.format(self.parallel_build_count)], cwd=build_dir)
+        self.make(env=env)
 
-        if len(self.options.built_binaries):
-            for bin in self.options.built_binaries:
-                dest = os.path.join(self.installdir, 'bin', os.path.basename(bin))
-                snapcraft.file_utils.link_or_copy(
-                    os.path.join(build_dir, bin), dest, follow_symlinks=True)
-
-                if self.options.build_type == 'Release':
-                    mime_type = self.run_output('file --mime-type -b {}'.format(dest).split())
+        if self.options.artifacts and self.options.build_type == 'Release':
+            for artifact in self.options.artifacts:
+                dest = os.path.join(self.installdir, artifact)
+                if os.path.isfile(dest):
+                    mime_type = self.run_output(
+                        'file --mime-type -b {}'.format(dest).split())
                     if 'application/x-executable' in mime_type:
                         self.run(['strip', dest])
-        else:
-            self.run(['make', 'install', 'DESTDIR={}'.format(
-                self.installdir)], cwd=build_dir)
