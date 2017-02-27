87445ccf8c2cf8d4b3b020ffb084329a6962f273
Christoph <auchri@users.noreply.github.com>
2017-02-27 09:42:11 +0100

    Add interpreter path
    Fix #3064

--- Telegram/Resources/langs/download.sh
+++ Telegram/Resources/langs/download.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 set -e
 FullExecPath=$PWD
 pushd `dirname $0` > /dev/null
@@ -26,7 +27,8 @@ done < tdesktop/Telegram/Resources/langs/list
 cd translations/telegram-desktop.langstrings/
 for file in *.strings; do
   iconv -f "UTF-16LE" -t "UTF-8" "$file" > "../../tdesktop/Telegram/Resources/langs/lang_$file.tmp"
-  awk '{ if (NR==1) sub(/^\xef\xbb\xbf/,""); sub(//,""); print }' "../../tdesktop/Telegram/Resources/langs/lang_$file.tmp" > "../../tdesktop/Telegram/Resources/langs/lang_$file"
+  awk '{ if (NR==1) sub(/^\xef\xbb\xbf/,""); sub(/
+/,""); print }' "../../tdesktop/Telegram/Resources/langs/lang_$file.tmp" > "../../tdesktop/Telegram/Resources/langs/lang_$file"
   rm "../../tdesktop/Telegram/Resources/langs/lang_$file.tmp"
 done
 touch $FullScriptPath/../telegram.qrc
