195d3d13d733ee1041aa63d0e86ee827af647168
John Preston <johnprestonmail@gmail.com>
2017-03-01 21:37:15 +0300

    Fix regression from 87445ccf8c in a helper script.

--- Telegram/Resources/langs/download.sh
+++ Telegram/Resources/langs/download.sh
@@ -27,8 +27,7 @@ done < tdesktop/Telegram/Resources/langs/list
 cd translations/telegram-desktop.langstrings/
 for file in *.strings; do
   iconv -f "UTF-16LE" -t "UTF-8" "$file" > "../../tdesktop/Telegram/Resources/langs/lang_$file.tmp"
-  awk '{ if (NR==1) sub(/^\xef\xbb\xbf/,""); sub(/
-/,""); print }' "../../tdesktop/Telegram/Resources/langs/lang_$file.tmp" > "../../tdesktop/Telegram/Resources/langs/lang_$file"
+  awk '{ if (NR==1) sub(/^\xef\xbb\xbf/,""); sub(//,""); print }' "../../tdesktop/Telegram/Resources/langs/lang_$file.tmp" > "../../tdesktop/Telegram/Resources/langs/lang_$file"
   rm "../../tdesktop/Telegram/Resources/langs/lang_$file.tmp"
 done
 touch $FullScriptPath/../telegram.qrc
