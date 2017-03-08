6d216042e6f99be2f2601b71ca7f32934fea1882
John Preston <johnprestonmail@gmail.com>
2017-03-08 13:33:16 +0300

    Alpha 1.0.19: jump to date, macOS do not disturb.
    Production testing of the AuthSession downloader() and notifications().

--- Telegram/SourceFiles/messenger.cpp
+++ Telegram/SourceFiles/messenger.cpp
@@ -637,8 +637,8 @@ void Messenger::checkMapVersion() {
 	if (Local::oldMapVersion() < AppVersion) {
 		if (Local::oldMapVersion()) {
 			QString versionFeatures;
-			if ((cAlphaVersion() || cBetaVersion()) && Local::oldMapVersion() < 1000015) {
-				versionFeatures = QString::fromUtf8("\xe2\x80\x94 Wrong supergroup members display fix.\n\xe2\x80\x94 RTL text layout fix.\n\xe2\x80\x94 Linux GTK file choose dialog should show image previews.\n\xe2\x80\x94 Bug fixes and other minor improvements.");
+			if ((cAlphaVersion() || cBetaVersion()) && Local::oldMapVersion() < 1000019) {
+				versionFeatures = QString::fromUtf8("\xE2\x80\x94 Go to date. Click on the date in a chat to jump to a specific day.\n\xE2\x80\x94 Respect macOS 'Do not disturb' settings for sound notifications.\n\xE2\x80\x94 Bug fixes and other minor improvements.");
 			} else if (!(cAlphaVersion() || cBetaVersion()) && Local::oldMapVersion() < 1000012) {
 				versionFeatures = langNewVersionText();
 			} else {
