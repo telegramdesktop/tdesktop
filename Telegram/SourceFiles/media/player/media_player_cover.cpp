telegramdesktop/tdesktop

commit 87ff770020a4cdb68bbe947985744288c6d66113

Make Media::Clip::Playback independent of slider.
Now animation of the playback progress is processed inside the
Media::Clip::Playback and the sliders just hold plain float64 value.

Telegram/SourceFiles/media/player/media_player_cover.cpp
@@ -72,7 +72,8 @@ CoverWidget::CoverWidget(QWidget *parent) : TWidget(parent)
 , _nameLabel(this, st::mediaPlayerName)
 , _timeLabel(this, st::mediaPlayerTime)
 , _close(this, st::mediaPlayerPanelClose)
-, _playback(std::make_unique<Clip::Playback>(new Ui::MediaSlider(this, st::mediaPlayerPanelPlayback)))
+, _playbackSlider(this, st::mediaPlayerPanelPlayback)
+, _playback(std::make_unique<Clip::Playback>())
 , _playPause(this)
 , _volumeToggle(this, st::mediaPlayerVolumeToggle)
 , _volumeController(this)
@@ -86,11 +87,19 @@ CoverWidget::CoverWidget(QWidget *parent) : TWidget(parent)
 	_timeLabel->setAttribute(Qt::WA_TransparentForMouseEvents);
 	setMouseTracking(true);
 
-	_playback->setChangeProgressCallback([this](float64 value) {
-		handleSeekProgress(value);
+	_playback->setInLoadingStateChangedCallback([this](bool loading) {
+		_playbackSlider->setDisabled(loading);
 	});
-	_playback->setChangeFinishedCallback([this](float64 value) {
+	_playback->setValueChangedCallback([this](float64 value) {
+		_playbackSlider->setValue(value);
+	});
+	_playbackSlider->setChangeProgressCallback([this](float64 value) {
+		handleSeekProgress(value);
+		_playback->setValue(value, false);
+	});
+	_playbackSlider->setChangeFinishedCallback([this](float64 value) {
 		handleSeekFinished(value);
+		_playback->setValue(value, false);
 	});
 	_playPause->setClickedCallback([this] {
 		instance()->playPauseCancelClicked();
@@ -168,7 +177,7 @@ void CoverWidget::resizeEvent(QResizeEvent *e) {
 
 	int skip = (st::mediaPlayerPanelPlayback.seekSize.width() / 2);
 	int length = (width() - 2 * st::mediaPlayerPanelPadding + st::mediaPlayerPanelPlayback.seekSize.width());
-	_playback->setGeometry(st::mediaPlayerPanelPadding - skip, st::mediaPlayerPanelPlaybackTop, length, 2 * st::mediaPlayerPanelPlaybackPadding + st::mediaPlayerPanelPlayback.width);
+	_playbackSlider->setGeometry(st::mediaPlayerPanelPadding - skip, st::mediaPlayerPanelPlaybackTop, length, 2 * st::mediaPlayerPanelPlaybackPadding + st::mediaPlayerPanelPlayback.width);
 
 	auto top = st::mediaPlayerPanelVolumeToggleTop;
 	auto right = st::mediaPlayerPanelPlayLeft;
@@ -269,11 +278,11 @@ void CoverWidget::updateTimeText(const TrackState &state) {
 
 	if (state.id.audio()->loading()) {
 		_time = QString::number(qRound(state.id.audio()->progress() * 100)) + '%';
-		_playback->setDisabled(true);
+		_playbackSlider->setDisabled(true);
 	} else {
 		display = display / frequency;
 		_time = formatDurationText(display);
-		_playback->setDisabled(false);
+		_playbackSlider->setDisabled(false);
 	}
 	if (_seekPositionMs < 0) {
 		updateTimeLabel();

Telegram/SourceFiles/media/player/media_player_widget.cpp
@@ -91,7 +91,8 @@ Widget::Widget(QWidget *parent) : TWidget(parent)
 , _repeatTrack(this, st::mediaPlayerRepeatButton)
 , _close(this, st::mediaPlayerClose)
 , _shadow(this, st::shadowFg)
-, _playback(std::make_unique<Clip::Playback>(new Ui::FilledSlider(this, st::mediaPlayerPlayback))) {
+, _playbackSlider(this, st::mediaPlayerPlayback)
+, _playback(std::make_unique<Clip::Playback>()) {
 	setAttribute(Qt::WA_OpaquePaintEvent);
 	setMouseTracking(true);
 	resize(width(), st::mediaPlayerHeight + st::lineWidth);
@@ -99,11 +100,19 @@ Widget::Widget(QWidget *parent) : TWidget(parent)
 	_nameLabel->setAttribute(Qt::WA_TransparentForMouseEvents);
 	_timeLabel->setAttribute(Qt::WA_TransparentForMouseEvents);
 
-	_playback->setChangeProgressCallback([this](float64 value) {
-		handleSeekProgress(value);
+	_playback->setInLoadingStateChangedCallback([this](bool loading) {
+		_playbackSlider->setDisabled(loading);
 	});
-	_playback->setChangeFinishedCallback([this](float64 value) {
+	_playback->setValueChangedCallback([this](float64 value) {
+		_playbackSlider->setValue(value);
+	});
+	_playbackSlider->setChangeProgressCallback([this](float64 value) {
+		handleSeekProgress(value);
+		_playback->setValue(value, false);
+	});
+	_playbackSlider->setChangeFinishedCallback([this](float64 value) {
 		handleSeekFinished(value);
+		_playback->setValue(value, false);
 	});
 	_playPause->setClickedCallback([this] {
 		instance()->playPauseCancelClicked();
@@ -167,12 +176,12 @@ void Widget::setShadowGeometryToLeft(int x, int y, int w, int h) {
 
 void Widget::showShadow() {
 	_shadow->show();
-	_playback->show();
+	_playbackSlider->show();
 }
 
 void Widget::hideShadow() {
 	_shadow->hide();
-	_playback->hide();
+	_playbackSlider->hide();
 }
 
 QPoint Widget::getPositionForVolumeWidget() const {
@@ -223,7 +232,7 @@ void Widget::resizeEvent(QResizeEvent *e) {
 
 	updatePlayPrevNextPositions();
 
-	_playback->setGeometry(0, height() - st::mediaPlayerPlayback.fullWidth, width(), st::mediaPlayerPlayback.fullWidth);
+	_playbackSlider->setGeometry(0, height() - st::mediaPlayerPlayback.fullWidth, width(), st::mediaPlayerPlayback.fullWidth);
 }
 
 void Widget::paintEvent(QPaintEvent *e) {
@@ -346,11 +355,11 @@ void Widget::updateTimeText(const TrackState &state) {
 
 	if (state.id.audio()->loading()) {
 		_time = QString::number(qRound(state.id.audio()->progress() * 100)) + '%';
-		_playback->setDisabled(true);
+		_playbackSlider->setDisabled(true);
 	} else {
 		display = display / frequency;
 		_time = formatDurationText(display);
-		_playback->setDisabled(false);
+		_playbackSlider->setDisabled(false);
 	}
 	if (_seekPositionMs < 0) {
 		updateTimeLabel();

Telegram/SourceFiles/media/view/media_clip_controller.cpp
@@ -34,7 +34,8 @@ namespace Clip {
 
 Controller::Controller(QWidget *parent) : TWidget(parent)
 , _playPauseResume(this, st::mediaviewPlayButton)
-, _playback(std::make_unique<Playback>(new Ui::MediaSlider(this, st::mediaviewPlayback)))
+, _playbackSlider(this, st::mediaviewPlayback)
+, _playback(std::make_unique<Playback>())
 , _volumeController(this)
 , _fullScreenToggle(this, st::mediaviewFullScreenButton)
 , _playedAlready(this, st::mediaviewPlayProgressLabel)
@@ -50,11 +51,19 @@ Controller::Controller(QWidget *parent) : TWidget(parent)
 	connect(_fullScreenToggle, SIGNAL(clicked()), this, SIGNAL(toFullScreenPressed()));
 	connect(_volumeController, SIGNAL(volumeChanged(float64)), this, SIGNAL(volumeChanged(float64)));
 
-	_playback->setChangeProgressCallback([this](float64 value) {
-		handleSeekProgress(value);
+	_playback->setInLoadingStateChangedCallback([this](bool loading) {
+		_playbackSlider->setDisabled(loading);
 	});
-	_playback->setChangeFinishedCallback([this](float64 value) {
+	_playback->setValueChangedCallback([this](float64 value) {
+		_playbackSlider->setValue(value);
+	});
+	_playbackSlider->setChangeProgressCallback([this](float64 value) {
+		handleSeekProgress(value);
+		_playback->setValue(value, false);
+	});
+	_playbackSlider->setChangeFinishedCallback([this](float64 value) {
 		handleSeekFinished(value);
+		_playback->setValue(value, false);
 	});
 }
 
@@ -93,7 +102,7 @@ void Controller::hideAnimated() {
 template <typename Callback>
 void Controller::startFading(Callback start) {
 	start();
-	_playback->show();
+	_playbackSlider->show();
 }
 
 void Controller::fadeFinished() {
@@ -101,7 +110,7 @@ void Controller::fadeFinished() {
 }
 
 void Controller::fadeUpdated(float64 opacity) {
-	_playback->setFadeOpacity(opacity);
+	_playbackSlider->setFadeOpacity(opacity);
 }
 
 void Controller::updatePlayback(const Player::TrackState &state) {
@@ -178,12 +187,12 @@ void Controller::setInFullScreen(bool inFullScreen) {
 
 void Controller::grabStart() {
 	showChildren();
-	_playback->hide();
+	_playbackSlider->hide();
 }
 
 void Controller::grabFinish() {
 	hideChildren();
-	_playback->show();
+	_playbackSlider->show();
 }
 
 void Controller::resizeEvent(QResizeEvent *e) {
@@ -195,9 +204,9 @@ void Controller::resizeEvent(QResizeEvent *e) {
 
 	_volumeController->moveToRight(st::mediaviewFullScreenLeft + _fullScreenToggle->width() + st::mediaviewVolumeLeft, (height() - _volumeController->height()) / 2);
 
-	int playbackWidth = width() - st::mediaviewPlayPauseLeft - _playPauseResume->width() - playTop - fullScreenTop - _volumeController->width() - st::mediaviewVolumeLeft - _fullScreenToggle->width() - st::mediaviewFullScreenLeft;
-	_playback->resize(playbackWidth, st::mediaviewPlayback.seekSize.height());
-	_playback->moveToLeft(st::mediaviewPlayPauseLeft + _playPauseResume->width() + playTop, st::mediaviewPlaybackTop);
+	auto playbackWidth = width() - st::mediaviewPlayPauseLeft - _playPauseResume->width() - playTop - fullScreenTop - _volumeController->width() - st::mediaviewVolumeLeft - _fullScreenToggle->width() - st::mediaviewFullScreenLeft;
+	_playbackSlider->resize(playbackWidth, st::mediaviewPlayback.seekSize.height());
+	_playbackSlider->moveToLeft(st::mediaviewPlayPauseLeft + _playPauseResume->width() + playTop, st::mediaviewPlaybackTop);
 
 	_playedAlready->moveToLeft(st::mediaviewPlayPauseLeft + _playPauseResume->width() + playTop, st::mediaviewPlayProgressTop);
 	_toPlayLeft->moveToRight(width() - (st::mediaviewPlayPauseLeft + _playPauseResume->width() + playTop) - playbackWidth, st::mediaviewPlayProgressTop);

Telegram/SourceFiles/media/player/media_player_cover.h
@@ -26,6 +26,7 @@ namespace Ui {
 class FlatLabel;
 class LabelSimple;
 class IconButton;
+class MediaSlider;
 } // namespace Ui
 
 namespace Media {
@@ -80,6 +81,7 @@ private:
 	object_ptr<Ui::FlatLabel> _nameLabel;
 	object_ptr<Ui::LabelSimple> _timeLabel;
 	object_ptr<Ui::IconButton> _close;
+	object_ptr<Ui::MediaSlider> _playbackSlider;
 	std::unique_ptr<Clip::Playback> _playback;
 	object_ptr<Ui::IconButton> _previousTrack = { nullptr };
 	object_ptr<PlayButton> _playPause;

Telegram/SourceFiles/media/player/media_player_volume_controller.cpp
@@ -45,12 +45,10 @@ VolumeController::VolumeController(QWidget *parent) : TWidget(parent)
 	});
 	subscribe(Global::RefSongVolumeChanged(), [this] {
 		if (!_slider->isChanging()) {
-			_slider->setValue(Global::SongVolume(), true);
+			_slider->setValue(Global::SongVolume());
 		}
 	});
-
-	auto animated = false;
-	setVolume(Global::SongVolume(), animated);
+	setVolume(Global::SongVolume());
 
 	resize(st::mediaPlayerPanelVolumeWidth, 2 * st::mediaPlayerPanelPlaybackPadding + st::mediaPlayerPanelPlayback.width);
 }
@@ -65,8 +63,8 @@ void VolumeController::resizeEvent(QResizeEvent *e) {
 	_slider->setGeometry(rect());
 }
 
-void VolumeController::setVolume(float64 volume, bool animated) {
-	_slider->setValue(volume, animated);
+void VolumeController::setVolume(float64 volume) {
+	_slider->setValue(volume);
 	if (volume > 0) {
 		Global::SetRememberedSongVolume(volume);
 	}

Telegram/SourceFiles/media/player/media_player_volume_controller.h
@@ -38,7 +38,7 @@ protected:
 	void resizeEvent(QResizeEvent *e) override;
 
 private:
-	void setVolume(float64 volume, bool animated = true);
+	void setVolume(float64 volume);
 	void applyVolumeChange(float64 volume);
 
 	object_ptr<Ui::MediaSlider> _slider;

Telegram/SourceFiles/media/player/media_player_widget.h
@@ -27,6 +27,7 @@ class FlatLabel;
 class LabelSimple;
 class IconButton;
 class PlainShadow;
+class FilledSlider;
 } // namespace Ui
 
 namespace Media {
@@ -101,6 +102,7 @@ private:
 	object_ptr<Ui::IconButton> _repeatTrack;
 	object_ptr<Ui::IconButton> _close;
 	object_ptr<Ui::PlainShadow> _shadow = { nullptr };
+	object_ptr<Ui::FilledSlider> _playbackSlider;
 	std::unique_ptr<Clip::Playback> _playback;
 
 };

Telegram/SourceFiles/media/view/media_clip_controller.h
@@ -24,6 +24,7 @@ namespace Ui {
 class LabelSimple;
 class FadeAnimation;
 class IconButton;
+class MediaSlider;
 } // namespace Ui
 
 namespace Media {
@@ -86,6 +87,7 @@ private:
 	TimeMs _lastDurationMs = 0;
 
 	object_ptr<Ui::IconButton> _playPauseResume;
+	object_ptr<Ui::MediaSlider> _playbackSlider;
 	std::unique_ptr<Playback> _playback;
 	object_ptr<VolumeController> _volumeController;
 	object_ptr<Ui::IconButton> _fullScreenToggle;

commit 66662e02a65ba60632085af0a6c64834334d39ed

Fix crash in video player seeking.
Regression was introduced in 87ff770020.

Seek handler may destroy controller so first the playback is updated.

Telegram/SourceFiles/media/player/media_player_cover.cpp
@@ -94,12 +94,12 @@ CoverWidget::CoverWidget(QWidget *parent) : TWidget(parent)
 		_playbackSlider->setValue(value);
 	});
 	_playbackSlider->setChangeProgressCallback([this](float64 value) {
-		handleSeekProgress(value);
 		_playback->setValue(value, false);
+		handleSeekProgress(value);
 	});
 	_playbackSlider->setChangeFinishedCallback([this](float64 value) {
-		handleSeekFinished(value);
 		_playback->setValue(value, false);
+		handleSeekFinished(value);
 	});
 	_playPause->setClickedCallback([this] {
 		instance()->playPauseCancelClicked(AudioMsgId::Type::Song);

Telegram/SourceFiles/media/player/media_player_widget.cpp
@@ -111,15 +111,15 @@ Widget::Widget(QWidget *parent) : TWidget(parent)
 		if (_type != AudioMsgId::Type::Song) {
 			return; // Round video seek is not supported for now :(
 		}
-		handleSeekProgress(value);
 		_playback->setValue(value, false);
+		handleSeekProgress(value);
 	});
 	_playbackSlider->setChangeFinishedCallback([this](float64 value) {
 		if (_type != AudioMsgId::Type::Song) {
 			return; // Round video seek is not supported for now :(
 		}
-		handleSeekFinished(value);
 		_playback->setValue(value, false);
+		handleSeekFinished(value);
 	});
 	_playPause->setClickedCallback([this] {
 		instance()->playPauseCancelClicked(_type);

Telegram/SourceFiles/media/view/media_clip_controller.cpp
@@ -58,12 +58,12 @@ Controller::Controller(QWidget *parent) : TWidget(parent)
 		_playbackSlider->setValue(value);
 	});
 	_playbackSlider->setChangeProgressCallback([this](float64 value) {
-		handleSeekProgress(value);
 		_playback->setValue(value, false);
+		handleSeekProgress(value); // This may destroy Controller.
 	});
 	_playbackSlider->setChangeFinishedCallback([this](float64 value) {
-		handleSeekFinished(value);
 		_playback->setValue(value, false);
+		handleSeekFinished(value);
 	});
 }
 