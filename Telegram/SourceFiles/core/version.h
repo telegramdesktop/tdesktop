341b44119f16f5d98487d821935d33cdb49045b7
John Preston <johnprestonmail@gmail.com>
2017-06-30 15:08:29 +0300

    Version 1.1.9.
    - Supergroups can now have up to 10.000 members.
    - Appoint supergroup admins with granular rights.
    Choose who can add users, manage messages, block members,
    edit group info & username, add new admins, etc.
    - Restrict and ban supergroup members with granular precision.
    Read-only bans, GIF & sticker bans, media bans,
    temporary bans and restrictions.
    - Check the new event log to see all service actions taken by
    members and admins of a channel or supergroup in the last 48 hours.
    - Toggle night mode in the main menu.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -24,7 +24,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 
 #define BETA_VERSION_MACRO (0ULL)
 
-constexpr int AppVersion = 1001008;
-constexpr str_const AppVersionStr = "1.1.8";
-constexpr bool AppAlphaVersion = true;
+constexpr int AppVersion = 1001009;
+constexpr str_const AppVersionStr = "1.1.9";
+constexpr bool AppAlphaVersion = false;
 constexpr uint64 AppBetaVersion = BETA_VERSION_MACRO;
