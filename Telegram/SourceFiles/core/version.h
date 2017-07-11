76cafc5059196dc085a73aa5ccf201f1d4f6278c
John Preston <johnprestonmail@gmail.com>
2017-07-11 20:28:37 +0300

    Alpha 1.1.12.
    - Click on forwarded messages bar to change the recipient chat
    in case you chose a wrong one first.
    - Quickly share posts from channels and media messages from bots.
    - Search in supergroup members by name.
    - Search in channel members by name if you're a channel admin.
    - Copy links to messages in public supergroups.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -24,7 +24,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 
 #define BETA_VERSION_MACRO (0ULL)
 
-constexpr int AppVersion = 1001011;
-constexpr str_const AppVersionStr = "1.1.11";
+constexpr int AppVersion = 1001012;
+constexpr str_const AppVersionStr = "1.1.12";
 constexpr bool AppAlphaVersion = true;
 constexpr uint64 AppBetaVersion = BETA_VERSION_MACRO;
