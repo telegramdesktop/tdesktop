a90a18920bbf631ec4dffa51ad075999f5ea89d7
John Preston <johnprestonmail@gmail.com>
2017-08-01 21:45:04 +0300

    Version 1.1.19.
    - Search by messages of specific group members.
    - Bug fixes and other minor improvements.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -24,7 +24,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 
 #define BETA_VERSION_MACRO (0ULL)
 
-constexpr int AppVersion = 1001018;
-constexpr str_const AppVersionStr = "1.1.18";
+constexpr int AppVersion = 1001019;
+constexpr str_const AppVersionStr = "1.1.19";
 constexpr bool AppAlphaVersion = false;
 constexpr uint64 AppBetaVersion = BETA_VERSION_MACRO;
