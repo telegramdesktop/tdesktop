710f9d049ff4ee00655938c1987fd37f3789bb5e
John Preston <johnprestonmail@gmail.com>
2017-09-04 15:34:40 +0300

    Version 1.1.22.
    - Bug fixes and other minor improvements.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -24,7 +24,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 
 #define BETA_VERSION_MACRO (0ULL)
 
-constexpr int AppVersion = 1001021;
-constexpr str_const AppVersionStr = "1.1.21";
+constexpr int AppVersion = 1001022;
+constexpr str_const AppVersionStr = "1.1.22";
 constexpr bool AppAlphaVersion = false;
 constexpr uint64 AppBetaVersion = BETA_VERSION_MACRO;
