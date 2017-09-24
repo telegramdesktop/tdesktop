982d7ff96122a7c01382ba2f8b52e7ed6c6147e0
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2017-09-25 01:35:32 +0200

    AppVeyor is derping

--- .appveyor/install.bat
+++ .appveyor/install.bat
@@ -5,6 +5,8 @@ SET LIB_DIR=%BUILD_DIR%\Libraries
 SET SRC_DIR=%BUILD_DIR%\tdesktop
 SET QT_VERSION=5_6_2
 
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" x86
+
 call:configureBuild
 call:getDependencies
 call:setupGYP
