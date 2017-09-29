7ccd67db6e276c4fb6cd1157e9c286fd5576b6cb
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2017-09-29 17:57:29 +0200

    don't call vcvarsall.bat in AppVeyor script
    Follow-up for #3920
    
    Signed-off-by: Duncan Ogilvie mr.exodia.tpodt@gmail.com (github: mrexodia)

--- .appveyor/install.bat
+++ .appveyor/install.bat
@@ -5,8 +5,6 @@ SET LIB_DIR=%BUILD_DIR%\Libraries
 SET SRC_DIR=%BUILD_DIR%\tdesktop
 SET QT_VERSION=5_6_2
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" x86
-
 call:configureBuild
 call:getDependencies
 call:setupGYP
