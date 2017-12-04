1a273702d30f0297521d50fc1f9673d334105107
John Preston <johnprestonmail@gmail.com>
2017-12-04 13:52:22 +0400

    Revert closed beta version.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -22,9 +22,9 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 
 #include "core/utils.h"
 
-#define BETA_VERSION_MACRO (1001026001ULL)
+#define BETA_VERSION_MACRO (0ULL)
 
 constexpr int AppVersion = 1001026;
 constexpr str_const AppVersionStr = "1.1.26";
-constexpr bool AppAlphaVersion = false;
+constexpr bool AppAlphaVersion = true;
 constexpr uint64 AppBetaVersion = BETA_VERSION_MACRO;
