defec611e31c5e8e73182b2284e7a08b7f1e47c5
John Preston <johnprestonmail@gmail.com>
2017-12-17 12:40:53 +0400

    Alpha version 1.2.3.
    - Several crash fixes.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -24,7 +24,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 
 #define BETA_VERSION_MACRO (0ULL)
 
-constexpr int AppVersion = 1002002;
-constexpr str_const AppVersionStr = "1.2.2";
+constexpr int AppVersion = 1002003;
+constexpr str_const AppVersionStr = "1.2.3";
 constexpr bool AppAlphaVersion = true;
 constexpr uint64 AppBetaVersion = BETA_VERSION_MACRO;
