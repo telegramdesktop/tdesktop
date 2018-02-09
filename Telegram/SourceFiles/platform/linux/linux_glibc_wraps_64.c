9f4060bb00da9836a3e1d33e43da679baaefa11c
Marco Trevisan (Treviño) <mail@3v1n0.net>
2018-02-09 03:11:58 +0100

    linux-glibc-wraps: define versions for clock_gettime in ARM

--- Telegram/SourceFiles/platform/linux/linux_glibc_wraps_64.c
+++ Telegram/SourceFiles/platform/linux/linux_glibc_wraps_64.c
@@ -7,8 +7,17 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #include <time.h>
 
+#if defined(_M_X64) || defined(__x86_64__)
+#define GETTIME_GLIBC_VERSION "2.2.5"
+#elif defined(__aarch64__)
+#define GETTIME_GLIBC_VERSION "2.17"
+#else
+#error Please add glibc wraps for your architecture
+#endif
+
 int __clock_gettime_glibc_old(clockid_t clk_id, struct timespec *tp);
-__asm__(".symver __clock_gettime_glibc_old,clock_gettime@GLIBC_2.2.5");
+__asm__(".symver __clock_gettime_glibc_old,clock_gettime@GLIBC_" GETTIME_GLIBC_VERSION);
+
 
 int __wrap_clock_gettime(clockid_t clk_id, struct timespec *tp) {
         return __clock_gettime_glibc_old(clk_id, tp);
