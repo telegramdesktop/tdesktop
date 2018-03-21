9aa2831fef527156aafe3ad80794207253c231f4
John Preston <johnprestonmail@gmail.com>
2018-03-21 18:05:45 +0400

    Version 1.2.14.
    - Discover new stickers.
    Type one emoji to see suggestions from popular sticker sets.
    Suggestions from your installed sticker sets will come first.
    - Search for Stickers. Click on the new search icon
    to access your sticker sets or find new ones.
    - Quick Reply. Double click near a message for a quick reply.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -11,7 +11,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 #define BETA_VERSION_MACRO (0ULL)
 
-constexpr int AppVersion = 1002013;
-constexpr str_const AppVersionStr = "1.2.13";
-constexpr bool AppAlphaVersion = true;
+constexpr int AppVersion = 1002014;
+constexpr str_const AppVersionStr = "1.2.14";
+constexpr bool AppAlphaVersion = false;
 constexpr uint64 AppBetaVersion = BETA_VERSION_MACRO;
