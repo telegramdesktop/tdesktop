6fa86a7dcc2651c28a5db77b219b13f0e2a915fe
Marco Trevisan (Treviño) <mail@3v1n0.net>
2018-04-05 17:15:20 -0500

    plugins: add copyright informations

--- snap/plugins/x-patched-python.py
+++ snap/plugins/x-patched-python.py
@@ -1,3 +1,20 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Author: Marco Trevisan <marco@ubuntu.com>
+# Copyright (C) 2017-2018 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import snapcraft
 import requests
