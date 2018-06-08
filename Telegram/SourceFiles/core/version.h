5c4b81434e2d40ac114e1d259cf23cad51b7f077
John Preston <johnprestonmail@gmail.com>
2018-06-08 23:12:00 +0300

    Alpha version 1.3.5.
    - Bug fixes and other minor improvements.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -11,7 +11,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 #define BETA_VERSION_MACRO (0ULL)
 
-constexpr int AppVersion = 1003004;
-constexpr str_const AppVersionStr = "1.3.4";
+constexpr int AppVersion = 1003005;
+constexpr str_const AppVersionStr = "1.3.5";
 constexpr bool AppAlphaVersion = true;
 constexpr uint64 AppBetaVersion = BETA_VERSION_MACRO;
