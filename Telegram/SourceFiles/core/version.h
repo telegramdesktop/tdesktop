e60311811b1657fed296ae0eca116a6e2f42672f
John Preston <johnprestonmail@gmail.com>
2018-06-11 22:28:08 +0300

    Version 1.3.7.
    - Push fixes to stable version.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -11,7 +11,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 #define BETA_VERSION_MACRO (0ULL)
 
-constexpr int AppVersion = 1003006;
-constexpr str_const AppVersionStr = "1.3.6";
-constexpr bool AppAlphaVersion = true;
+constexpr int AppVersion = 1003007;
+constexpr str_const AppVersionStr = "1.3.7";
+constexpr bool AppAlphaVersion = false;
 constexpr uint64 AppBetaVersion = BETA_VERSION_MACRO;
