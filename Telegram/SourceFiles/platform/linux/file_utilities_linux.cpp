telegramdesktop/tdesktop

commit 67ea175fc6b8a98571acb32bb0b2cf842b301e2a

Show choose file box over the passport panel.

Telegram/SourceFiles/platform/linux/file_utilities_linux.cpp
@@ -139,8 +139,14 @@ bool PreviewSupported() {
 		&& (Libs::gdk_pixbuf_new_from_file_at_size != nullptr);
 }
 
-bool GetNative(QStringList &files, QByteArray &remoteContent, const QString &caption, const QString &filter, Type type, QString startFile) {
-	auto parent = Messenger::Instance().getFileDialogParent();
+bool GetNative(
+		QPointer<QWidget> parent,
+		QStringList &files,
+		QByteArray &remoteContent,
+		const QString &caption,
+		const QString &filter,
+		Type type,
+		QString startFile) {
 	internal::GtkFileDialog dialog(parent, caption, QString(), filter);
 
 	dialog.setModal(true);
@@ -185,13 +191,34 @@ bool GetNative(QStringList &files, QByteArray &remoteContent, const QString &cap
 
 } // namespace
 
-bool Get(QStringList &files, QByteArray &remoteContent, const QString &caption, const QString &filter, Type type, QString startFile) {
+bool Get(
+		QPointer<QWidget> parent,
+		QStringList &files,
+		QByteArray &remoteContent,
+		const QString &caption,
+		const QString &filter,
+		Type type,
+		QString startFile) {
 #ifndef TDESKTOP_DISABLE_GTK_INTEGRATION
 	if (NativeSupported()) {
-		return GetNative(files, remoteContent, caption, filter, type, startFile);
+		return GetNative(
+			parent,
+			files,
+			remoteContent,
+			caption,
+			filter,
+			type,
+			startFile);
 	}
 #endif // !TDESKTOP_DISABLE_GTK_INTEGRATION
-	return ::FileDialog::internal::GetDefault(files, remoteContent, caption, filter, type, startFile);
+	return ::FileDialog::internal::GetDefault(
+		parent,
+		files,
+		remoteContent,
+		caption,
+		filter,
+		type,
+		startFile);
 }
 
 #ifndef TDESKTOP_DISABLE_GTK_INTEGRATION

Telegram/SourceFiles/boxes/download_path_box.cpp
@@ -83,20 +83,25 @@ void DownloadPathBox::radioChanged(Directory value) {
 }
 
 void DownloadPathBox::onEditPath() {
-	auto initialPath = [] {
+	const auto initialPath = [] {
 		if (!Global::DownloadPath().isEmpty() && Global::DownloadPath() != qstr("tmp")) {
 			return Global::DownloadPath().left(Global::DownloadPath().size() - (Global::DownloadPath().endsWith('/') ? 1 : 0));
 		}
 		return QString();
-	};
-	FileDialog::GetFolder(lang(lng_download_path_choose), initialPath(), base::lambda_guarded(this, [this](const QString &result) {
+	}();
+	const auto handleFolder = [=](const QString &result) {
 		if (!result.isEmpty()) {
 			_path = result + '/';
 			_pathBookmark = psDownloadPathBookmark(_path);
 			setPathText(QDir::toNativeSeparators(_path));
 			_group->setValue(Directory::Custom);
 		}
-	}));
+	};
+	FileDialog::GetFolder(
+		this,
+		lang(lng_download_path_choose),
+		initialPath,
+		base::lambda_guarded(this, handleFolder));
 }
 
 void DownloadPathBox::save() {

Telegram/SourceFiles/core/file_utilities.cpp
@@ -13,15 +13,36 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "messenger.h"
 
 bool filedialogGetSaveFile(
+		QPointer<QWidget> parent,
 		QString &file,
 		const QString &caption,
 		const QString &filter,
 		const QString &initialPath) {
 	QStringList files;
 	QByteArray remoteContent;
-	bool result = Platform::FileDialog::Get(files, remoteContent, caption, filter, FileDialog::internal::Type::WriteFile, initialPath);
+	bool result = Platform::FileDialog::Get(
+		parent,
+		files,
+		remoteContent,
+		caption,
+		filter,
+		FileDialog::internal::Type::WriteFile,
+		initialPath);
 	file = files.isEmpty() ? QString() : files.at(0);
 	return result;
+}
+
+bool filedialogGetSaveFile(
+		QString &file,
+		const QString &caption,
+		const QString &filter,
+		const QString &initialPath) {
+	return filedialogGetSaveFile(
+		Messenger::Instance().getFileDialogParent(),
+		file,
+		caption,
+		filter,
+		initialPath);
 }
 
 QString filedialogDefaultName(
@@ -130,6 +151,7 @@ void UnsafeLaunchDefault(const QString &filepath) {
 namespace FileDialog {
 
 void GetOpenPath(
+		QPointer<QWidget> parent,
 		const QString &caption,
 		const QString &filter,
 		base::lambda<void(OpenResult &&result)> callback,
@@ -138,6 +160,7 @@ void GetOpenPath(
 		auto files = QStringList();
 		auto remoteContent = QByteArray();
 		const auto success = Platform::FileDialog::Get(
+			parent,
 			files,
 			remoteContent,
 			caption,
@@ -161,6 +184,7 @@ void GetOpenPath(
 }
 
 void GetOpenPaths(
+		QPointer<QWidget> parent,
 		const QString &caption,
 		const QString &filter,
 		base::lambda<void(OpenResult &&result)> callback,
@@ -169,6 +193,7 @@ void GetOpenPaths(
 		auto files = QStringList();
 		auto remoteContent = QByteArray();
 		const auto success = Platform::FileDialog::Get(
+			parent,
 			files,
 			remoteContent,
 			caption,
@@ -188,6 +213,7 @@ void GetOpenPaths(
 }
 
 void GetWritePath(
+		QPointer<QWidget> parent,
 		const QString &caption,
 		const QString &filter,
 		const QString &initialPath,
@@ -195,7 +221,7 @@ void GetWritePath(
 		base::lambda<void()> failed) {
 	InvokeQueued(QApplication::instance(), [=] {
 		auto file = QString();
-		if (filedialogGetSaveFile(file, caption, filter, initialPath)) {
+		if (filedialogGetSaveFile(parent, file, caption, filter, initialPath)) {
 			if (callback) {
 				callback(std::move(file));
 			}
@@ -206,6 +232,7 @@ void GetWritePath(
 }
 
 void GetFolder(
+		QPointer<QWidget> parent,
 		const QString &caption,
 		const QString &initialPath,
 		base::lambda<void(QString &&result)> callback,
@@ -214,6 +241,7 @@ void GetFolder(
 		auto files = QStringList();
 		auto remoteContent = QByteArray();
 		const auto success = Platform::FileDialog::Get(
+			parent,
 			files,
 			remoteContent,
 			caption,
@@ -244,7 +272,14 @@ void InitLastPathDefault() {
 	cSetDialogLastPath(QStandardPaths::writableLocation(QStandardPaths::DownloadLocation));
 }
 
-bool GetDefault(QStringList &files, QByteArray &remoteContent, const QString &caption, const QString &filter, FileDialog::internal::Type type, QString startFile = QString()) {
+bool GetDefault(
+		QPointer<QWidget> parent,
+		QStringList &files,
+		QByteArray &remoteContent,
+		const QString &caption,
+		const QString &filter,
+		FileDialog::internal::Type type,
+		QString startFile = QString()) {
 	if (cDialogLastPath().isEmpty()) {
 		Platform::FileDialog::InitLastPath();
 	}

Telegram/SourceFiles/core/file_utilities.h
@@ -54,22 +54,26 @@ struct OpenResult {
 	QByteArray remoteContent;
 };
 void GetOpenPath(
+	QPointer<QWidget> parent,
 	const QString &caption,
 	const QString &filter,
 	base::lambda<void(OpenResult &&result)> callback,
 	base::lambda<void()> failed = base::lambda<void()>());
 void GetOpenPaths(
+	QPointer<QWidget> parent,
 	const QString &caption,
 	const QString &filter,
 	base::lambda<void(OpenResult &&result)> callback,
 	base::lambda<void()> failed = base::lambda<void()>());
 void GetWritePath(
+	QPointer<QWidget> parent,
 	const QString &caption,
 	const QString &filter,
 	const QString &initialPath,
 	base::lambda<void(QString &&result)> callback,
 	base::lambda<void()> failed = base::lambda<void()>());
 void GetFolder(
+	QPointer<QWidget> parent,
 	const QString &caption,
 	const QString &initialPath,
 	base::lambda<void(QString &&result)> callback,
@@ -89,6 +93,7 @@ enum class Type {
 void InitLastPathDefault();
 
 bool GetDefault(
+	QPointer<QWidget> parent,
 	QStringList &files,
 	QByteArray &remoteContent,
 	const QString &caption,

Telegram/SourceFiles/history/admin_log/history_admin_log_inner.cpp
@@ -1085,6 +1085,7 @@ void InnerWidget::savePhotoToFile(PhotoData *photo) {
 
 	auto filter = qsl("JPEG Image (*.jpg);;") + FileDialog::AllFilesFilter();
 	FileDialog::GetWritePath(
+		this,
 		lang(lng_save_photo),
 		filter,
 		filedialogDefaultName(qsl("photo"), qsl(".jpg")),

Telegram/SourceFiles/history/history_inner_widget.cpp
@@ -1738,6 +1738,7 @@ void HistoryInner::savePhotoToFile(not_null<PhotoData*> photo) {
 
 	auto filter = qsl("JPEG Image (*.jpg);;") + FileDialog::AllFilesFilter();
 	FileDialog::GetWritePath(
+		this,
 		lang(lng_save_photo),
 		filter,
 		filedialogDefaultName(

Telegram/SourceFiles/history/history_widget.cpp
@@ -3221,7 +3221,7 @@ void HistoryWidget::chooseAttach() {
 
 	auto filter = FileDialog::AllFilesFilter() + qsl(";;Image files (*") + cImgExtensions().join(qsl(" *")) + qsl(")");
 
-	FileDialog::GetOpenPaths(lang(lng_choose_files), filter, base::lambda_guarded(this, [this](FileDialog::OpenResult &&result) {
+	FileDialog::GetOpenPaths(this, lang(lng_choose_files), filter, base::lambda_guarded(this, [this](FileDialog::OpenResult &&result) {
 		if (result.paths.isEmpty() && result.remoteContent.isEmpty()) {
 			return;
 		}

Telegram/SourceFiles/history/view/history_view_context_menu.cpp
@@ -49,6 +49,7 @@ void SavePhotoToFile(not_null<PhotoData*> photo) {
 	}
 
 	FileDialog::GetWritePath(
+		Messenger::Instance().getFileDialogParent(),
 		lang(lng_save_photo),
 		qsl("JPEG Image (*.jpg);;") + FileDialog::AllFilesFilter(),
 		filedialogDefaultName(qsl("photo"), qsl(".jpg")),

commit b0077d98f0a620ec0755d537ed643e1a2e09dfb6

Version 1.3.7: Fix file dialog parent widget.
Regression was introduced in 67ea175fc6.

Fixes #4817.

Telegram/SourceFiles/platform/linux/file_utilities_linux.cpp
@@ -199,6 +199,9 @@ bool Get(
 		const QString &filter,
 		Type type,
 		QString startFile) {
+	if (parent) {
+		parent = parent->window();
+	}
 #ifndef TDESKTOP_DISABLE_GTK_INTEGRATION
 	if (NativeSupported()) {
 		return GetNative(