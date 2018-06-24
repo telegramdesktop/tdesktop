97d27fe8699656cdc7dda922e2ae162880e6d47e
John Preston <johnprestonmail@gmail.com>
2018-06-24 03:15:25 +0100

    Closed beta 1.3.7.4.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -9,7 +9,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 #include "core/utils.h"
 
-#define BETA_VERSION_MACRO (1003007003ULL)
+#define BETA_VERSION_MACRO (1003007004ULL)
 
 constexpr int AppVersion = 1003007;
 constexpr str_const AppVersionStr = "1.3.7";
