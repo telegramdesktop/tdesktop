a919737f6ef98b56cd7db41577ecfc269a60f444
John Preston <johnprestonmail@gmail.com>
2018-07-13 14:19:25 +0300

    Version 1.3.10.
    - Bug fixes and other minor improvements.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -9,9 +9,9 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 #include "core/utils.h"
 
-#define BETA_VERSION_MACRO (1003009003ULL)
+#define BETA_VERSION_MACRO (0ULL)
 
-constexpr int AppVersion = 1003009;
-constexpr str_const AppVersionStr = "1.3.9";
+constexpr int AppVersion = 1003010;
+constexpr str_const AppVersionStr = "1.3.10";
 constexpr bool AppAlphaVersion = false;
 constexpr uint64 AppBetaVersion = BETA_VERSION_MACRO;
