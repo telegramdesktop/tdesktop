88175aff1f1c63360440498525ad84dee885fbcc
John Preston <johnprestonmail@gmail.com>
2018-09-11 22:15:27 +0300

    Closed beta version 1.3.16.1.
    - New settings design.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -9,9 +9,9 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 #include "core/utils.h"
 
-#define BETA_VERSION_MACRO (0ULL)
+#define BETA_VERSION_MACRO (1003016001ULL)
 
 constexpr int AppVersion = 1003016;
 constexpr str_const AppVersionStr = "1.3.16";
-constexpr bool AppAlphaVersion = true;
+constexpr bool AppAlphaVersion = false;
 constexpr uint64 AppBetaVersion = BETA_VERSION_MACRO;
