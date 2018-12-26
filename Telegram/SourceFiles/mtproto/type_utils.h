be0b0c1984709f1d567baacc98043c39f00557bc
23rd <23rd@vivaldi.net>
2018-12-26 09:18:53 +0300

    Fixed an editing of unsupported messages.

--- Telegram/SourceFiles/mtproto/type_utils.h
+++ Telegram/SourceFiles/mtproto/type_utils.h
@@ -68,8 +68,8 @@ enum class MTPDmessage_ClientFlag : uint32 {
 	// message has an admin badge in supergroup
 	f_has_admin_badge = (1U << 20),
 
-	//// message is not displayed because it is part of a group
-	//f_hidden_by_group = (1U << 19),
+	// message is unsupported by a current version of client
+	f_is_unsupported = (1U << 19),
 
 	// update this when adding new client side flags
 	MIN_FIELD = (1U << 19),
