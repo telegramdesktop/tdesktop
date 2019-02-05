18fa1954ed56ca6a56817801511b169d3fd78a0d
TheWug <3932619+TheWug@users.noreply.github.com>
2019-02-05 02:10:46 -0800

    Enable searching for messages in groups that were sent by users who have left.
    Change SearchFromController to parent from AddSpecialBoxController instead
    of ParticipantsBoxController, and make Role::Member a viable option for
    that controller, so that the search field falls back on global search.

--- Telegram/SourceFiles/boxes/peers/add_participants_box.cpp
+++ Telegram/SourceFiles/boxes/peers/add_participants_box.cpp
@@ -312,6 +312,8 @@ void AddSpecialBoxController::prepare() {
 	delegate()->peerListSetSearchMode(PeerListSearchMode::Enabled);
 	auto title = [&] {
 		switch (_role) {
+		case Role::Members:
+			return tr::lng_profile_participants_section();
 		case Role::Admins:
 			return tr::lng_channel_add_admin();
 		case Role::Restricted:
@@ -443,6 +445,7 @@ void AddSpecialBoxController::loadMoreRows() {
 void AddSpecialBoxController::rowClicked(not_null<PeerListRow*> row) {
 	auto user = row->peer()->asUser();
 	switch (_role) {
+	case Role::Members: return;
 	case Role::Admins: return showAdmin(user);
 	case Role::Restricted: return showRestricted(user);
 	case Role::Kicked: return kickUser(user);
