7d15cca1ee4b3e437c2462596de667afa74e134c
John Preston <johnprestonmail@gmail.com>
2019-02-12 19:02:08 +0300

    Version 1.5.15.
    - Crash fix.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -15,7 +15,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #define TDESKTOP_ALPHA_VERSION (0ULL)
 #endif // TDESKTOP_OFFICIAL_TARGET
 
-constexpr auto AppVersion = 1005014;
-constexpr auto AppVersionStr = "1.5.14";
+constexpr auto AppVersion = 1005015;
+constexpr auto AppVersionStr = "1.5.15";
 constexpr auto AppBetaVersion = false;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
