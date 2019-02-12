telegramdesktop/tdesktop

commit 83bc6fb39cc0172fd8bfdf618cb5626393f048ce

Fix changelog dialogs list appearance.
Fixes #5678, fixes #5674.

Telegram/SourceFiles/history/history.cpp
@@ -2086,8 +2086,13 @@ void History::markFullyLoaded() {
 }
 
 void History::setLastMessage(HistoryItem *item) {
-	if (_lastMessage && *_lastMessage == item) {
-		return;
+	if (_lastMessage) {
+		if (*_lastMessage == item) {
+			return;
+		} else if (!IsServerMsgId((*_lastMessage)->id)
+				&& (*_lastMessage)->date() > item->date()) {
+			return;
+		}
 	}
 	_lastMessage = item;
 	_chatListMessage = std::nullopt;

commit 83c5a67af5a2956bffa59fd15289b95b53c7b883

Fix another crash in last message checking.
Regression was introduced in 83bc6fb39c.

Telegram/SourceFiles/history/history.cpp
@@ -2090,6 +2090,7 @@ void History::setLastMessage(HistoryItem *item) {
 		if (*_lastMessage == item) {
 			return;
 		} else if (*_lastMessage
+			&& item
 			&& !IsServerMsgId((*_lastMessage)->id)
 			&& (*_lastMessage)->date() > item->date()) {
 			return;