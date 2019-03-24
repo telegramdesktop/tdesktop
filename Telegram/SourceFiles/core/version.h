0b5500fe755e04d74984c1a0fe8dd780205f62e6
John Preston <johnprestonmail@gmail.com>
2019-03-24 18:48:26 +0400

    Version 1.6.2.
    - Delete any message on both ends in any private chat, anytime.
    - Control whether your forwarded messages link back to your account.
    - Control who may see your profile picture.
    - Enjoy the new streamlined group management screen.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -7,7 +7,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #pragma once
 
-#define TDESKTOP_REQUESTED_ALPHA_VERSION (1006001001ULL)
+#define TDESKTOP_REQUESTED_ALPHA_VERSION (0ULL)
 
 #ifdef TDESKTOP_OFFICIAL_TARGET
 #define TDESKTOP_ALPHA_VERSION TDESKTOP_REQUESTED_ALPHA_VERSION
@@ -15,7 +15,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #define TDESKTOP_ALPHA_VERSION (0ULL)
 #endif // TDESKTOP_OFFICIAL_TARGET
 
-constexpr auto AppVersion = 1006001;
-constexpr auto AppVersionStr = "1.6.1";
+constexpr auto AppVersion = 1006002;
+constexpr auto AppVersionStr = "1.6.2";
 constexpr auto AppBetaVersion = false;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
