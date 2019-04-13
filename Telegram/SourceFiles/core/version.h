0023fdc7070d0d1bd793ee84232e5ad289901ec0
John Preston <johnprestonmail@gmail.com>
2019-04-13 10:43:57 +0400

    Version 1.6.7.
    - Replace media when editing messages with media content.
    - Jump quickly to the top of your chats list.
    - Get emoji suggestions for the first word you type in a message.
    - Help Telegram improve emoji suggestions in your language
    using this interface https://translations.telegram.org/en/emoji

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -15,7 +15,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #define TDESKTOP_ALPHA_VERSION (0ULL)
 #endif // TDESKTOP_OFFICIAL_TARGET
 
-constexpr auto AppVersion = 1006006;
-constexpr auto AppVersionStr = "1.6.6";
-constexpr auto AppBetaVersion = true;
+constexpr auto AppVersion = 1006007;
+constexpr auto AppVersionStr = "1.6.7";
+constexpr auto AppBetaVersion = false;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
