8af684f7f78f6c317323e1d1f2829e90e3bbd580
John Preston <johnprestonmail@gmail.com>
2019-05-08 23:55:33 +0300

    Version 1.7.
    Introducing Archived Chats:
    
    - Archive any chat from the right-click menu.
    - Chats with enabled notifications will pop out of the archive
    when a notification arrives.
    - Muted chats will stay in the archive.
    - Pin an unlimited number of chats in your archive.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -7,7 +7,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #pragma once
 
-#define TDESKTOP_REQUESTED_ALPHA_VERSION (1006007003ULL)
+#define TDESKTOP_REQUESTED_ALPHA_VERSION (0ULL)
 
 #ifdef TDESKTOP_OFFICIAL_TARGET
 #define TDESKTOP_ALPHA_VERSION TDESKTOP_REQUESTED_ALPHA_VERSION
@@ -15,7 +15,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #define TDESKTOP_ALPHA_VERSION (0ULL)
 #endif // TDESKTOP_OFFICIAL_TARGET
 
-constexpr auto AppVersion = 1006007;
-constexpr auto AppVersionStr = "1.6.7";
+constexpr auto AppVersion = 1007000;
+constexpr auto AppVersionStr = "1.7";
 constexpr auto AppBetaVersion = false;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
