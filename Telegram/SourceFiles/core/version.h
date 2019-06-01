9fce11b6722c282c94908a3e521914586508d58f
John Preston <johnprestonmail@gmail.com>
2019-06-01 11:29:58 +0300

    Version 1.7.3.
    - Bug fixes and other minor improvements.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -15,7 +15,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #define TDESKTOP_ALPHA_VERSION (0ULL)
 #endif // TDESKTOP_OFFICIAL_TARGET
 
-constexpr auto AppVersion = 1007002;
-constexpr auto AppVersionStr = "1.7.2";
+constexpr auto AppVersion = 1007003;
+constexpr auto AppVersionStr = "1.7.3";
 constexpr auto AppBetaVersion = false;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
