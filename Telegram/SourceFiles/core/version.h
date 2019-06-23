978a4ac0ad108ead7a14366a06b22930d12645fd
John Preston <johnprestonmail@gmail.com>
2019-06-23 16:32:06 +0200

    Version 1.7.9.
    - You can now add any users to your contacts,
    even if their phone numbers are not visible.
    - Transfer ownership of group chats and channels
    by granting full rights to another admin.
    Useful when switching jobs or if you just want to retire as creator.
    - Hide archived chats to the main menu.
    - See who is online straight from the chat list.
    - Use the MacBook Pro TouchBar to apply formatting to selected text,
    insert emoji, and send stickers.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -7,7 +7,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #pragma once
 
-#define TDESKTOP_REQUESTED_ALPHA_VERSION (1007008001ULL)
+#define TDESKTOP_REQUESTED_ALPHA_VERSION (0ULL)
 
 #ifdef TDESKTOP_OFFICIAL_TARGET
 #define TDESKTOP_ALPHA_VERSION TDESKTOP_REQUESTED_ALPHA_VERSION
@@ -15,7 +15,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #define TDESKTOP_ALPHA_VERSION (0ULL)
 #endif // TDESKTOP_OFFICIAL_TARGET
 
-constexpr auto AppVersion = 1007008;
-constexpr auto AppVersionStr = "1.7.8";
+constexpr auto AppVersion = 1007009;
+constexpr auto AppVersionStr = "1.7.9";
 constexpr auto AppBetaVersion = false;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
