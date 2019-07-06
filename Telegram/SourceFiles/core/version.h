75fd2d02575f92b8137a273fe9273a777ef02cd2
John Preston <johnprestonmail@gmail.com>
2019-07-06 15:27:31 +0200

    Version 1.7.13.
    - Send ultra-lightweight high-quality animated stickers
    to express emotion with motion.
    - Receive animated stickers instantly on any connection
    at just 20-30 KB per sticker.
    - Enjoy smooth animations at 60 frames per second.
    - Create new animated sets and upload them to @stickers
    for everybody to use.
    - Try out these sample stickers: t.me/addstickers/hotcherry
    - Use strikethrough and underline formatting.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -15,7 +15,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #define TDESKTOP_ALPHA_VERSION (0ULL)
 #endif // TDESKTOP_OFFICIAL_TARGET
 
-constexpr auto AppVersion = 1007012;
-constexpr auto AppVersionStr = "1.7.12";
-constexpr auto AppBetaVersion = true;
+constexpr auto AppVersion = 1007013;
+constexpr auto AppVersionStr = "1.7.13";
+constexpr auto AppBetaVersion = false;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
