telegramdesktop/tdesktop

commit b814c6307aee395cf2b25948b4639d648928af27

Check that sticker drag point is inside.

Telegram/SourceFiles/history/view/media/history_view_media_unwrapped.cpp
@@ -195,6 +195,38 @@ void UnwrappedMedia::drawSurrounding(
 	}
 }
 
+PointState UnwrappedMedia::pointState(QPoint point) const {
+	if (width() < st::msgPadding.left() + st::msgPadding.right() + 1) {
+		return PointState::Outside;
+	}
+
+	const auto rightAligned = _parent->hasOutLayout() && !Adaptive::ChatWide();
+	const auto inWebPage = (_parent->media() != this);
+	const auto item = _parent->data();
+	const auto via = inWebPage ? nullptr : item->Get<HistoryMessageVia>();
+	const auto reply = inWebPage ? nullptr : item->Get<HistoryMessageReply>();
+	auto usex = 0;
+	auto usew = maxWidth();
+	if (!inWebPage) {
+		usew -= additionalWidth(via, reply);
+		if (rightAligned) {
+			usex = width() - usew;
+		}
+	}
+	if (rtl()) {
+		usex = width() - usex - usew;
+	}
+
+	const auto usey = rightAligned ? 0 : (height() - _contentSize.height());
+	const auto useh = rightAligned
+		? std::max(
+			_contentSize.height(),
+			height() - st::msgDateImgPadding.y() * 2 - st::msgDateFont->height)
+		: _contentSize.height();
+	const auto inner = QRect(usex, usey, usew, useh);
+	return inner.contains(point) ? PointState::Inside : PointState::Outside;
+}
+
 TextState UnwrappedMedia::textState(QPoint point, StateRequest request) const {
 	auto result = TextState(_parent);
 	if (width() < st::msgPadding.left() + st::msgPadding.right() + 1) {

Telegram/SourceFiles/history/view/media/history_view_media_unwrapped.h
@@ -49,6 +49,7 @@ public:
 		std::unique_ptr<Content> content);
 
 	void draw(Painter &p, const QRect &r, TextSelection selection, crl::time ms) const override;
+	PointState pointState(QPoint point) const override;
 	TextState textState(QPoint point, StateRequest request) const override;
 
 	bool toggleSelectionByHandlerClick(const ClickHandlerPtr &p) const override {

commit 04617e4a1215561cfa8e32ad3dc409b13a47918c

Fixed display of tooltip for date of stickers.
 - Fixed #6426.
 - Regression was introduced in b814c6307aee395cf2b25948b4639d648928af27.

Telegram/SourceFiles/history/view/media/history_view_media_unwrapped.cpp
@@ -150,7 +150,7 @@ void UnwrappedMedia::drawSurrounding(
 	const auto rightAction = _parent->displayRightAction();
 	const auto fullRight = calculateFullRight(inner);
 	auto fullBottom = height();
-	if (rightAction ? true : needInfoDisplay()) {
+	if (needInfoDisplay()) {
 		_parent->drawInfo(
 			p,
 			fullRight,
@@ -217,13 +217,19 @@ PointState UnwrappedMedia::pointState(QPoint point) const {
 		usex = width() - usex - usew;
 	}
 
+	const auto datey = height() - st::msgDateImgPadding.y() * 2
+		- st::msgDateFont->height;
 	const auto usey = rightAligned ? 0 : (height() - _contentSize.height());
 	const auto useh = rightAligned
-		? std::max(
-			_contentSize.height(),
-			height() - st::msgDateImgPadding.y() * 2 - st::msgDateFont->height)
+		? std::max(_contentSize.height(), datey)
 		: _contentSize.height();
 	const auto inner = QRect(usex, usey, usew, useh);
+
+	// Rectangle of date bubble.
+	if (point.x() < calculateFullRight(inner) && point.y() > datey) {
+		return PointState::Inside;
+	}
+
 	return inner.contains(point) ? PointState::Inside : PointState::Outside;
 }
 
@@ -358,6 +364,7 @@ QPoint UnwrappedMedia::calculateFastActionPosition(
 bool UnwrappedMedia::needInfoDisplay() const {
 	return (_parent->data()->id < 0)
 		|| (_parent->isUnderCursor())
+		|| (_parent->displayRightAction())
 		|| (_parent->hasOutLayout()
 			&& !Adaptive::ChatWide()
 			&& _content->alwaysShowOutTimestamp());