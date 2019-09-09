c4bc76c0229f1b40a0832a5c1ff95997421d6af5
John Preston <johnprestonmail@gmail.com>
2019-09-10 00:52:05 +0300

    Beta version 1.8.7.
    - Bug fixes and other minor improvements.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -15,7 +15,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #define TDESKTOP_ALPHA_VERSION (0ULL)
 #endif // TDESKTOP_OFFICIAL_TARGET
 
-constexpr auto AppVersion = 1008006;
-constexpr auto AppVersionStr = "1.8.6";
+constexpr auto AppVersion = 1008007;
+constexpr auto AppVersionStr = "1.8.7";
 constexpr auto AppBetaVersion = true;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
