f85691b1cbcfb2649a6ba32d3714ed1daadfda46
Slava Bazhenov <slava.bazhenov@ayrlabs.com>
2019-09-11 16:33:18 +0300

    Release 1.8.8.1

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -15,7 +15,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #define TDESKTOP_ALPHA_VERSION (0ULL)
 #endif // TDESKTOP_OFFICIAL_TARGET
 
-constexpr auto AppVersion = 1008008;
-constexpr auto AppVersionStr = "1.8.8";
+constexpr auto AppVersion = 1008008001;
+constexpr auto AppVersionStr = "1.8.8.1";
 constexpr auto AppBetaVersion = false;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
