b7fed0377ccd91d6feb1fecf2c29ba935880e3f0
John Preston <johnprestonmail@gmail.com>
2019-11-08 11:27:39 +0300

    Build codegen utils with cmake.

---
+++ cmake/constants.cmake
@@ -0,0 +1,8 @@
+get_filename_component(libs_loc "../Libraries" REALPATH)
+get_filename_component(third_party_loc "Telegram/ThirdParty" REALPATH)
+
+if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+    set(build_debug 1)
+else()
+    set(build_debug 0)
+endif()
