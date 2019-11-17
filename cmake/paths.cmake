7b6fcb467a8a2f53c21aaa7dfcbee82c7702dd96
John Preston <johnprestonmail@gmail.com>
2019-11-18 00:34:34 +0300

    Move common code to cmake_helpers.

--- cmake/paths.cmake
+++
@@ -1,3 +0,0 @@
-get_filename_component(libs_loc "../Libraries" REALPATH)
-get_filename_component(third_party_loc "Telegram/ThirdParty" REALPATH)
-get_filename_component(submodules_loc "Telegram" REALPATH)
