8c0e168009a15f2141ac4372f1caebdf6c38ae9d
John Preston <johnprestonmail@gmail.com>
2019-12-31 17:26:41 +0300

    Version 1.9.3.
    - Videos in chats start playing automatically.
    - Resume playback from where you left off
    when watching long videos and listening to long audio tracks.
    - Control automatic playback for
    videos, GIFs and round video messages
    in Settings > Advanced > Automatic media download.
    - Enjoy system spell checker support on all modern systems.

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -15,7 +15,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #define TDESKTOP_ALPHA_VERSION (0ULL)
 #endif // TDESKTOP_ALLOW_CLOSED_ALPHA
 
-constexpr auto AppVersion = 1009002;
-constexpr auto AppVersionStr = "1.9.2";
-constexpr auto AppBetaVersion = true;
+constexpr auto AppVersion = 1009003;
+constexpr auto AppVersionStr = "1.9.3";
+constexpr auto AppBetaVersion = false;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
