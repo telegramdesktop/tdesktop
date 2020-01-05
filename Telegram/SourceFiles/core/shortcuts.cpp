c15e4e82efa188cdc507a2a72bcae26b2d09f095
Anton Afanasyev <anton.a.afanasyev@gmail.com>
2020-01-05 08:14:52 +0300

    Add Ctrl+9 shortcut to show archive.

--- Telegram/SourceFiles/core/shortcuts.cpp
+++ Telegram/SourceFiles/core/shortcuts.cpp
@@ -74,6 +74,8 @@ const auto CommandByName = base::flat_map<QString, Command>{
 	{ qsl("first_chat")       , Command::ChatFirst },
 	{ qsl("last_chat")        , Command::ChatLast },
 	{ qsl("self_chat")        , Command::ChatSelf },
+
+	{ qsl("show_archive")     , Command::ShowArchive },
 };
 
 const auto CommandNames = base::flat_map<Command, QString>{
@@ -96,6 +98,8 @@ const auto CommandNames = base::flat_map<Command, QString>{
 	{ Command::ChatFirst     , qsl("first_chat") },
 	{ Command::ChatLast      , qsl("last_chat") },
 	{ Command::ChatSelf      , qsl("self_chat") },
+
+	{ Command::ShowArchive   , qsl("show_archive") },
 };
 
 class Manager {
@@ -331,6 +335,8 @@ void Manager::fillDefaults() {
 	set(qsl("ctrl+5"), Command::ChatPinned5);
 
 	set(qsl("ctrl+0"), Command::ChatSelf);
+
+	set(qsl("ctrl+9"), Command::ShowArchive);
 }
 
 void Manager::writeDefaultFile() {
