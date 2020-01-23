2699af5d5ac5714c4dbd5d0211cd078caa3a4097
John Preston <johnprestonmail@gmail.com>
2020-01-23 10:31:16 +0300

    Version 1.9.7.
    - Create three new kinds of polls.
    - See who voted for what in polls with visible votes.
    - Vote for several options in polls that allow multiple answers.
    - Guess the correct answer in quiz-style polls.
    - Explore various ways of combining the different poll options.
    - Add polls from the '...' menu in any group or channel.
    - Use bots like @QuizBot to create quizzes
    with several questions and media attachments.
    - Schedule messages to be sent when your recipient comes online
    (only works if you know their online status).

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -7,7 +7,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #pragma once
 
-#define TDESKTOP_REQUESTED_ALPHA_VERSION (1009006001ULL)
+#define TDESKTOP_REQUESTED_ALPHA_VERSION (0ULL)
 
 #ifdef TDESKTOP_ALLOW_CLOSED_ALPHA
 #define TDESKTOP_ALPHA_VERSION TDESKTOP_REQUESTED_ALPHA_VERSION
@@ -15,7 +15,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #define TDESKTOP_ALPHA_VERSION (0ULL)
 #endif // TDESKTOP_ALLOW_CLOSED_ALPHA
 
-constexpr auto AppVersion = 1009006;
-constexpr auto AppVersionStr = "1.9.6";
+constexpr auto AppVersion = 1009007;
+constexpr auto AppVersionStr = "1.9.7";
 constexpr auto AppBetaVersion = false;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
