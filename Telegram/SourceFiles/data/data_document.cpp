6a28f9b8ae49db42656666583641de725680f94a
Av <github@avbrand.com>
2020-03-03 11:47:49 -0800

    Increase media cache memory size to solve flicker issue
    When displaying large numbers of GIFs, Telegram's Media Cache runs out of size and unloads items that are still being displayed.

--- Telegram/SourceFiles/data/data_document.cpp
+++ Telegram/SourceFiles/data/data_document.cpp
@@ -42,7 +42,8 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 namespace {
 
-constexpr auto kMemoryForCache = 32 * 1024 * 1024;
+// Updated Mar 3, 2020: Increase the size of the memory cache for media, to prevent items still being displayed from being unloaded.
+constexpr auto kMemoryForCache = 128 * 1024 * 1024; // was 32, updated to 128
 const auto kAnimatedStickerDimensions = QSize(512, 512);
 
 using FilePathResolve = DocumentData::FilePathResolve;
