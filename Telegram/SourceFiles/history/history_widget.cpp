07eab8555a0abbd62f615fa291f1d79e848f060e
John Preston <johnprestonmail@gmail.com>
2020-03-06 15:07:24 +0400

    Fix crash in lottie animation destroy-on-init.

--- Telegram/SourceFiles/history/history_widget.cpp
+++ Telegram/SourceFiles/history/history_widget.cpp
@@ -5106,6 +5106,8 @@ int HistoryWidget::countInitialScrollTop() {
 }
 
 void HistoryWidget::createUnreadBarIfBelowVisibleArea(int withScrollTop) {
+	Expects(_history != nullptr);
+
 	if (_history->unreadBar()) {
 		return;
 	}
@@ -5227,7 +5229,7 @@ void HistoryWidget::updateHistoryGeometry(
 		newScrollTop = countInitialScrollTop();
 		_historyInited = true;
 		_scrollToAnimation.stop();
-	} else if (wasAtBottom && !loadedDown) {
+	} else if (wasAtBottom && !loadedDown && !_history->unreadBar()) {
 		newScrollTop = countAutomaticScrollTop();
 	} else {
 		newScrollTop = std::min(
