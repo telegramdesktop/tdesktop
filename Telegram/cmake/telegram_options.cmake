a0dbac5e0cf6cf111193913b22823089e64a4f6e
Ilya Fedin <fedin-ilja2010@ya.ru>
2020-03-07 09:28:28 +0400

    Remove separate switch for GTK integration and fix condition for GTK file dialog
    Since it is used only for GTK file dialog and to get it by default on any static builds

--- Telegram/cmake/telegram_options.cmake
+++ Telegram/cmake/telegram_options.cmake
@@ -7,7 +7,6 @@
 option(TDESKTOP_FORCE_GTK_FILE_DIALOG "Force using GTK file dialog (Linux only)." OFF)
 option(TDESKTOP_DISABLE_REGISTER_CUSTOM_SCHEME "Disable automatic 'tg://' URL scheme handler registration." ${DESKTOP_APP_USE_PACKAGED})
 option(TDESKTOP_DISABLE_NETWORK_PROXY "Disable all code for working through Socks5 or MTProxy." OFF)
-option(TDESKTOP_DISABLE_GTK_INTEGRATION "Disable all code for GTK integration (Linux only)." ON)
 option(TDESKTOP_USE_PACKAGED_TGVOIP "Find libtgvoip using CMake instead of bundled one." ${DESKTOP_APP_USE_PACKAGED})
 option(TDESKTOP_API_TEST "Use test API credentials." OFF)
 set(TDESKTOP_API_ID "0" CACHE STRING "Provide 'api_id' for the Telegram API access.")
@@ -42,12 +41,12 @@ if (TDESKTOP_API_ID STREQUAL "0" OR TDESKTOP_API_HASH STREQUAL "")
     " ")
 endif()
 
-if (NOT DESKTOP_APP_SPECIAL_TARGET STREQUAL "")
+if (NOT DESKTOP_APP_USE_PACKAGED)
     set(TDESKTOP_FORCE_GTK_FILE_DIALOG ON)
 endif()
 
-if (TDESKTOP_FORCE_GTK_FILE_DIALOG)
-    set(TDESKTOP_DISABLE_GTK_INTEGRATION OFF)
+if (NOT TDESKTOP_FORCE_GTK_FILE_DIALOG)
+    set(TDESKTOP_DISABLE_GTK_INTEGRATION ON)
 endif()
 
 if (DESKTOP_APP_DISABLE_SPELLCHECK)
