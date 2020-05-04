telegramdesktop/tdesktop

commit dd78052f928a144a71eaa3725ddbda2212d5cae0

Use new toast style structure.

Telegram/SourceFiles/history/history_inner_widget.cpp
@@ -2484,19 +2484,18 @@ void HistoryInner::elementStartStickerLoop(
 }
 
 void HistoryInner::elementShowPollResults(
-		not_null<PollData*> poll,
-		FullMsgId context) {
-	_controller->showPollResults(poll, context);
+	not_null<PollData*> poll,
+	FullMsgId context) {
 }
 
 void HistoryInner::elementShowTooltip(const TextWithEntities &text) {
-	auto config = Ui::Toast::Config();
-	config.multiline = config.dark = true;
-	config.minWidth = st::msgMinWidth;
-	config.maxWidth = st::windowMinWidth;
-	config.text = text;
-	config.durationMs = CountToastDuration(config.text);
-	Ui::Toast::Show(_widget, config);
+	Ui::Toast::Show(_widget, Ui::Toast::Config{
+		.text = text,
+		.st = &st::historyInfoToast,
+		.durationMs = CountToastDuration(text),
+		.multiline = true,
+		.dark = true,
+	});
 }
 
 auto HistoryInner::getSelectionState() const

Telegram/SourceFiles/chat_helpers/chat_helpers.style
@@ -218,8 +218,11 @@ stickerGroupCategoryAbout: defaultTextStyle;
 stickerGroupCategoryAddMargin: margins(0px, 10px, 0px, 5px);
 stickerGroupCategoryAdd: stickersTrendingAdd;
 
-stickersToastMaxWidth: 340px;
-stickersToastPadding: margins(16px, 13px, 16px, 12px);
+stickersToast: Toast(defaultToast) {
+	minWidth: 340px;
+	maxWidth: 340px;
+	padding: margins(16px, 13px, 16px, 12px);
+}
 
 stickersEmpty: icon {{ "stickers_empty", windowSubTextFg }};
 

Telegram/SourceFiles/chat_helpers/stickers.cpp
@@ -77,12 +77,11 @@ void ApplyArchivedResult(const MTPDmessages_stickerSetInstallResultArchive &d) {
 	Local::writeInstalledStickers();
 	Local::writeArchivedStickers();
 
-	auto toast = Ui::Toast::Config();
-	toast.text = { tr::lng_stickers_packs_archived(tr::now) };
-	toast.maxWidth = toast.minWidth = st::stickersToastMaxWidth;
-	toast.multiline = true;
-	toast.padding = st::stickersToastPadding;
-	Ui::Toast::Show(toast);
+	Ui::Toast::Show(Ui::Toast::Config{
+		.text = { tr::lng_stickers_packs_archived(tr::now) },
+		.st = &st::stickersToast,
+		.multiline = true,
+	});
 //	Ui::show(Box<StickersBox>(archived, &Auth()), Ui::LayerOption::KeepOther);
 
 	Auth().data().notifyStickersUpdated();

Telegram/SourceFiles/history/history.style
@@ -379,6 +379,17 @@ botKbTinyButton: BotKeyboardButton {
 botKbScroll: defaultSolidScroll;
 
 historyDateFadeDuration: 200;
+historyDiceToast: Toast(defaultToast) {
+	minWidth: msgMinWidth;
+	maxWidth: 640px;
+}
+historyErrorToast: Toast(defaultToast) {
+	minWidth: msgMinWidth;
+}
+historyInfoToast: Toast(defaultToast) {
+	minWidth: msgMinWidth;
+	maxWidth: 380px;
+}
 
 historyPhotoLeft: 14px;
 historyPhotoBubbleMinWidth: 200px;

Telegram/SourceFiles/history/history_widget.cpp
@@ -236,11 +236,11 @@ object_ptr<Ui::FlatButton> SetupDiscussButton(
 }
 
 void ShowErrorToast(const QString &text) {
-	auto config = Ui::Toast::Config();
-	config.multiline = true;
-	config.minWidth = st::msgMinWidth;
-	config.text = { text };
-	Ui::Toast::Show(config);
+	Ui::Toast::Show(Ui::Toast::Config{
+		.text = { text },
+		.st = &st::historyErrorToast,
+		.multiline = true,
+	});
 }
 
 } // namespace

Telegram/SourceFiles/history/view/history_view_scheduled_section.cpp
@@ -48,11 +48,11 @@ namespace HistoryView {
 namespace {
 
 void ShowErrorToast(const QString &text) {
-	auto config = Ui::Toast::Config();
-	config.multiline = true;
-	config.minWidth = st::msgMinWidth;
-	config.text = { text };
-	Ui::Toast::Show(config);
+	Ui::Toast::Show(Ui::Toast::Config{
+		.text = { text },
+		.st = &st::historyErrorToast,
+		.multiline = true,
+	});
 }
 
 } // namespace

Telegram/SourceFiles/history/view/media/history_view_dice.cpp
@@ -19,6 +19,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "ui/text/text_utilities.h"
 #include "lang/lang_keys.h"
 #include "main/main_session.h"
+#include "styles/style_history.h"
 
 namespace HistoryView {
 namespace {
@@ -35,12 +36,12 @@ namespace {
 		not_null<History*> history,
 		const QString &emoji) {
 	return std::make_shared<LambdaClickHandler>([=] {
-		auto config = Ui::Toast::Config();
-		config.multiline = true;
-		config.minWidth = st::msgMinWidth;
-		config.maxWidth = st::toastMaxWidth + st::msgMinWidth;
-		config.text = { tr::lng_about_random(tr::now, lt_emoji, emoji) };
-		config.durationMs = Ui::Toast::kDefaultDuration * 2;
+		auto config = Ui::Toast::Config{
+			.text = { tr::lng_about_random(tr::now, lt_emoji, emoji) },
+			.st = &st::historyDiceToast,
+			.durationMs = Ui::Toast::kDefaultDuration * 2,
+			.multiline = true,
+		};
 		auto link = Ui::Text::Link(
 			tr::lng_about_random_send(tr::now).toUpper());
 		link.entities.push_back(

Telegram/SourceFiles/passport/passport_form_controller.cpp
@@ -759,9 +759,9 @@ std::vector<not_null<const Value*>> FormController::submitGetErrors() {
 		_view->showToast(tr::lng_passport_success(tr::now));
 
 		base::call_delayed(
-			(st::toastFadeInDuration
+			(st::defaultToast.durationFadeIn
 				+ Ui::Toast::kDefaultDuration
-				+ st::toastFadeOutDuration),
+				+ st::defaultToast.durationFadeOut),
 			this,
 			[=] { cancel(); });
 	}).fail([=](const RPCError &error) {

commit 73691e795b27f29a820144ee8c55f825b310db2b

Fix broken poll results view.
Regression was introduced in dd78052f92.

Fixes #7780.

Copy-Paste is bad.

Telegram/SourceFiles/history/history_inner_widget.cpp
@@ -2477,8 +2477,9 @@ void HistoryInner::elementStartStickerLoop(
 }
 
 void HistoryInner::elementShowPollResults(
-	not_null<PollData*> poll,
-	FullMsgId context) {
+		not_null<PollData*> poll,
+		FullMsgId context) {
+	_controller->showPollResults(poll, context);
 }
 
 void HistoryInner::elementShowTooltip(