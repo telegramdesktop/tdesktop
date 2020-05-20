97305c8cb534bf26432a05775ba50d5ec5f7a3b6
John Preston <johnprestonmail@gmail.com>
2020-05-20 12:49:41 +0400

    Fix build.

--- Telegram/SourceFiles/window/notifications_manager.cpp
+++ Telegram/SourceFiles/window/notifications_manager.cpp
@@ -39,7 +39,7 @@ constexpr auto kWaitingForAllGroupedDelay = crl::time(1000);
 
 #ifdef Q_OS_MAC
 constexpr auto kSystemAlertDuration = crl::time(1000);
-#elif // !Q_OS_MAC
+#else // !Q_OS_MAC
 constexpr auto kSystemAlertDuration = crl::time(0);
 #endif // Q_OS_MAC
 
