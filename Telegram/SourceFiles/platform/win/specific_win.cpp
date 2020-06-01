498b470a1c5cc909859d8aa10b49dfea16f5a517
c0re100 <corehusky@gmail.com>
2020-06-02 01:46:49 +0800

    Missing platform header

--- Telegram/SourceFiles/platform/win/specific_win.cpp
+++ Telegram/SourceFiles/platform/win/specific_win.cpp
@@ -12,6 +12,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "platform/win/windows_app_user_model_id.h"
 #include "platform/win/windows_dlls.h"
 #include "base/call_delayed.h"
+#include "base/platform/base_platform_info.h"
 #include "lang/lang_keys.h"
 #include "mainwindow.h"
 #include "mainwidget.h"
