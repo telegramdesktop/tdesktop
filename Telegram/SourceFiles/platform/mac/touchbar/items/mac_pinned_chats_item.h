54149fb156bf731bedd09c570ac220c5ace89df7
23rd <23rd@vivaldi.net>
2020-07-03 21:48:13 +0300

    Moved panel of pinned dialogs for touchbar to separate file.

---
+++ Telegram/SourceFiles/platform/mac/touchbar/items/mac_pinned_chats_item.h
@@ -0,0 +1,20 @@
+/*
+This file is part of Telegram Desktop,
+the official desktop application for the Telegram messaging service.
+
+For license and copyright information please follow this link:
+https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
+*/
+#pragma once
+
+#include <AppKit/NSImageView.h>
+
+namespace Main {
+class Session;
+} // namespace Main
+
+API_AVAILABLE(macos(10.12.2))
+@interface PinnedDialogsPanel : NSImageView
+- (id)init:(not_null<Main::Session*>)session
+	destroyEvent:(rpl::producer<>)touchBarSwitches;
+@end
