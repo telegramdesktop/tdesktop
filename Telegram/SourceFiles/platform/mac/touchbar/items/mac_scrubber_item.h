c50df6a6bc95707131208bafcddabf1f6b64a5b4
23rd <23rd@vivaldi.net>
2020-07-05 20:26:31 +0300

    Moved touchbar scrubber item for stickers and emoji to separate file.

---
+++ Telegram/SourceFiles/platform/mac/touchbar/items/mac_scrubber_item.h
@@ -0,0 +1,21 @@
+/*
+This file is part of Telegram Desktop,
+the official desktop application for the Telegram messaging service.
+
+For license and copyright information please follow this link:
+https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
+*/
+#pragma once
+
+#import <AppKit/NSPopoverTouchBarItem.h>
+#import <AppKit/NSTouchBar.h>
+
+namespace Window {
+class Controller;
+} // namespace Window
+
+API_AVAILABLE(macos(10.12.2))
+@interface StickerEmojiPopover : NSPopoverTouchBarItem<NSTouchBarDelegate>
+- (id)init:(not_null<Window::Controller*>)controller
+	identifier:(NSTouchBarItemIdentifier)identifier;
+@end
