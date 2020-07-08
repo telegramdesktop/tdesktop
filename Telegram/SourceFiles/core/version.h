a56a9f2c854fa8e04233d1de19dc843c3cb4a99b
c0re100 <corehusky@gmail.com>
2020-07-09 02:52:26 +0800

    Bump to 2.1.18.1

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -26,4 +26,4 @@ constexpr auto AppVersion = 2001018;
 constexpr auto AppVersionStr = "2.1.18";
 constexpr auto AppBetaVersion = true;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
-constexpr auto AppVersionReleaseStr = "2.1.17.3";
+constexpr auto AppVersionReleaseStr = "2.1.18.1";
