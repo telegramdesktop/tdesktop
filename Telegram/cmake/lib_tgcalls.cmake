a4ee90e8c6f4956b27350f1541e7b4ce3818dbf4
John Preston <johnprestonmail@gmail.com>
2020-07-16 16:05:04 +0400

    Remove webrtc test code usage on Windows.

--- Telegram/cmake/lib_tgcalls.cmake
+++ Telegram/cmake/lib_tgcalls.cmake
@@ -11,8 +11,6 @@ add_library(tdesktop::lib_tgcalls ALIAS lib_tgcalls)
 set(tgcalls_dir ${third_party_loc}/tgcalls)
 set(tgcalls_loc ${tgcalls_dir}/tgcalls)
 
-target_compile_options(lib_tgcalls PRIVATE -fobjc-arc)
-
 nice_target_sources(lib_tgcalls ${tgcalls_loc}
 PRIVATE
     CodecSelectHelper.cpp
@@ -43,6 +41,8 @@ PRIVATE
     platform/tdesktop/VideoCapturerInterfaceImpl.h
     platform/tdesktop/VideoCapturerTrackSource.cpp
     platform/tdesktop/VideoCapturerTrackSource.h
+    platform/tdesktop/VideoCameraCapturer.cpp
+    platform/tdesktop/VideoCameraCapturer.h
 
     # Windows
     platform/windows/WindowsInterface.cpp
@@ -87,6 +87,10 @@ if (WIN32)
         WEBRTC_WIN
     )
 elseif (APPLE)
+    target_compile_options(lib_tgcalls
+    PRIVATE
+        -fobjc-arc
+    )
     target_compile_definitions(lib_tgcalls
     PRIVATE
         WEBRTC_MAC
