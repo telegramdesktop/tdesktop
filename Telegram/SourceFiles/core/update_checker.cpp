a347160e685924acd4976b57779ee51a1f4a0e00
c0re100 <corehusky@gmail.com>
2020-08-16 15:22:11 +0800

    Disable MtpChecker

--- Telegram/SourceFiles/core/update_checker.cpp
+++ Telegram/SourceFiles/core/update_checker.cpp
@@ -1228,9 +1228,9 @@ void Updater::start(bool forceWait) {
 		startImplementation(
 			&_httpImplementation,
 			std::make_unique<HttpChecker>(_testing));
-		startImplementation(
-			&_mtpImplementation,
-			std::make_unique<MtpChecker>(_session, _testing));
+		//startImplementation(
+		//	&_mtpImplementation,
+		//	std::make_unique<MtpChecker>(_session, _testing));
 
 		_checking.fire({});
 	} else {
