e523492de01ea5fec5e6f3113011b04903842a9a
Ilya Fedin <fedin-ilja2010@ya.ru>
2020-08-29 01:02:26 +0400

    Add missed methods to specific_mac.h and specific_win.h

--- Telegram/SourceFiles/platform/mac/specific_mac.h
+++ Telegram/SourceFiles/platform/mac/specific_mac.h
@@ -44,6 +44,14 @@ inline bool TrayIconSupported() {
 	return true;
 }
 
+inline bool SetWindowExtents(QWindow *window, const QMargins &extents) {
+	return false;
+}
+
+inline bool UnsetWindowExtents(QWindow *window) {
+	return false;
+}
+
 inline bool WindowsNeedShadow() {
 	return false;
 }
