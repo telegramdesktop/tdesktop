059ac9d1e54ce821dea6a6e1cd6dc9945748cdd0
Vitaly Kryukov <vit110188@gmail.com>
2020-09-09 15:46:15 +0300

    Update mtproto_domain_resolver.cpp

--- Telegram/SourceFiles/mtproto/details/mtproto_domain_resolver.cpp
+++ Telegram/SourceFiles/mtproto/details/mtproto_domain_resolver.cpp
@@ -65,7 +65,7 @@ QByteArray DnsUserAgent() {
 	static const auto kResult = QByteArray(
 		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
 		"AppleWebKit/537.36 (KHTML, like Gecko) "
-		"Chrome/85.0.4183.83 Safari/537.36");
+		"Chrome/85.0.4183.102 Safari/537.36");
 	return kResult;
 }
 
