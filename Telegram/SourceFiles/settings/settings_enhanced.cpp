e41805429dd074b1863d219a6f518b12bf0bb844
c0re100 <corehusky@gmail.com>
2020-10-06 08:13:06 +0800

    Format enhanced setting file

--- Telegram/SourceFiles/settings/settings_enhanced.cpp
+++ Telegram/SourceFiles/settings/settings_enhanced.cpp
@@ -36,70 +36,69 @@ https://github.com/kotatogram/kotatogram-desktop/blob/dev/LEGAL
 
 namespace Settings {
 
-void SetupEnhancedNetwork(not_null<Ui::VerticalLayout*> container) {
-	AddDivider(container);
-	AddSkip(container);
-	AddSubsectionTitle(container, tr::lng_settings_network());
+	void SetupEnhancedNetwork(not_null<Ui::VerticalLayout *> container) {
+		AddDivider(container);
+		AddSkip(container);
+		AddSubsectionTitle(container, tr::lng_settings_network());
 
-	AddButtonWithLabel(
-		container,
-		tr::lng_settings_net_speed_boost(),
-		rpl::single(NetBoostBox::BoostLabel(cNetSpeedBoost())),
-		st::settingsButton
-	)->addClickHandler([=] {
-		Ui::show(Box<NetBoostBox>());
-	});
+		AddButtonWithLabel(
+				container,
+				tr::lng_settings_net_speed_boost(),
+				rpl::single(NetBoostBox::BoostLabel(cNetSpeedBoost())),
+				st::settingsButton
+		)->addClickHandler([=] {
+			Ui::show(Box<NetBoostBox>());
+		});
 
-	AddSkip(container);
-}
+		AddSkip(container);
+	}
 
-void SetupEnhancedMessages(not_null<Ui::VerticalLayout*> container) {
-	AddDivider(container);
-	AddSkip(container);
-	AddSubsectionTitle(container, tr::lng_settings_messages());
+	void SetupEnhancedMessages(not_null<Ui::VerticalLayout *> container) {
+		AddDivider(container);
+		AddSkip(container);
+		AddSubsectionTitle(container, tr::lng_settings_messages());
 
-	const auto wrap = container->add(
-		object_ptr<Ui::SlideWrap<Ui::VerticalLayout>>(
-			container,
-			object_ptr<Ui::VerticalLayout>(container)));
-	const auto inner = wrap->entity();
+		const auto wrap = container->add(
+				object_ptr<Ui::SlideWrap<Ui::VerticalLayout>>(
+						container,
+						object_ptr<Ui::VerticalLayout>(container)));
+		const auto inner = wrap->entity();
 
-	AddButton(
-		inner,
-		tr::lng_settings_show_message_id(),
-		st::settingsButton
-	)->toggleOn(
-		rpl::single(cShowMessagesID())
-	)->toggledChanges(
-	) | rpl::filter([=](bool toggled) {
-		return (toggled != cShowMessagesID());
-	}) | rpl::start_with_next([=](bool toggled) {
-		cSetShowMessagesID(toggled);
-		EnhancedSettings::Write();
-		App::restart();
-	}, container->lifetime());
+		AddButton(
+				inner,
+				tr::lng_settings_show_message_id(),
+				st::settingsButton
+		)->toggleOn(
+				rpl::single(cShowMessagesID())
+		)->toggledChanges(
+		) | rpl::filter([=](bool toggled) {
+			return (toggled != cShowMessagesID());
+		}) | rpl::start_with_next([=](bool toggled) {
+			cSetShowMessagesID(toggled);
+			EnhancedSettings::Write();
+			App::restart();
+		}, container->lifetime());
 
-	AddSkip(inner);
-	AddDividerText(inner, tr::lng_show_messages_id_desc());
+		AddSkip(inner);
+		AddDividerText(inner, tr::lng_show_messages_id_desc());
 
-	AddSkip(container);
-}
+		AddSkip(container);
+	}
 
-Enhanced::Enhanced(
-	QWidget *parent,
-	not_null<Window::SessionController*> controller)
-: Section(parent) {
-	setupContent(controller);
-}
+	Enhanced::Enhanced(
+			QWidget *parent,
+			not_null<Window::SessionController *> controller)
+			: Section(parent) {
+		setupContent(controller);
+	}
 
-void Enhanced::setupContent(not_null<Window::SessionController*> controller) {
-	const auto content = Ui::CreateChild<Ui::VerticalLayout>(this);
+	void Enhanced::setupContent(not_null<Window::SessionController *> controller) {
+		const auto content = Ui::CreateChild<Ui::VerticalLayout>(this);
 
-	SetupEnhancedNetwork(content);
-	SetupEnhancedMessages(content);
+		SetupEnhancedNetwork(content);
+		SetupEnhancedMessages(content);
 
-	Ui::ResizeFitChild(this, content);
-}
-
+		Ui::ResizeFitChild(this, content);
+	}
 } // namespace Settings
 
