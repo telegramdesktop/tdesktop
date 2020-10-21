5f932a8828957bc9b497f90797d59d3f8c9aba12
John Preston <johnprestonmail@gmail.com>
2020-10-21 10:58:46 +0300

    Show 'View X Replies' in all groups.

--- Telegram/SourceFiles/history/history_inner_widget.cpp
+++ Telegram/SourceFiles/history/history_inner_widget.cpp
@@ -1552,10 +1552,8 @@ void HistoryInner::showContextMenu(QContextMenuEvent *e, bool showFromTouch) {
 		}
 		const auto repliesCount = item->repliesCount();
 		const auto withReplies = IsServerMsgId(item->id)
-			&& (repliesCount > 0 || item->replyToTop());
-		if (withReplies
-			&& item->history()->peer->isMegagroup()
-			&& item->history()->peer->asMegagroup()->linkedChat()) {
+			&& (repliesCount > 0);
+		if (withReplies && item->history()->peer->isMegagroup()) {
 			const auto rootId = repliesCount ? item->id : item->replyToTop();
 			const auto phrase = (repliesCount > 0)
 				? tr::lng_replies_view(
