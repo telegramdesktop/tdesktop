c3cbbe6052d40a264786042358d748112c67215f
Ilya Fedin <fedin-ilja2010@ya.ru>
2020-10-29 18:34:31 +0400

    Remove forgotten sws_scale check
    It should been removed in 7d29f9ce17c837a0ae2944f5b2c75dc16c9f61f6, but was forgotten

--- Telegram/SourceFiles/media/clip/media_clip_ffmpeg.cpp
+++ Telegram/SourceFiles/media/clip/media_clip_ffmpeg.cpp
@@ -258,11 +258,7 @@ bool FFMpegReaderImplementation::renderFrame(QImage &to, bool &hasAlpha, const Q
 		// AV_NUM_DATA_POINTERS defined in AVFrame struct
 		uint8_t *toData[AV_NUM_DATA_POINTERS] = { to.bits(), nullptr };
 		int toLinesize[AV_NUM_DATA_POINTERS] = { to.bytesPerLine(), 0 };
-		int res;
-		if ((res = sws_scale(_swsContext, _frame->data, _frame->linesize, 0, _frame->height, toData, toLinesize)) != _swsSize.height()) {
-			LOG(("Gif Error: Unable to sws_scale to good size %1, height %2, should be %3").arg(logData()).arg(res).arg(_swsSize.height()));
-			return false;
-		}
+		sws_scale(_swsContext, _frame->data, _frame->linesize, 0, _frame->height, toData, toLinesize);
 	}
 	if (hasAlpha) {
 		FFmpeg::PremultiplyInplace(to);
