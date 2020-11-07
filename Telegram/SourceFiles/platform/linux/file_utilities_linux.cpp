acaf8e4931dbec11ceb087cd8408958022e6556e
Ilya Fedin <fedin-ilja2010@ya.ru>
2020-11-08 00:29:58 +0400

    Use g_filename_to_uri

--- Telegram/SourceFiles/platform/linux/file_utilities_linux.cpp
+++ Telegram/SourceFiles/platform/linux/file_utilities_linux.cpp
@@ -82,7 +82,7 @@ void UnsafeLaunch(const QString &filepath) {
 	const auto absolutePath = QFileInfo(filepath).absoluteFilePath();
 
 	if (!g_app_info_launch_default_for_uri(
-		("file://" + absolutePath).toUtf8(),
+		g_filename_to_uri(absolutePath.toUtf8(), nullptr, nullptr),
 		nullptr,
 		nullptr)) {
 		QDesktopServices::openUrl(QUrl::fromLocalFile(filepath));
