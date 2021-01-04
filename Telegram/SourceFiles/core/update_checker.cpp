02f04b8a1c11c93d197c501cfd5a0f5d898aaaaa
c0re100 <corehusky@gmail.com>
2021-01-04 08:28:00 +0800

    Add beta field

--- Telegram/SourceFiles/core/update_checker.cpp
+++ Telegram/SourceFiles/core/update_checker.cpp
@@ -618,6 +618,7 @@ void HttpChecker::start() {
 	DEBUG_LOG(("Update Info: requesting update state"));
 	QNetworkRequest request(url);
 	request.setRawHeader("td-version", AppVersionReleaseStr);
+	request.setRawHeader("beta", AppBetaVersion ? "1" : "0");
 	_manager = std::make_unique<QNetworkAccessManager>();
 	_reply = _manager->get(request);
 	_reply->connect(_reply, &QNetworkReply::finished, [=] {
