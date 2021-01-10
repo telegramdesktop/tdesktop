c13a13def77b03679b0dce7a72ac2a9641074139
Ilya Fedin <fedin-ilja2010@ya.ru>
2021-01-10 16:27:12 +0400

    Use operator-> for tray icon biggest size

--- Telegram/SourceFiles/platform/linux/main_window_linux.cpp
+++ Telegram/SourceFiles/platform/linux/main_window_linux.cpp
@@ -250,7 +250,7 @@ QIcon TrayIconGen(int counter, bool muted) {
 						std::less<>(),
 						&QSize::width);
 
-					if ((*biggestSize).width() > firstAttemptSize.width()) {
+					if (biggestSize->width() > firstAttemptSize.width()) {
 						currentImageBack = systemIcon
 							.pixmap(*biggestSize)
 							.toImage();
@@ -381,7 +381,7 @@ std::unique_ptr<QTemporaryFile> TrayIconFile(
 			std::less<>(),
 			&QSize::width);
 
-		if ((*biggestSize).width() > firstAttemptSize.width()) {
+		if (biggestSize->width() > firstAttemptSize.width()) {
 			scalePixmap(icon.pixmap(*biggestSize)).save(ret.get());
 		} else {
 			scalePixmap(firstAttempt).save(ret.get());
