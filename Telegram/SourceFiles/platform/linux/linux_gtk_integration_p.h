9af8eb347ab33f9411b309ad270c682e38a0ac96
Ilya Fedin <fedin-ilja2010@ya.ru>
2021-01-25 00:31:58 +0400

    Fix build with linked gtk

--- Telegram/SourceFiles/platform/linux/linux_gtk_integration_p.h
+++ Telegram/SourceFiles/platform/linux/linux_gtk_integration_p.h
@@ -27,7 +27,7 @@ extern "C" {
 #endif // !LINK_TO_GTK
 
 // To be able to compile with gtk-2.0 headers as well
-#define GdkMonitor GdkScreen
+typedef struct _GdkMonitor GdkMonitor;
 typedef struct _GtkAppChooser GtkAppChooser;
 
 namespace Platform {
