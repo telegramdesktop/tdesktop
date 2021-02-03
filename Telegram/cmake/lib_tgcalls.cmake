80b804cb5b9d319d868cec7f7cb228126b1087a0
Nicholas Guriev <nicholas@guriev.su>
2021-02-03 15:23:23 +0300

    Clean up DESKTOP_APP_DISABLE_WEBRTC_INTEGRATION

--- Telegram/cmake/lib_tgcalls.cmake
+++ Telegram/cmake/lib_tgcalls.cmake
@@ -25,103 +25,101 @@ PRIVATE
     Instance.h
 )
 
-if (NOT DESKTOP_APP_DISABLE_WEBRTC_INTEGRATION)
-    nice_target_sources(lib_tgcalls ${tgcalls_loc}
-    PRIVATE
-        AudioDeviceHelper.cpp
-        AudioDeviceHelper.h
-        CodecSelectHelper.cpp
-        CodecSelectHelper.h
-        CryptoHelper.cpp
-        CryptoHelper.h
-        EncryptedConnection.cpp
-        EncryptedConnection.h
-        InstanceImpl.cpp
-        InstanceImpl.h
-        LogSinkImpl.cpp
-        LogSinkImpl.h
-        Manager.cpp
-        Manager.h
-        MediaManager.cpp
-        MediaManager.h
-        Message.cpp
-        Message.h
-        NetworkManager.cpp
-        NetworkManager.h
-        ThreadLocalObject.h
-        VideoCaptureInterface.cpp
-        VideoCaptureInterface.h
-        VideoCaptureInterfaceImpl.cpp
-        VideoCaptureInterfaceImpl.h
-        VideoCapturerInterface.h
+nice_target_sources(lib_tgcalls ${tgcalls_loc}
+PRIVATE
+    AudioDeviceHelper.cpp
+    AudioDeviceHelper.h
+    CodecSelectHelper.cpp
+    CodecSelectHelper.h
+    CryptoHelper.cpp
+    CryptoHelper.h
+    EncryptedConnection.cpp
+    EncryptedConnection.h
+    InstanceImpl.cpp
+    InstanceImpl.h
+    LogSinkImpl.cpp
+    LogSinkImpl.h
+    Manager.cpp
+    Manager.h
+    MediaManager.cpp
+    MediaManager.h
+    Message.cpp
+    Message.h
+    NetworkManager.cpp
+    NetworkManager.h
+    ThreadLocalObject.h
+    VideoCaptureInterface.cpp
+    VideoCaptureInterface.h
+    VideoCaptureInterfaceImpl.cpp
+    VideoCaptureInterfaceImpl.h
+    VideoCapturerInterface.h
 
-        group/GroupInstanceImpl.cpp
-        group/GroupInstanceImpl.h
+    group/GroupInstanceImpl.cpp
+    group/GroupInstanceImpl.h
 
-        platform/PlatformInterface.h
+    platform/PlatformInterface.h
 
-        # Android
-        platform/android/AndroidContext.cpp
-        platform/android/AndroidContext.h
-        platform/android/AndroidInterface.cpp
-        platform/android/AndroidInterface.h
-        platform/android/VideoCameraCapturer.cpp
-        platform/android/VideoCameraCapturer.h
-        platform/android/VideoCapturerInterfaceImpl.cpp
-        platform/android/VideoCapturerInterfaceImpl.h
+    # Android
+    platform/android/AndroidContext.cpp
+    platform/android/AndroidContext.h
+    platform/android/AndroidInterface.cpp
+    platform/android/AndroidInterface.h
+    platform/android/VideoCameraCapturer.cpp
+    platform/android/VideoCameraCapturer.h
+    platform/android/VideoCapturerInterfaceImpl.cpp
+    platform/android/VideoCapturerInterfaceImpl.h
 
-        # iOS / macOS
-        platform/darwin/DarwinInterface.h
-        platform/darwin/DarwinInterface.mm
-        platform/darwin/GLVideoView.h
-        platform/darwin/GLVideoView.mm
-        platform/darwin/TGRTCCVPixelBuffer.h
-        platform/darwin/TGRTCCVPixelBuffer.mm
-        platform/darwin/TGRTCDefaultVideoDecoderFactory.h
-        platform/darwin/TGRTCDefaultVideoDecoderFactory.mm
-        platform/darwin/TGRTCDefaultVideoEncoderFactory.h
-        platform/darwin/TGRTCDefaultVideoEncoderFactory.mm
-        platform/darwin/TGRTCVideoDecoderH264.h
-        platform/darwin/TGRTCVideoDecoderH264.mm
-        platform/darwin/TGRTCVideoDecoderH265.h
-        platform/darwin/TGRTCVideoDecoderH265.mm
-        platform/darwin/TGRTCVideoEncoderH264.h
-        platform/darwin/TGRTCVideoEncoderH264.mm
-        platform/darwin/TGRTCVideoEncoderH265.h
-        platform/darwin/TGRTCVideoEncoderH265.mm
-        platform/darwin/VideoCameraCapturer.h
-        platform/darwin/VideoCameraCapturer.mm
-        platform/darwin/VideoCameraCapturerMac.h
-        platform/darwin/VideoCameraCapturerMac.mm
-        platform/darwin/VideoCapturerInterfaceImpl.h
-        platform/darwin/VideoCapturerInterfaceImpl.mm
-        platform/darwin/VideoMetalView.h
-        platform/darwin/VideoMetalView.mm
-        platform/darwin/VideoMetalViewMac.h
-        platform/darwin/VideoMetalViewMac.mm
+    # iOS / macOS
+    platform/darwin/DarwinInterface.h
+    platform/darwin/DarwinInterface.mm
+    platform/darwin/GLVideoView.h
+    platform/darwin/GLVideoView.mm
+    platform/darwin/TGRTCCVPixelBuffer.h
+    platform/darwin/TGRTCCVPixelBuffer.mm
+    platform/darwin/TGRTCDefaultVideoDecoderFactory.h
+    platform/darwin/TGRTCDefaultVideoDecoderFactory.mm
+    platform/darwin/TGRTCDefaultVideoEncoderFactory.h
+    platform/darwin/TGRTCDefaultVideoEncoderFactory.mm
+    platform/darwin/TGRTCVideoDecoderH264.h
+    platform/darwin/TGRTCVideoDecoderH264.mm
+    platform/darwin/TGRTCVideoDecoderH265.h
+    platform/darwin/TGRTCVideoDecoderH265.mm
+    platform/darwin/TGRTCVideoEncoderH264.h
+    platform/darwin/TGRTCVideoEncoderH264.mm
+    platform/darwin/TGRTCVideoEncoderH265.h
+    platform/darwin/TGRTCVideoEncoderH265.mm
+    platform/darwin/VideoCameraCapturer.h
+    platform/darwin/VideoCameraCapturer.mm
+    platform/darwin/VideoCameraCapturerMac.h
+    platform/darwin/VideoCameraCapturerMac.mm
+    platform/darwin/VideoCapturerInterfaceImpl.h
+    platform/darwin/VideoCapturerInterfaceImpl.mm
+    platform/darwin/VideoMetalView.h
+    platform/darwin/VideoMetalView.mm
+    platform/darwin/VideoMetalViewMac.h
+    platform/darwin/VideoMetalViewMac.mm
 
-        # POSIX
+    # POSIX
 
-        # Teleram Desktop
-        platform/tdesktop/DesktopInterface.cpp
-        platform/tdesktop/DesktopInterface.h
-        platform/tdesktop/VideoCapturerInterfaceImpl.cpp
-        platform/tdesktop/VideoCapturerInterfaceImpl.h
-        platform/tdesktop/VideoCapturerTrackSource.cpp
-        platform/tdesktop/VideoCapturerTrackSource.h
-        platform/tdesktop/VideoCameraCapturer.cpp
-        platform/tdesktop/VideoCameraCapturer.h
+    # Teleram Desktop
+    platform/tdesktop/DesktopInterface.cpp
+    platform/tdesktop/DesktopInterface.h
+    platform/tdesktop/VideoCapturerInterfaceImpl.cpp
+    platform/tdesktop/VideoCapturerInterfaceImpl.h
+    platform/tdesktop/VideoCapturerTrackSource.cpp
+    platform/tdesktop/VideoCapturerTrackSource.h
+    platform/tdesktop/VideoCameraCapturer.cpp
+    platform/tdesktop/VideoCameraCapturer.h
 
-        # All
-        reference/InstanceImplReference.cpp
-        reference/InstanceImplReference.h
-    )
+    # All
+    reference/InstanceImplReference.cpp
+    reference/InstanceImplReference.h
+)
 
-    target_link_libraries(lib_tgcalls
-    PRIVATE
-        desktop-app::external_webrtc
-    )
-endif()
+target_link_libraries(lib_tgcalls
+PRIVATE
+    desktop-app::external_webrtc
+)
 
 target_compile_definitions(lib_tgcalls
 PRIVATE
@@ -143,25 +141,23 @@ elseif (APPLE)
     PRIVATE
         WEBRTC_MAC
     )
-    if (NOT DESKTOP_APP_DISABLE_WEBRTC_INTEGRATION)
-        remove_target_sources(lib_tgcalls ${tgcalls_loc}
-            platform/darwin/GLVideoView.h
-            platform/darwin/GLVideoView.mm
-            platform/darwin/VideoCameraCapturer.h
-            platform/darwin/VideoCameraCapturer.mm
-            platform/darwin/VideoMetalView.h
-            platform/darwin/VideoMetalView.mm
-            platform/darwin/VideoMetalViewMac.h
-            platform/darwin/VideoMetalViewMac.mm
-            platform/tdesktop/DesktopInterface.cpp
-            platform/tdesktop/DesktopInterface.h
-            platform/tdesktop/VideoCapturerTrackSource.cpp
-            platform/tdesktop/VideoCapturerTrackSource.h
-            platform/tdesktop/VideoCapturerInterfaceImpl.cpp
-            platform/tdesktop/VideoCapturerInterfaceImpl.h
-        )
-    endif()
-else()
+    remove_target_sources(lib_tgcalls ${tgcalls_loc}
+        platform/darwin/GLVideoView.h
+        platform/darwin/GLVideoView.mm
+        platform/darwin/VideoCameraCapturer.h
+        platform/darwin/VideoCameraCapturer.mm
+        platform/darwin/VideoMetalView.h
+        platform/darwin/VideoMetalView.mm
+        platform/darwin/VideoMetalViewMac.h
+        platform/darwin/VideoMetalViewMac.mm
+        platform/tdesktop/DesktopInterface.cpp
+        platform/tdesktop/DesktopInterface.h
+        platform/tdesktop/VideoCapturerTrackSource.cpp
+        platform/tdesktop/VideoCapturerTrackSource.h
+        platform/tdesktop/VideoCapturerInterfaceImpl.cpp
+        platform/tdesktop/VideoCapturerInterfaceImpl.h
+    )
+elseif (CMAKE_SYSTEM_NAME STREQUAL "Linux")
     target_compile_definitions(lib_tgcalls
     PRIVATE
         WEBRTC_LINUX
