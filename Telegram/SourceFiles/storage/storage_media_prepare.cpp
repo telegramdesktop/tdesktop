telegramdesktop/tdesktop

commit ce1b94eb169bef3b0f90ec9397ebee602e28a998

Send PDFs only as files.
Fixes #10294.

Telegram/SourceFiles/storage/storage_media_prepare.cpp
@@ -40,7 +40,7 @@ bool ValidPhotoForAlbum(
 		const QString &mime) {
 	if (image.animated
 		|| Core::IsMimeSticker(mime)
-		|| (mime == u"application/pdf"_q)) {
+		|| !mime.startsWith(u"image/")) {
 		return false;
 	}
 	const auto width = image.data.width();

Telegram/SourceFiles/storage/localimageloader.cpp
@@ -875,7 +875,8 @@ void FileLoadTask::process(Args &&args) {
 				}
 			} else if (isAnimation) {
 				attributes.push_back(MTP_documentAttributeAnimated());
-			} else if (_type != SendMediaType::File) {
+			} else if (filemime.startsWith(u"image/"_q)
+				&& _type != SendMediaType::File) {
 				auto medium = (w > 320 || h > 320) ? fullimage.scaled(320, 320, Qt::KeepAspectRatio, Qt::SmoothTransformation) : fullimage;
 				auto full = (w > 1280 || h > 1280) ? fullimage.scaled(1280, 1280, Qt::KeepAspectRatio, Qt::SmoothTransformation) : fullimage;
 				{

commit 93a88b54ad065d43ce4ebd7ea503ab44ef3e6990

Fix sending images from clipboard.
Regression was introduced in ce1b94eb1.

Telegram/SourceFiles/storage/storage_media_prepare.cpp
@@ -40,7 +40,7 @@ bool ValidPhotoForAlbum(
 		const QString &mime) {
 	if (image.animated
 		|| Core::IsMimeSticker(mime)
-		|| !mime.startsWith(u"image/")) {
+		|| (!mime.isEmpty() && !mime.startsWith(u"image/"))) {
 		return false;
 	}
 	const auto width = image.data.width();