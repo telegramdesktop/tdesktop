d3d7d76eabf87ea58c2c7bed33e5e5362816e525
Ilya Fedin <fedin-ilja2010@ya.ru>
2021-03-01 22:40:09 +0400

    Move NotificationServiceWatcher to glibmm

--- Telegram/SourceFiles/platform/linux/linux_notification_service_watcher.cpp
+++ Telegram/SourceFiles/platform/linux/linux_notification_service_watcher.cpp
@@ -10,9 +10,11 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "core/application.h"
 #include "main/main_domain.h"
 #include "window/notifications_manager.h"
+#include "base/platform/linux/base_linux_glibmm_helper.h"
 #include "base/platform/linux/base_linux_dbus_utilities.h"
 
-#include <QtDBus/QDBusConnection>
+#include <glibmm.h>
+#include <giomm.h>
 
 namespace Platform {
 namespace internal {
@@ -40,27 +42,43 @@ bool IsNotificationServiceActivatable() {
 
 } // namespace
 
-NotificationServiceWatcher::NotificationServiceWatcher()
-: _dbusWatcher(
-		kNotificationService.utf16(),
-		QDBusConnection::sessionBus(),
-		QDBusServiceWatcher::WatchForOwnerChange) {
-	const auto signal = &QDBusServiceWatcher::serviceOwnerChanged;
-	QObject::connect(&_dbusWatcher, signal, [=](
-			const QString &service,
-			const QString &oldOwner,
-			const QString &newOwner) {
-		crl::on_main([=] {
-			if (!Core::App().domain().started()) {
-				return;
-			} else if (IsNotificationServiceActivatable()
-				&& newOwner.isEmpty()) {
-				return;
-			}
+class NotificationServiceWatcher::Private {
+public:
+	Glib::RefPtr<Gio::DBus::Connection> dbusConnection;
+	uint signalId = 0;
+};
 
-			Core::App().notifications().createManager();
-		});
-	});
+NotificationServiceWatcher::NotificationServiceWatcher()
+: _private(std::make_unique<Private>()) {
+	try {
+		_private->dbusConnection = Gio::DBus::Connection::get_sync(
+			Gio::DBus::BusType::BUS_TYPE_SESSION);
+
+		_private->signalId = base::Platform::DBus::RegisterServiceWatcher(
+			_private->dbusConnection,
+			std::string(kNotificationService),
+			[](
+				const Glib::ustring &service,
+				const Glib::ustring &oldOwner,
+				const Glib::ustring &newOwner) {
+				if (!Core::App().domain().started()
+					|| (IsNotificationServiceActivatable()
+						&& newOwner.empty())) {
+					return;
+				}
+
+				crl::on_main([] {
+					Core::App().notifications().createManager();
+				});
+			});
+	} catch (...) {
+	}
+}
+
+NotificationServiceWatcher::~NotificationServiceWatcher() {
+	if (_private->dbusConnection && _private->signalId != 0) {
+		_private->dbusConnection->signal_unsubscribe(_private->signalId);
+	}
 }
 
 } // namespace internal
