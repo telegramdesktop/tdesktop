b60088335bb326167f0a9a83b28d563123734454
c0re100 <corehusky@gmail.com>
2021-03-05 02:17:10 +0800

    Bump to 2.6.1.2

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -26,4 +26,4 @@ constexpr auto AppVersion = 2006001;
 constexpr auto AppVersionStr = "2.6.1";
 constexpr auto AppBetaVersion = false;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
-constexpr auto AppVersionReleaseStr = "2.6.1.1";
+constexpr auto AppVersionReleaseStr = "2.6.1.2";
