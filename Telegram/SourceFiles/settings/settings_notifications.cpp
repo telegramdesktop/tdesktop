718e0c231d5fa5c99c95417e6a0c3c2aeac640ad
Ilya Fedin <fedin-ilja2010@ya.ru>
2021-04-05 10:17:36 +0400

    Fix build

--- Telegram/SourceFiles/settings/settings_notifications.cpp
+++ Telegram/SourceFiles/settings/settings_notifications.cpp
@@ -20,6 +20,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "platform/platform_specific.h"
 #include "platform/platform_notifications_manager.h"
 #include "base/platform/base_platform_info.h"
+#include "mainwindow.h"
 #include "core/application.h"
 #include "main/main_session.h"
 #include "main/main_account.h"
