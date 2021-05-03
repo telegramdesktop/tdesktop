db6b571f60de9997303d41cff4155ed4d8bbe703
John Preston <johnprestonmail@gmail.com>
2021-05-03 21:42:59 +0400

    Add voice chat admin event log filter in channels.

--- Telegram/SourceFiles/history/admin_log/history_admin_log_filter.cpp
+++ Telegram/SourceFiles/history/admin_log/history_admin_log_filter.cpp
@@ -282,8 +282,8 @@ void FilterBox::Inner::createActionsCheckboxes(const FilterValue &filter) {
 	addFlag(Flag::f_edit, tr::lng_admin_log_filter_messages_edited(tr::now));
 	if (isGroup) {
 		addFlag(Flag::f_pinned, tr::lng_admin_log_filter_messages_pinned(tr::now));
-		addFlag(Flag::f_group_call, tr::lng_admin_log_filter_voice_chats(tr::now));
 	}
+	addFlag(Flag::f_group_call, tr::lng_admin_log_filter_voice_chats(tr::now));
 	addFlag(Flag::f_invites, tr::lng_admin_log_filter_invite_links(tr::now));
 	addFlag(Flag::f_leave, tr::lng_admin_log_filter_members_removed(tr::now));
 }
