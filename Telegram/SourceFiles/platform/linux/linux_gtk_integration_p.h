4348baffc44a2e26c5714142f5c53854ca06fbda
Ilya Fedin <fedin-ilja2010@ya.ru>
2021-06-24 09:07:51 +0400

    Get rid of GTK cast templates

--- Telegram/SourceFiles/platform/linux/linux_gtk_integration_p.h
+++ Telegram/SourceFiles/platform/linux/linux_gtk_integration_p.h
@@ -27,6 +27,7 @@ inline GtkSelectionData* (*gtk_clipboard_wait_for_contents)(GtkClipboard *clipbo
 inline GdkPixbuf* (*gtk_clipboard_wait_for_image)(GtkClipboard *clipboard) = nullptr;
 inline gboolean (*gtk_selection_data_targets_include_image)(const GtkSelectionData *selection_data, gboolean writable) = nullptr;
 inline void (*gtk_selection_data_free)(GtkSelectionData *data) = nullptr;
+inline GType (*gtk_file_chooser_get_type)(void) G_GNUC_CONST = nullptr;
 inline GtkWidget* (*gtk_file_chooser_dialog_new)(const gchar *title, GtkWindow *parent, GtkFileChooserAction action, const gchar *first_button_text, ...) G_GNUC_NULL_TERMINATED = nullptr;
 inline gboolean (*gtk_file_chooser_set_current_folder)(GtkFileChooser *chooser, const gchar *filename) = nullptr;
 inline gchar* (*gtk_file_chooser_get_current_folder)(GtkFileChooser *chooser) = nullptr;
@@ -35,12 +36,15 @@ inline gboolean (*gtk_file_chooser_select_filename)(GtkFileChooser *chooser, con
 inline GSList* (*gtk_file_chooser_get_filenames)(GtkFileChooser *chooser) = nullptr;
 inline void (*gtk_file_chooser_set_filter)(GtkFileChooser *chooser, GtkFileFilter *filter) = nullptr;
 inline GtkFileFilter* (*gtk_file_chooser_get_filter)(GtkFileChooser *chooser) = nullptr;
+inline GType (*gtk_window_get_type)(void) G_GNUC_CONST = nullptr;
 inline void (*gtk_window_set_title)(GtkWindow *window, const gchar *title) = nullptr;
 inline void (*gtk_file_chooser_set_local_only)(GtkFileChooser *chooser, gboolean local_only) = nullptr;
 inline void (*gtk_file_chooser_set_action)(GtkFileChooser *chooser, GtkFileChooserAction action) = nullptr;
 inline void (*gtk_file_chooser_set_select_multiple)(GtkFileChooser *chooser, gboolean select_multiple) = nullptr;
 inline void (*gtk_file_chooser_set_do_overwrite_confirmation)(GtkFileChooser *chooser, gboolean do_overwrite_confirmation) = nullptr;
+inline GType (*gtk_dialog_get_type)(void) G_GNUC_CONST = nullptr;
 inline GtkWidget* (*gtk_dialog_get_widget_for_response)(GtkDialog *dialog, gint response_id) = nullptr;
+inline GType (*gtk_button_get_type)(void) G_GNUC_CONST = nullptr;
 inline void (*gtk_button_set_label)(GtkButton *button, const gchar *label) = nullptr;
 inline void (*gtk_file_chooser_remove_filter)(GtkFileChooser *chooser, GtkFileFilter *filter) = nullptr;
 inline void (*gtk_file_filter_set_name)(GtkFileFilter *filter, const gchar *name) = nullptr;
@@ -50,6 +54,7 @@ inline void (*gtk_file_chooser_set_preview_widget)(GtkFileChooser *chooser, GtkW
 inline gchar* (*gtk_file_chooser_get_preview_filename)(GtkFileChooser *chooser) = nullptr;
 inline void (*gtk_file_chooser_set_preview_widget_active)(GtkFileChooser *chooser, gboolean active) = nullptr;
 inline GtkFileFilter* (*gtk_file_filter_new)(void) = nullptr;
+inline GType (*gtk_image_get_type)(void) G_GNUC_CONST = nullptr;
 inline GtkWidget* (*gtk_image_new)(void) = nullptr;
 inline void (*gtk_image_set_from_pixbuf)(GtkImage *image, GdkPixbuf *pixbuf) = nullptr;
 inline GType (*gtk_app_chooser_get_type)(void) G_GNUC_CONST = nullptr;
@@ -57,53 +62,6 @@ inline GtkWidget* (*gtk_app_chooser_dialog_new)(GtkWindow *parent, GtkDialogFlag
 inline GAppInfo* (*gtk_app_chooser_get_app_info)(GtkAppChooser *self) = nullptr;
 inline void (*gdk_window_set_modal_hint)(GdkWindow *window, gboolean modal) = nullptr;
 inline void (*gdk_window_focus)(GdkWindow *window, guint32 timestamp) = nullptr;
-
-template <typename Result, typename Object>
-inline Result *g_type_cic_helper(Object *instance, GType iface_type) {
-	return reinterpret_cast<Result*>(g_type_check_instance_cast(reinterpret_cast<GTypeInstance*>(instance), iface_type));
-}
-
-inline GType (*gtk_dialog_get_type)(void) G_GNUC_CONST = nullptr;
-template <typename Object>
-inline GtkDialog *gtk_dialog_cast(Object *obj) {
-	return g_type_cic_helper<GtkDialog, Object>(obj, gtk_dialog_get_type());
-}
-
-inline GType (*gtk_file_chooser_get_type)(void) G_GNUC_CONST = nullptr;
-template <typename Object>
-inline GtkFileChooser *gtk_file_chooser_cast(Object *obj) {
-	return g_type_cic_helper<GtkFileChooser, Object>(obj, gtk_file_chooser_get_type());
-}
-
-inline GType (*gtk_image_get_type)(void) G_GNUC_CONST = nullptr;
-template <typename Object>
-inline GtkImage *gtk_image_cast(Object *obj) {
-	return g_type_cic_helper<GtkImage, Object>(obj, gtk_image_get_type());
-}
-
-inline GType (*gtk_button_get_type)(void) G_GNUC_CONST = nullptr;
-template <typename Object>
-inline GtkButton *gtk_button_cast(Object *obj) {
-	return g_type_cic_helper<GtkButton, Object>(obj, gtk_button_get_type());
-}
-
-inline GType (*gtk_window_get_type)(void) G_GNUC_CONST = nullptr;
-template <typename Object>
-inline GtkWindow *gtk_window_cast(Object *obj) {
-	return g_type_cic_helper<GtkWindow, Object>(obj, gtk_window_get_type());
-}
-
-template <typename Object>
-inline bool g_type_cit_helper(Object *instance, GType iface_type) {
-	if (!instance) return false;
-
-	auto ginstance = reinterpret_cast<GTypeInstance*>(instance);
-	if (ginstance->g_class && ginstance->g_class->g_type == iface_type) {
-		return true;
-	}
-	return g_type_check_instance_is_a(ginstance, iface_type);
-}
-
 inline gint (*gtk_dialog_run)(GtkDialog *dialog) = nullptr;
 inline GdkAtom (*gdk_atom_intern)(const gchar *atom_name, gboolean only_if_exists) = nullptr;
 inline GdkDisplay* (*gdk_display_get_default)(void) = nullptr;
