1d81a6dec38a1b48b79ebdeba0200de71fa41620
Ilya Fedin <fedin-ilja2010@ya.ru>
2021-07-03 05:48:35 +0400

    Control GtkOpenWithDialog lifetime from outside

--- Telegram/SourceFiles/platform/linux/linux_gtk_open_with_dialog.h
+++ Telegram/SourceFiles/platform/linux/linux_gtk_open_with_dialog.h
@@ -11,11 +11,29 @@ namespace Platform {
 namespace File {
 namespace internal {
 
-bool ShowGtkOpenWithDialog(
+class GtkOpenWithDialog {
+public:
+	GtkOpenWithDialog(
+		const QString &parent,
+		const QString &filepath);
+
+	~GtkOpenWithDialog();
+
+	[[nodiscard]] rpl::producer<bool> response();
+	[[nodiscard]] rpl::lifetime &lifetime() {
+		return _lifetime;
+	}
+
+private:
+	class Private;
+	const std::unique_ptr<Private> _private;
+
+	rpl::lifetime _lifetime;
+};
+
+[[nodiscard]] std::unique_ptr<GtkOpenWithDialog> CreateGtkOpenWithDialog(
 	const QString &parent,
 	const QString &filepath);
-
-[[nodiscard]] rpl::producer<bool> GtkOpenWithDialogResponse();
 
 } // namespace internal
 } // namespace File
