a9ad09a648e91c3d22710521a2aa7b42d1e5819d
Ilya Fedin <fedin-ilja2010@ya.ru>
2021-07-08 19:48:00 +0400

    Optimize pasting with gtk

--- Telegram/SourceFiles/platform/linux/linux_gtk_integration_p.h
+++ Telegram/SourceFiles/platform/linux/linux_gtk_integration_p.h
@@ -21,18 +21,13 @@ inline void (*gtk_widget_realize)(GtkWidget *widget) = nullptr;
 inline void (*gtk_widget_destroy)(GtkWidget *widget) = nullptr;
 inline GtkClipboard* (*gtk_clipboard_get)(GdkAtom selection) = nullptr;
 inline GtkSelectionData* (*gtk_clipboard_wait_for_contents)(GtkClipboard *clipboard, GdkAtom target) = nullptr;
-inline GdkPixbuf* (*gtk_clipboard_wait_for_image)(GtkClipboard *clipboard) = nullptr;
-inline gboolean (*gtk_selection_data_targets_include_image)(const GtkSelectionData *selection_data, gboolean writable) = nullptr;
+inline const guchar* (*gtk_selection_data_get_data)(const GtkSelectionData *selection_data) = nullptr;
+inline gint (*gtk_selection_data_get_length)(const GtkSelectionData *selection_data) = nullptr;
 inline void (*gtk_selection_data_free)(GtkSelectionData *data) = nullptr;
 inline GType (*gtk_app_chooser_get_type)(void) G_GNUC_CONST = nullptr;
 inline GtkWidget* (*gtk_app_chooser_dialog_new)(GtkWindow *parent, GtkDialogFlags flags, GFile *file) = nullptr;
 inline GAppInfo* (*gtk_app_chooser_get_app_info)(GtkAppChooser *self) = nullptr;
 inline GdkAtom (*gdk_atom_intern)(const gchar *atom_name, gboolean only_if_exists) = nullptr;
-inline gboolean (*gdk_pixbuf_get_has_alpha)(const GdkPixbuf *pixbuf) = nullptr;
-inline guchar* (*gdk_pixbuf_get_pixels)(const GdkPixbuf *pixbuf) = nullptr;
-inline int (*gdk_pixbuf_get_width)(const GdkPixbuf *pixbuf) = nullptr;
-inline int (*gdk_pixbuf_get_height)(const GdkPixbuf *pixbuf) = nullptr;
-inline int (*gdk_pixbuf_get_rowstride)(const GdkPixbuf *pixbuf) = nullptr;
 
 } // namespace Gtk
 } // namespace Platform
