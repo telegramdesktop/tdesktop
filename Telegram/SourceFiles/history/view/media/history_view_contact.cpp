1792bed721f434cbf9d1d5dd5a55d25025cd8789
John Preston <johnprestonmail@gmail.com>
2021-08-18 15:52:04 +0300

    Remove additional padding in shared contacts.

--- Telegram/SourceFiles/history/view/media/history_view_contact.cpp
+++ Telegram/SourceFiles/history/view/media/history_view_contact.cpp
@@ -147,15 +147,6 @@ QSize Contact::countOptimalSize() {
 	accumulate_max(maxWidth, tleft + _name.maxWidth() + tright);
 	accumulate_min(maxWidth, st::msgMaxWidth);
 	auto minHeight = st.padding.top() + st.thumbSize + st.padding.bottom();
-	if (_userId) {
-		const auto msgsigned = item->Get<HistoryMessageSigned>();
-		const auto views = item->Get<HistoryMessageViews>();
-		if ((msgsigned && !msgsigned->isAnonymousRank)
-			|| (views
-				&& (views->views.count >= 0 || views->replies.count > 0))) {
-			minHeight += st::msgDateFont->height - st::msgDateDelta.y();
-		}
-	}
 	if (!isBubbleTop()) {
 		minHeight -= st::msgFileTopMinus;
 	}
