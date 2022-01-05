3f0fed19d8b15eba8601f3e0a04cea93cbffdb15
John Preston <johnprestonmail@gmail.com>
2022-01-05 22:15:17 +0300

    Fix build for macOS.

--- Telegram/SourceFiles/platform/mac/integration_mac.mm
+++ Telegram/SourceFiles/platform/mac/integration_mac.mm
@@ -7,3 +7,18 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #include "platform/mac/integration_mac.h"
 
+#include "platform/platform_integration.h"
+
+namespace Platform {
+namespace {
+
+class MacIntegration final : public Integration {
+};
+
+} // namespace
+
+std::unique_ptr<Integration> CreateIntegration() {
+	return std::make_unique<MacIntegration>();
+}
+
+} // namespace Platform
