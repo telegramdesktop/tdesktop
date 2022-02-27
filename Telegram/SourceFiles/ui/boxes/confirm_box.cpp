1394b1d56f07ff86411678bca33ce3b9487f3842
23rd <23rd@vivaldi.net>
2022-02-27 08:44:02 +0300

    Added ConfirmBox implementation with GenericBox.

--- Telegram/SourceFiles/ui/boxes/confirm_box.cpp
+++ Telegram/SourceFiles/ui/boxes/confirm_box.cpp
@@ -36,7 +36,78 @@ TextParseOptions kMarkedTextBoxOptions = {
 	Qt::LayoutDirectionAuto, // dir
 };
 
+void GenericConfirmBox(not_null<Ui::GenericBox*> box, ConfirmBoxArgs &&args) {
+	const auto weak = Ui::MakeWeak(box);
+	const auto lifetime = box->lifetime().make_state<rpl::lifetime>();
+
+	const auto label = box->addRow(
+		object_ptr<Ui::FlatLabel>(
+			box.get(),
+			v::text::take_marked(std::move(args.text)),
+			args.labelStyle ? *args.labelStyle : st::boxLabel),
+		st::boxPadding);
+
+	const auto prepareCallback = [&](ConfirmBoxArgs::Callback &callback) {
+		return [=, confirmed = std::move(callback)]() {
+			if (const auto callbackPtr = std::get_if<1>(&confirmed)) {
+				if (auto callback = (*callbackPtr)) {
+					callback();
+				}
+			} else if (const auto callbackPtr = std::get_if<2>(&confirmed)) {
+				if (auto callback = (*callbackPtr)) {
+					callback(crl::guard(weak, [=] { weak->closeBox(); }));
+				}
+			} else if (weak) {
+				weak->closeBox();
+			}
+		};
+	};
+
+	const auto &defaultButtonStyle = box->getDelegate()->style().button;
+
+	box->addButton(
+		v::text::take_plain(std::move(args.confirmText), tr::lng_box_ok()),
+		[=, c = prepareCallback(args.confirmed)]() {
+			lifetime->destroy();
+			c();
+		},
+		args.confirmStyle ? *args.confirmStyle : defaultButtonStyle);
+
+	if (!args.inform) {
+		const auto cancelButton = box->addButton(
+			v::text::take_plain(std::move(args.cancelText), tr::lng_cancel()),
+			crl::guard(weak, [=, c = prepareCallback(args.cancelled)]() {
+				lifetime->destroy();
+				c();
+			}),
+			args.cancelStyle ? *args.cancelStyle : defaultButtonStyle);
+
+		box->boxClosing(
+		) | rpl::start_with_next(crl::guard(cancelButton, [=] {
+			cancelButton->clicked(Qt::KeyboardModifiers(), Qt::LeftButton);
+		}), *lifetime);
+	}
+
+	if (args.labelFilter) {
+		label->setClickHandlerFilter(std::move(args.labelFilter));
+	}
+	if (args.strictCancel) {
+		lifetime->destroy();
+	}
+}
+
 } // namespace
+
+object_ptr<Ui::GenericBox> MakeConfirmBox(ConfirmBoxArgs &&args) {
+	return Box(GenericConfirmBox, std::move(args));
+}
+
+object_ptr<Ui::GenericBox> MakeInformBox(v::text::data text) {
+	return MakeConfirmBox({
+		.text = std::move(text),
+		.inform = true,
+	});
+}
 
 ConfirmBox::ConfirmBox(
 	QWidget*,
--- Telegram/SourceFiles/ui/boxes/confirm_box.h
+++ Telegram/SourceFiles/ui/boxes/confirm_box.h
@@ -8,8 +8,39 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #pragma once
 
 #include "boxes/abstract_box.h"
+#include "ui/layers/generic_box.h"
+#include "ui/text/text_variant.h"
 
 namespace Ui {
+
+struct ConfirmBoxArgs {
+	using Callback = std::variant<
+		v::null_t,
+		Fn<void()>,
+		Fn<void(Fn<void()>)>>;
+
+	v::text::data text;
+	Callback confirmed = v::null;
+	Callback cancelled = v::null;
+
+	v::text::data confirmText;
+	v::text::data cancelText;
+
+	const style::RoundButton *confirmStyle = nullptr;
+	const style::RoundButton *cancelStyle = nullptr;
+
+	const style::FlatLabel *labelStyle = nullptr;
+	Fn<bool(const ClickHandlerPtr&, Qt::MouseButton)> labelFilter;
+
+	bool inform = false;
+	// If strict cancel is set the cancel.callback() is only called
+	// if the cancel button was pressed.
+	bool strictCancel = false;
+};
+
+[[nodiscard]] object_ptr<Ui::GenericBox> MakeConfirmBox(
+	ConfirmBoxArgs &&args);
+[[nodiscard]] object_ptr<Ui::GenericBox> MakeInformBox(v::text::data text);
 
 class InformBox;
 class ConfirmBox : public Ui::BoxContent, public ClickHandlerHost {
