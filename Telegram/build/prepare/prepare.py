a2ac9e10e21c74efd61e00f2e2583525f0206ac9
John Preston <johnprestonmail@gmail.com>
2022-03-14 17:25:14 +0400

    Fix camera capture DCHECKs in tg_owt on Windows.

--- Telegram/build/prepare/prepare.py
+++ Telegram/build/prepare/prepare.py
@@ -1238,7 +1238,7 @@ mac:
 stage('tg_owt', """
     git clone https://github.com/desktop-app/tg_owt.git
     cd tg_owt
-    git checkout 8d9c724c07
+    git checkout 1fe5e68d99
     git submodule init
     git submodule update src/third_party/libyuv
 win:
