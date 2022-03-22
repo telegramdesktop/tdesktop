aabf68dc86d816666ece36413347b4c5bbf832f4
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-03-23 03:04:17 +0400

    Replace setMinimumWidth with updateMinimumSize in Window::MainWindow
    There's no RpWindow::setMinimumWidth

--- Telegram/SourceFiles/window/main_window.cpp
+++ Telegram/SourceFiles/window/main_window.cpp
@@ -941,7 +941,7 @@ void MainWindow::showRightColumn(object_ptr<TWidget> widget) {
 	const auto nowMinimumWidth = computeMinWidth();
 	const auto firstResize = (nowMinimumWidth < wasMinimumWidth);
 	if (firstResize) {
-		setMinimumWidth(nowMinimumWidth);
+		updateMinimumSize();
 	}
 	if (!isMaximized()) {
 		tryToExtendWidthBy(wasWidth + nowRightWidth - wasRightWidth - width());
@@ -949,7 +949,7 @@ void MainWindow::showRightColumn(object_ptr<TWidget> widget) {
 		updateControlsGeometry();
 	}
 	if (!firstResize) {
-		setMinimumWidth(nowMinimumWidth);
+		updateMinimumSize();
 	}
 }
 
