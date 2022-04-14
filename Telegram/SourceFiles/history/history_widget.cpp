2996cbc518609f865a0f77e2137e4f95f1da79fe
John Preston <johnprestonmail@gmail.com>
2022-04-14 15:22:10 +0400

    Fix bot menu button on voice recording.

--- Telegram/SourceFiles/history/history_widget.cpp
+++ Telegram/SourceFiles/history/history_widget.cpp
@@ -4464,6 +4464,8 @@ bool HistoryWidget::updateCmdStartShown() {
 				? tr::lng_bot_menu_button()
 				: rpl::single(_botMenuButtonText)),
 			st::historyBotMenuButton);
+		orderWidgets();
+
 		_botMenuButton->setTextTransform(
 			Ui::RoundButton::TextTransform::NoTransform);
 		_botMenuButton->setFullRadius(true);
