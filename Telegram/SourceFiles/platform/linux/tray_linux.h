f67c3bbf65fea2aced1f7012d7ff49bffd00c343
23rd <23rd@vivaldi.net>
2022-04-21 01:24:26 +0300

    Added placeholders for platform dependent implementations of tray.

--- Telegram/SourceFiles/platform/linux/tray_linux.h
+++ Telegram/SourceFiles/platform/linux/tray_linux.h
@@ -15,6 +15,26 @@ class Tray final {
 public:
 	Tray();
 
+	[[nodiscard]] rpl::producer<> aboutToShowRequests() const;
+	[[nodiscard]] rpl::producer<> showFromTrayRequests() const;
+	[[nodiscard]] rpl::producer<> hideToTrayRequests() const;
+	[[nodiscard]] rpl::producer<> iconClicks() const;
+
+	void createIcon();
+	void destroyIcon();
+
+	void updateIcon();
+
+	void createMenu();
+	void destroyMenu();
+
+	void addAction(rpl::producer<QString> text, Fn<void()> &&callback);
+
+	void showTrayMessage() const;
+	[[nodiscard]] bool hasTrayMessageSupport() const;
+
+	[[nodiscard]] rpl::lifetime &lifetime();
+
 private:
 
 };
