1478de3c3d4b74e32829380f7931e663fda283a0
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-06-21 22:01:40 +0400

    Restore missed QWindow::setScreen

--- Telegram/SourceFiles/media/view/media_view_pip.cpp
+++ Telegram/SourceFiles/media/view/media_view_pip.cpp
@@ -475,7 +475,7 @@ void PipPanel::setPositionDefault() {
 	const auto parentScreen = widgetScreen(_parent);
 	const auto myScreen = widgetScreen(widget());
 	if (parentScreen && myScreen && myScreen != parentScreen) {
-		widget()->setScreen(parentScreen);
+		widget()->windowHandle()->setScreen(parentScreen);
 	}
 	auto position = Position();
 	position.snapped = RectPart::Top | RectPart::Left;
