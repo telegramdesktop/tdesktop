2f92830f6a4106ec234b36f597993586f4c206f5
John Preston <johnprestonmail@gmail.com>
2022-06-23 09:31:32 +0400

    Use correct title in premium sticker toast.

--- Telegram/SourceFiles/history/view/history_view_sticker_toast.cpp
+++ Telegram/SourceFiles/history/view/history_view_sticker_toast.cpp
@@ -12,12 +12,14 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "ui/widgets/buttons.h"
 #include "data/data_document.h"
 #include "data/data_document_media.h"
+#include "data/data_session.h"
 #include "main/main_session.h"
 #include "lang/lang_keys.h"
 #include "ui/text/text_utilities.h"
 #include "boxes/sticker_set_box.h"
 #include "lottie/lottie_single_player.h"
 #include "window/window_session_controller.h"
+#include "apiwrap.h"
 #include "styles/style_chat.h"
 
 namespace HistoryView {
@@ -36,7 +38,9 @@ StickerToast::StickerToast(
 , _destroy(std::move(destroy)) {
 }
 
-StickerToast::~StickerToast() = default;
+StickerToast::~StickerToast() {
+	cancelRequest();
+}
 
 void StickerToast::showFor(not_null<DocumentData*> document) {
 	const auto sticker = document->sticker();
@@ -49,11 +53,76 @@ void StickerToast::showFor(not_null<DocumentData*> document) {
 			return;
 		}
 		strong->hideAnimated();
+	} else if (_setRequestId) {
+		if (_for == document) {
+			return;
+		}
+		cancelRequest();
 	}
 	_for = document;
 
+	const auto title = lookupTitle();
+	if (!title.isEmpty()) {
+		showWithTitle(title);
+	} else {
+		requestSet();
+	}
+}
+
+QString StickerToast::lookupTitle() const {
+	Expects(_for != nullptr);
+
+	const auto sticker = _for->sticker();
+	if (!sticker) {
+		return {};
+	}
+
+	const auto id = sticker->set.id;
+	if (!id) {
+		return {};
+	}
+
+	const auto &sets = _for->owner().stickers().sets();
+	const auto i = sets.find(id);
+	if (i == end(sets)) {
+		return {};
+	}
+	return i->second->title;
+}
+
+void StickerToast::requestSet() {
+	Expects(_for != nullptr);
+
+	if (const auto sticker = _for->sticker()) {
+		const auto api = &_controller->session().api();
+		_setRequestId = api->request(MTPmessages_GetStickerSet(
+			Data::InputStickerSet(sticker->set),
+			MTP_int(0) // hash
+		)).done([=](const MTPmessages_StickerSet &result) {
+			_setRequestId = 0;
+			result.match([&](const MTPDmessages_stickerSet &data) {
+				data.vset().match([&](const MTPDstickerSet &data) {
+					const auto owner = &_controller->session().data();
+					showWithTitle(owner->stickers().getSetTitle(data));
+				});
+			}, [&](const MTPDmessages_stickerSetNotModified &) {
+				LOG(("API Error: Got messages.stickerSetNotModified."));
+			});
+		}).fail([=] {
+			_setRequestId = 0;
+		}).send();
+	}
+}
+
+void StickerToast::cancelRequest() {
+	_controller->session().api().request(base::take(_setRequestId)).cancel();
+}
+
+void StickerToast::showWithTitle(const QString &title) {
+	Expects(_for != nullptr);
+
 	const auto text = Ui::Text::Bold(
-		tr::lng_sticker_premium_title(tr::now)
+		title
 	).append('\n').append(
 		tr::lng_sticker_premium_text(tr::now)
 	);
@@ -98,8 +167,8 @@ void StickerToast::showFor(not_null<DocumentData*> document) {
 	preview->resize(size, size);
 	preview->show();
 
-	const auto bytes = document->createMediaView()->bytes();
-	const auto filepath = document->filepath();
+	const auto bytes = _for->createMediaView()->bytes();
+	const auto filepath = _for->filepath();
 	const auto player = preview->lifetime().make_state<Lottie::SinglePlayer>(
 		Lottie::ReadContent(bytes, filepath),
 		Lottie::FrameRequest{ QSize(size, size) },
@@ -122,7 +191,7 @@ void StickerToast::showFor(not_null<DocumentData*> document) {
 
 	button->setClickedCallback([=, weak = _weak] {
 		_controller->show(
-			Box<StickerSetBox>(_controller, document->sticker()->set),
+			Box<StickerSetBox>(_controller, _for->sticker()->set),
 			Ui::LayerOption::KeepOther);
 		if (const auto strong = weak.get()) {
 			strong->hideAnimated();
