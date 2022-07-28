5d4125307f99076575dc6d4b6ee5da41c5b20a8c
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-07-28 07:33:04 +0400

    Default to Wayland on GNOME for new installs

--- Telegram/SourceFiles/core/launcher.cpp
+++ Telegram/SourceFiles/core/launcher.cpp
@@ -9,7 +9,6 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 #include "platform/platform_launcher.h"
 #include "platform/platform_specific.h"
-#include "platform/linux/linux_desktop_environment.h"
 #include "base/platform/base_platform_info.h"
 #include "base/platform/base_platform_file_utilities.h"
 #include "ui/main_queue_processor.h"
@@ -62,7 +61,9 @@ FilteredCommandLineArguments::FilteredCommandLineArguments(
 #endif // !Q_OS_WIN
 	}
 #elif defined Q_OS_UNIX
-	if (Platform::DesktopEnvironment::IsGnome() && qEnvironmentVariableIsEmpty("QT_QPA_PLATFORM")) {
+	if (QFile::exists(cWorkingDir() + qsl("tdata/nowayland"))
+		&& qEnvironmentVariableIsEmpty("QT_QPA_PLATFORM")) {
+		LOG(("Wayland: Disable on old installations"));
 		pushArgument("-platform");
 		pushArgument("xcb;wayland");
 	}
