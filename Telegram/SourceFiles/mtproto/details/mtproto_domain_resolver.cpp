07232e252083bd6ff99941ac7d461456b0a65cca
GitHub Action <action@github.com>
2022-08-01 00:29:41 +0000

    Update User-Agent for DNS to Chrome 103.0.5060.134.

--- Telegram/SourceFiles/mtproto/details/mtproto_domain_resolver.cpp
+++ Telegram/SourceFiles/mtproto/details/mtproto_domain_resolver.cpp
@@ -65,7 +65,7 @@ QByteArray DnsUserAgent() {
 	static const auto kResult = QByteArray(
 		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
 		"AppleWebKit/537.36 (KHTML, like Gecko) "
-		"Chrome/103.0.5060.53 Safari/537.36");
+		"Chrome/103.0.5060.134 Safari/537.36");
 	return kResult;
 }
 
