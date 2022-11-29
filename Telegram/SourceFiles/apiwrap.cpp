7866013ab61bbdcb06140cdb1c457f0373754f10
John Preston <johnprestonmail@gmail.com>
2022-11-29 17:32:38 +0400

    Clear draft that failed to be saved.

--- Telegram/SourceFiles/apiwrap.cpp
+++ Telegram/SourceFiles/apiwrap.cpp
@@ -2134,6 +2134,7 @@ void ApiWrap::saveDraftsToCloud() {
 			if (const auto cloudDraft = history->cloudDraft(topicRootId)) {
 				if (cloudDraft->saveRequestId == requestId) {
 					history->clearCloudDraft(topicRootId);
+					history->applyCloudDraft(topicRootId);
 				}
 			}
 			const auto i = _draftsSaveRequestIds.find(weak);
