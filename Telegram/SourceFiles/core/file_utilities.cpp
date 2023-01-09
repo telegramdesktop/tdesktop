1326359745b4e458c8058876fc0a875d2996eab4
Ilya Fedin <fedin-ilja2010@ya.ru>
2023-01-10 03:57:54 +0400

    Don't involve locale in filename timestamp generation
    It was batch replaced as part of 7b5781b84573ba02cf968b093fa37f23c6b4cb3f, but it's not really semantically valid

--- Telegram/SourceFiles/core/file_utilities.cpp
+++ Telegram/SourceFiles/core/file_utilities.cpp
@@ -76,7 +76,7 @@ QString filedialogDefaultName(
 	QString base;
 	if (fileTime) {
 		const auto date = base::unixtime::parse(fileTime);
-		base = prefix + QLocale().toString(date, "_yyyy-MM-dd_HH-mm-ss");
+		base = prefix + date.toString("_yyyy-MM-dd_HH-mm-ss");
 	} else {
 		struct tm tm;
 		time_t t = time(NULL);
