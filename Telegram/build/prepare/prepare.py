0a3d31a91f81bbf506a54d662fc3c30893571346
John Preston <johnprestonmail@gmail.com>
2023-01-10 15:34:33 +0400

    Beta version 4.5.4: Try updating OpenAL on macOS.

--- Telegram/build/prepare/prepare.py
+++ Telegram/build/prepare/prepare.py
@@ -1051,7 +1051,7 @@ release:
 mac:
     git clone https://github.com/kcat/openal-soft.git
     cd openal-soft
-    git checkout 1.22.2
+    git checkout 716f5373cb
     CFLAGS=$UNGUARDED CPPFLAGS=$UNGUARDED cmake -B build . \\
         -D CMAKE_INSTALL_PREFIX:PATH=$USED_PREFIX \\
         -D ALSOFT_EXAMPLES=OFF \\
