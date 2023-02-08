84b4ab1c3c5ea6582a4640bfee3ecccd8d68c98d
John Preston <johnprestonmail@gmail.com>
2023-02-08 13:19:52 +0400

    Version 4.6.2: Re-enable global /LTCG on Windows.
    With /LTCGOUT: empty path it should not try generating .iobj file,
    and work on both 32 bit and 64 bit build with all static libs.

--- Telegram/cmake/td_ui.cmake
+++ Telegram/cmake/td_ui.cmake
@@ -4,8 +4,8 @@
 # For license and copyright information please follow this link:
 # https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
-add_library(td_ui STATIC)
-init_non_host_target(td_ui ltcg)
+add_library(td_ui OBJECT)
+init_non_host_target(td_ui)
 add_library(tdesktop::td_ui ALIAS td_ui)
 
 include(lib_ui/cmake/generate_styles.cmake)
