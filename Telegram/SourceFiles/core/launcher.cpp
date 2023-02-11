7c73276e7eb0a48e0bd0064f09dce9bc71e5af8f
Ilya Fedin <fedin-ilja2010@ya.ru>
2023-02-11 07:37:11 +0400

    Set QT_DPI_ADJUSTMENT_POLICY for older Qt to the new default value

--- Telegram/SourceFiles/core/launcher.cpp
+++ Telegram/SourceFiles/core/launcher.cpp
@@ -320,6 +320,10 @@ void Launcher::init() {
 }
 
 void Launcher::initHighDpi() {
+#if QT_VERSION < QT_VERSION_CHECK(6, 2, 0)
+	qputenv("QT_DPI_ADJUSTMENT_POLICY", "AdjustDpi");
+#endif // Qt < 6.2.0
+
 	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
 	if (OptionFractionalScalingEnabled.value()) {
 		QApplication::setHighDpiScaleFactorRoundingPolicy(
