83ea3a4b21049a7c90430cee6328f60a1805949d
John Preston <johnprestonmail@gmail.com>
2023-03-10 10:05:05 +0400

    Fix build scripts on Windows.

--- Telegram/build/prepare/prepare.py
+++ Telegram/build/prepare/prepare.py
@@ -404,7 +404,7 @@ if customRunCommand:
 stage('patches', """
     git clone https://github.com/desktop-app/patches.git
     cd patches
-    git checkout 73f5d4762f
+    git checkout 4b35c02c5e
 """)
 
 stage('msys64', """
