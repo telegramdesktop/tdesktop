telegramdesktop/tdesktop

commit af51307aa61a2d1eb9aecd0f611478cf17ee0768

Implement opening of t.me/bot/app-s.

Telegram/SourceFiles/core/local_url_handlers.cpp
@@ -373,6 +373,8 @@ bool ResolveUsernameOrPhone(
 	if (const auto postId = postParam.toInt()) {
 		post = postId;
 	}
+	const auto appname = params.value(u"appname"_q);
+	const auto appstart = params.value(u"startapp"_q);
 	const auto commentParam = params.value(u"comment"_q);
 	const auto commentId = commentParam.toInt();
 	const auto topicParam = params.value(u"topic"_q);
@@ -383,6 +385,12 @@ bool ResolveUsernameOrPhone(
 	if (!gameParam.isEmpty() && validDomain(gameParam)) {
 		startToken = gameParam;
 		resolveType = ResolveType::ShareGame;
+	}
+	if (startToken.isEmpty() && params.contains(u"startapp"_q)) {
+		startToken = params.value(u"startapp"_q);
+	}
+	if (!appname.isEmpty()) {
+		resolveType = ResolveType::BotApp;
 	}
 	const auto myContext = context.value<ClickHandlerContext>();
 	using Navigation = Window::SessionNavigation;
@@ -403,6 +411,8 @@ bool ResolveUsernameOrPhone(
 		.startToken = startToken,
 		.startAdminRights = adminRights,
 		.startAutoSubmit = myContext.botStartAutoSubmit,
+		.botAppName = appname.isEmpty() ? postParam : appname,
+		.botAppForceConfirmation = myContext.mayShowConfirmation,
 		.attachBotUsername = params.value(u"attach"_q),
 		.attachBotToggleCommand = (params.contains(u"startattach"_q)
 			? params.value(u"startattach"_q)
@@ -1004,6 +1014,7 @@ QString TryConvertUrlToLocal(QString url) {
 			"("
 				"/?\\?|"
 				"/?$|"
+				"/[a-zA-Z0-9\\.\\_]+|"
 				"/\\d+/?(\\?|$)|"
 				"/\\d+/\\d+/?(\\?|$)"
 			")"_q, query, matchOptions)) {
@@ -1014,6 +1025,8 @@ QString TryConvertUrlToLocal(QString url) {
 				added = u"&topic=%1&post=%2"_q.arg(threadPostMatch->captured(1)).arg(threadPostMatch->captured(2));
 			} else if (const auto postMatch = regex_match(u"^/(\\d+)(/?\\?|/?$)"_q, usernameMatch->captured(2))) {
 				added = u"&post="_q + postMatch->captured(1);
+			} else if (const auto appNameMatch = regex_match(u"^/([a-zA-Z0-9\\.\\_]+)(/?\\?|/?$)"_q, usernameMatch->captured(2))) {
+				added = u"&appname="_q + appNameMatch->captured(1);
 			}
 			return base + added + (params.isEmpty() ? QString() : '&' + params);
 		}

Telegram/CMakeLists.txt
@@ -444,6 +444,8 @@ PRIVATE
     data/data_audio_msg_id.h
     data/data_auto_download.cpp
     data/data_auto_download.h
+    data/data_bot_app.cpp
+    data/data_bot_app.h
     data/data_chat.cpp
     data/data_chat.h
     data/data_chat_filters.cpp

Telegram/Resources/langs/lang.strings
@@ -396,6 +396,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 "lng_username_available" = "This username is available.";
 "lng_username_not_found" = "User @{user} not found.";
 "lng_username_by_phone_not_found" = "User {phone} not found.";
+"lng_username_app_not_found" = "Bot application not found.";
 "lng_username_link" = "This link opens a chat with you:";
 "lng_username_copied" = "Link copied to clipboard.";
 
@@ -1463,6 +1464,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 "lng_action_took_screenshot" = "{from} took a screenshot!";
 "lng_action_you_took_screenshot" = "You took a screenshot!";
 "lng_action_bot_allowed_from_domain" = "You allowed this bot to message you when you logged in on {domain}.";
+"lng_action_bot_allowed_from_app" = "You allowed this bot to message you when you opened {app}.";
 "lng_action_secure_values_sent" = "{user} received the following documents: {documents}";
 "lng_action_secure_personal_details" = "personal details";
 "lng_action_secure_proof_of_identity" = "proof of identity";

Telegram/SourceFiles/core/click_handler_types.cpp
@@ -108,7 +108,11 @@ void HiddenUrlClickHandler::Open(QString url, QVariant context) {
 	};
 	if (url.startsWith(u"tg://"_q, Qt::CaseInsensitive)
 		|| url.startsWith(u"internal:"_q, Qt::CaseInsensitive)) {
-		open();
+		UrlClickHandler::Open(url, QVariant::fromValue([&] {
+			auto result = context.value<ClickHandlerContext>();
+			result.mayShowConfirmation = !base::IsCtrlPressed();
+			return result;
+		}()));
 	} else {
 		const auto parsedUrl = QUrl::fromUserInput(url);
 		if (UrlRequiresConfirmation(parsedUrl) && !base::IsCtrlPressed()) {

Telegram/SourceFiles/core/click_handler_types.h
@@ -42,6 +42,7 @@ struct ClickHandlerContext {
 	Fn<HistoryView::ElementDelegate*()> elementDelegate;
 	base::weak_ptr<Window::SessionController> sessionWindow;
 	std::shared_ptr<Ui::Show> show;
+	bool mayShowConfirmation = false;
 	bool skipBotAutoLogin = false;
 	bool botStartAutoSubmit = false;
 	// Is filled from peer info.

Telegram/SourceFiles/data/data_bot_app.cpp
@@ -1,0 +1,13 @@
+/*
+This file is part of Telegram Desktop,
+the official desktop application for the Telegram messaging service.
+
+For license and copyright information please follow this link:
+https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
+*/
+#include "data/data_bot_app.h"
+
+BotAppData::BotAppData(not_null<Data::Session*> owner, const BotAppId &id)
+: owner(owner)
+, id(id) {
+}

Telegram/SourceFiles/data/data_bot_app.h
@@ -10,15 +10,18 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "data/data_photo.h"
 #include "data/data_document.h"
 
-struct GameData {
-	GameData(not_null<Data::Session*> owner, const GameId &id);
+struct BotAppData {
+	BotAppData(not_null<Data::Session*> owner, const BotAppId &id);
 
 	const not_null<Data::Session*> owner;
-	GameId id = 0;
-	uint64 accessHash = 0;
+	BotAppId id = 0;
+	PeerId botId = 0;
 	QString shortName;
 	QString title;
 	QString description;
 	PhotoData *photo = nullptr;
 	DocumentData *document = nullptr;
+
+	uint64 accessHash = 0;
+	uint64 hash = 0;
 };

Telegram/SourceFiles/data/data_session.cpp
@@ -41,6 +41,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "lang/lang_keys.h" // tr::lng_deleted(tr::now) in user name
 #include "data/stickers/data_stickers.h"
 #include "data/notify/data_notify_settings.h"
+#include "data/data_bot_app.h"
 #include "data/data_changes.h"
 #include "data/data_group_call.h"
 #include "data/data_media_types.h"
@@ -3448,6 +3449,45 @@ void Session::gameApplyFields(
 	game->photo = photo;
 	game->document = document;
 	notifyGameUpdateDelayed(game);
+}
+
+not_null<BotAppData*> Session::botApp(BotAppId id) {
+	const auto i = _botApps.find(id);
+	return (i != end(_botApps))
+		? i->second.get()
+		: _botApps.emplace(
+			id,
+			std::make_unique<BotAppData>(this, id)).first->second.get();
+}
+
+BotAppData *Session::findBotApp(PeerId botId, const QString &appName) const {
+	for (const auto &[id, app] : _botApps) {
+		if (app->botId == botId && app->shortName == appName) {
+			return app.get();
+		}
+	}
+	return nullptr;
+}
+
+BotAppData *Session::processBotApp(
+		PeerId botId,
+		const MTPBotApp &data) {
+	return data.match([&](const MTPDbotApp &data) {
+		const auto result = botApp(data.vid().v);
+		result->botId = botId;
+		result->shortName = qs(data.vshort_name());
+		result->title = qs(data.vtitle());
+		result->description = qs(data.vdescription());
+		result->photo = processPhoto(data.vphoto());
+		result->document = data.vdocument()
+			? processDocument(*data.vdocument()).get()
+			: nullptr;
+		result->accessHash = data.vaccess_hash().v;
+		result->hash = data.vhash().v;
+		return result.get();
+	}, [](const MTPDbotAppNotModified &) {
+		return (BotAppData*)nullptr;
+	});
 }
 
 not_null<PollData*> Session::poll(PollId id) {

commit 0b5038aaa2f66c688f8157cd00a549dc8bb30e37

Fix t.me channel comment links opening.
Regression was introduced in af51307aa6.

Telegram/SourceFiles/core/local_url_handlers.cpp
@@ -1034,7 +1034,7 @@ QString TryConvertUrlToLocal(QString url) {
 			"("
 				"/?\\?|"
 				"/?$|"
-				"/[a-zA-Z0-9\\.\\_]+|"
+				"/[a-zA-Z0-9\\.\\_]+/?(\\?|$)|"
 				"/\\d+/?(\\?|$)|"
 				"/\\d+/\\d+/?(\\?|$)"
 			")"_q, query, matchOptions)) {