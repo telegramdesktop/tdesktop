ca863bfb5be32d1ea259b3ebb683fdb84a0ae3a3
23rd <23rd@vivaldi.net>
2023-04-26 23:26:30 +0300

    Added dummy Data class for statistics.

---
+++ Telegram/SourceFiles/data/data_statistics.cpp
@@ -0,0 +1,14 @@
+/*
+This file is part of Telegram Desktop,
+the official desktop application for the Telegram messaging service.
+
+For license and copyright information please follow this link:
+https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
+*/
+#include "data/data_statistics.h"
+
+namespace Data {
+namespace {
+} // namespace
+
+} // namespace Data
