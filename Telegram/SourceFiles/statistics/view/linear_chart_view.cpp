671e81033c96475185333468c1227e185a4d838d
23rd <23rd@vivaldi.net>
2023-08-27 23:47:04 +0300

    Moved files of linear chart view class to directory.

--- Telegram/SourceFiles/statistics/view/linear_chart_view.cpp
+++ Telegram/SourceFiles/statistics/view/linear_chart_view.cpp
@@ -5,7 +5,7 @@ the official desktop application for the Telegram messaging service.
 For license and copyright information please follow this link:
 https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
-#include "statistics/linear_chart_view.h"
+#include "statistics/view/linear_chart_view.h"
 
 #include "data/data_statistics.h"
 #include "statistics/statistics_common.h"
