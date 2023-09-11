06cb16952c96aeb894bfc5f3add9e95e728fe77c
Grazyna <142352140+grazyna-1984@users.noreply.github.com>
2023-09-11 23:44:20 +0100

    remove beta from version

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -30,4 +30,4 @@ constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
 constexpr auto FakeAppVersion = 0100000;
 constexpr auto PTelegramAppVersion = 1005000;
 constexpr auto PTelegramAppVersionStr = "1.5.0";
-constexpr auto PTelegramAppBetaVersion = true;
+constexpr auto PTelegramAppBetaVersion = false;
