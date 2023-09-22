1bde0964175ecf778dbe09de7bfbccf5fe019b1b
Ilya Fedin <fedin-ilja2010@ya.ru>
2023-09-22 04:24:11 +0400

    Minimize amount of code to rebuild when switching options

---
+++ Telegram/SourceFiles/ui/controls/window_outdated_bar_dummy.cpp
@@ -0,0 +1,18 @@
+/*
+This file is part of Telegram Desktop,
+the official desktop application for the Telegram messaging service.
+
+For license and copyright information please follow this link:
+https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
+*/
+#include "ui/controls/window_outdated_bar.h"
+
+namespace Ui {
+
+object_ptr<RpWidget> CreateOutdatedBar(
+		not_null<QWidget*> parent,
+		const QString &workingPath) {
+	return { nullptr };
+}
+
+} // namespace Ui
