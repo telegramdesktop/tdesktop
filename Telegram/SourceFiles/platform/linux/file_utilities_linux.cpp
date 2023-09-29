d558fc4b8d9727d109e4ba420cf1bd6c9569688a
Ilya Fedin <fedin-ilja2010@ya.ru>
2023-09-29 21:59:14 +0400

    Make UnsafeShowOpenWith inline on Linux

--- Telegram/SourceFiles/platform/linux/file_utilities_linux.cpp
+++ Telegram/SourceFiles/platform/linux/file_utilities_linux.cpp
@@ -6,15 +6,3 @@ For license and copyright information please follow this link:
 https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #include "platform/linux/file_utilities_linux.h"
-
-#include "platform/linux/linux_xdp_open_with_dialog.h"
-
-namespace Platform {
-namespace File {
-
-bool UnsafeShowOpenWith(const QString &filepath) {
-	return internal::ShowXDPOpenWithDialog(filepath);
-}
-
-} // namespace File
-} // namespace Platform
--- Telegram/SourceFiles/platform/linux/file_utilities_linux.h
+++ Telegram/SourceFiles/platform/linux/file_utilities_linux.h
@@ -9,6 +9,8 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 #include "platform/platform_file_utilities.h"
 
+#include "platform/linux/linux_xdp_open_with_dialog.h"
+
 namespace Platform {
 namespace File {
 
@@ -26,6 +28,10 @@ inline void UnsafeOpenEmailLink(const QString &email) {
 
 inline bool UnsafeShowOpenWithDropdown(const QString &filepath) {
 	return false;
+}
+
+inline bool UnsafeShowOpenWith(const QString &filepath) {
+	return internal::ShowXDPOpenWithDialog(filepath);
 }
 
 inline void UnsafeLaunch(const QString &filepath) {
