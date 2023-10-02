ca0f8f54f642b82fd8a9dfae1660a2528dbeb17e
Sergey A. Osokin <osa@FreeBSD.org.ru>
2023-10-02 10:44:27 -0400

    Fix build on FreeBSD

--- Telegram/SourceFiles/platform/linux/notifications_manager_linux.cpp
+++ Telegram/SourceFiles/platform/linux/notifications_manager_linux.cpp
@@ -814,7 +814,7 @@ bool ByDefault() {
 
 	// A list of capabilities that offer feature parity
 	// with custom notifications
-	return ranges::all_of(std::initializer_list{
+	return ranges::all_of(std::array{
 		// To show message content
 		"body",
 		// To have buttons on notifications
