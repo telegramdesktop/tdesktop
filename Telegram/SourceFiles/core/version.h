434cf3fec038faf6e8da13de9df1fbfef28bc49f
Kolya <142352140+agl-1984@users.noreply.github.com>
2023-10-30 16:54:57 +0000

    Update version to 1.5.1

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -28,6 +28,6 @@ constexpr auto AppBetaVersion = false;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
 
 constexpr auto FakeAppVersion = 0100000;
-constexpr auto PTelegramAppVersion = 1005000;
-constexpr auto PTelegramAppVersionStr = "1.5.0";
+constexpr auto PTelegramAppVersion = 1005001;
+constexpr auto PTelegramAppVersionStr = "1.5.1";
 constexpr auto PTelegramAppBetaVersion = false;
