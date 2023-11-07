a7090c5fbaf2d47216ab99f9bb0a3331ea2d6daf
John Preston <johnprestonmail@gmail.com>
2023-11-07 08:35:35 +0400

    Fix possible crash in topic replies.

--- Telegram/SourceFiles/history/history_item.cpp
+++ Telegram/SourceFiles/history/history_item.cpp
@@ -3355,6 +3355,7 @@ void HistoryItem::createComponentsHelper(
 			: nullptr;
 		if (!config.reply.externalPeerId
 			&& topic
+			&& to
 			&& topic->rootId() != to->topicRootId()) {
 			config.reply.externalPeerId = replyTo.messageId.peer;
 		}
