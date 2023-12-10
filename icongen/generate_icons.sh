05874b4d3967d0c252ed2e4f2146c90ff7cf9a66
cumdev1337 <cumdev1337@dev.cum>
2023-12-10 06:03:04 +0200

    Icon update

---
+++ icongen/generate_icons.sh
@@ -0,0 +1,28 @@
+ffmpeg -i Cumgram.png -vf scale=1024:1024 icon512@2x.png
+ffmpeg -i Cumgram.png -vf scale=1024:1024 icon_round512@2x.png
+ffmpeg -i Cumgram.png -vf scale=1024:1024 icon_green.png
+
+ffmpeg -i Cumgram.png -vf scale=512:512 icon512.png
+ffmpeg -i Cumgram.png -vf scale=512:512 icon256@2x.png
+
+ffmpeg -i Cumgram.png -vf scale=256:256 logo_256_no_margin.png
+ffmpeg -i Cumgram.png -vf scale=256:256 logo_256.png
+ffmpeg -i Cumgram.png -vf scale=256:256 iconbig_green.png
+ffmpeg -i Cumgram.png -vf scale=256:256 icon256.png
+ffmpeg -i Cumgram.png -vf scale=256:256 icon128@2x.png
+
+ffmpeg -i Cumgram.png -vf scale=128:128 icon128.png
+ffmpeg -i Cumgram.png -vf scale=128:128 icon64@2x.png
+
+ffmpeg -i Cumgram.png -vf scale=96:96 icon48@2x.png
+
+ffmpeg -i Cumgram.png -vf scale=64:64 icon64.png
+ffmpeg -i Cumgram.png -vf scale=64:64 icon32@2x.png
+
+ffmpeg -i Cumgram.png -vf scale=48:48 icon256.ico
+ffmpeg -i Cumgram.png -vf scale=48:48 icon48.png
+
+ffmpeg -i Cumgram.png -vf scale=32:32 icon32.png
+ffmpeg -i Cumgram.png -vf scale=32:32 icon16@2x.png
+
+ffmpeg -i Cumgram.png -vf scale=16:16 icon16.png
---
+++ update.sh
@@ -0,0 +1,2 @@
+git pull
+git submodule update --init --recursive
