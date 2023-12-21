0e8058adb12a2f228de44672c8ed82deafcf5c17
John Preston <johnprestonmail@gmail.com>
2023-12-21 12:24:38 -0400

    Update tg_owt to support custom reflectors.

--- Telegram/build/prepare/prepare.py
+++ Telegram/build/prepare/prepare.py
@@ -1426,7 +1426,7 @@ mac:
 stage('tg_owt', """
     git clone https://github.com/desktop-app/tg_owt.git
     cd tg_owt
-    git checkout 76a3513d7f
+    git checkout afd9d5d317
     git submodule init
     git submodule update
 win:
