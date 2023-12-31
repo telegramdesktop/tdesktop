9787e52f3fc8c321bbc837479a6a91cb4b9331f6
Ilya Fedin <fedin-ilja2010@ya.ru>
2024-01-01 01:49:50 +0400

    Make use of wrongly unused variables

--- Telegram/SourceFiles/storage/localstorage.cpp
+++ Telegram/SourceFiles/storage/localstorage.cpp
@@ -613,7 +613,7 @@ void writeBackground(const Data::WallPaper &paper, const QImage &image) {
 		dst = dst.subspan(sizeof(qint32));
 		bytes::copy(dst, bytes::object_as_span(&height));
 		dst = dst.subspan(sizeof(qint32));
-		const auto src = bytes::make_span(image.constBits(), srcsize);
+		const auto src = bytes::make_span(copy.constBits(), srcsize);
 		if (srcsize == dstsize) {
 			bytes::copy(dst, src);
 		} else {
