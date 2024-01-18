37478d951ac5821418750cda774ba89333818ad5
John Preston <johnprestonmail@gmail.com>
2024-01-18 17:27:30 +0400

    Fix initial geometry for secondary windows.

--- Telegram/SourceFiles/core/application.cpp
+++ Telegram/SourceFiles/core/application.cpp
@@ -1321,7 +1321,7 @@ Window::Controller *Application::ensureSeparateWindowForPeer(
 		std::make_unique<Window::Controller>(peer, showAtMsgId)
 	).first->second.get();
 	processCreatedWindow(result);
-	result->widget()->show();
+	result->firstShow();
 	result->finishFirstShow();
 	return activate(result);
 }
@@ -1341,7 +1341,7 @@ Window::Controller *Application::ensureSeparateWindowForAccount(
 		std::make_unique<Window::Controller>(account)
 	).first->second.get();
 	processCreatedWindow(result);
-	result->widget()->show();
+	result->firstShow();
 	result->finishFirstShow();
 	return activate(result);
 }
