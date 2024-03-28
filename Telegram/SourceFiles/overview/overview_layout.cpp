009a46750d7dc5d23353b7c2538d01ac12ed25fb
Kolya <142352140+agl-1984@users.noreply.github.com>
2024-03-28 06:51:57 -0700

    anti spoof changes

--- Telegram/SourceFiles/overview/overview_layout.cpp
+++ Telegram/SourceFiles/overview/overview_layout.cpp
@@ -1611,7 +1611,7 @@ Link::Link(
 
 	const auto createHandler = [](const QString &url) {
 		return UrlClickHandler::IsSuspicious(url)
-			? std::make_shared<HiddenUrlClickHandler>(url)
+			? std::make_shared<HiddenUrlClickHandler>(url, url)
 			: std::make_shared<UrlClickHandler>(url, false);
 	};
 	_page = media ? media->webpage() : nullptr;
@@ -1941,7 +1941,7 @@ Link::LinkEntry::LinkEntry(const QString &url, const QString &text)
 : text(text)
 , width(st::normalFont->width(text))
 , lnk(UrlClickHandler::IsSuspicious(url)
-	? std::make_shared<HiddenUrlClickHandler>(url)
+	? std::make_shared<HiddenUrlClickHandler>(url, text)
 	: std::make_shared<UrlClickHandler>(url)) {
 }
 
