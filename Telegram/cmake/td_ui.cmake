68df8448a2fe9a915f7e268190921428065eeb5d
John Preston <johnprestonmail@gmail.com>
2024-05-03 11:50:44 +0400

    Use arc_angles.h from lib_ui.

--- Telegram/cmake/td_ui.cmake
+++ Telegram/cmake/td_ui.cmake
@@ -396,7 +396,6 @@ PRIVATE
     ui/widgets/vertical_drum_picker.cpp
     ui/widgets/vertical_drum_picker.h
 
-    ui/arc_angles.h
     ui/cached_round_corners.cpp
     ui/cached_round_corners.h
     ui/color_contrast.cpp
