bcb6e9e1af2e56d9d7b5e205e0b716fd4d9c8441
23rd <23rd@vivaldi.net>
2024-07-17 12:52:40 +0300

    Removed unused include directives from settings_common_session.

--- Telegram/SourceFiles/settings/settings_common_session.cpp
+++ Telegram/SourceFiles/settings/settings_common_session.cpp
@@ -7,30 +7,9 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #include "settings/settings_common_session.h"
 
-#include "api/api_cloud_password.h"
-#include "apiwrap.h"
-#include "core/application.h"
-#include "core/core_cloud_password.h"
-#include "lang/lang_keys.h"
-#include "main/main_domain.h"
-#include "main/main_session.h"
 #include "settings/cloud_password/settings_cloud_password_email_confirm.h"
-#include "settings/settings_advanced.h"
-#include "settings/settings_calls.h"
 #include "settings/settings_chat.h"
-#include "settings/settings_experimental.h"
-#include "settings/settings_folders.h"
-#include "settings/settings_information.h"
 #include "settings/settings_main.h"
-#include "settings/settings_notifications.h"
-#include "settings/settings_privacy_security.h"
-#include "ui/widgets/menu/menu_add_action_callback.h"
-#include "window/themes/window_theme_editor_box.h"
-#include "window/window_controller.h"
-#include "window/window_session_controller.h"
-#include "styles/style_menu_icons.h"
-
-#include <QAction>
 
 namespace Settings {
 
