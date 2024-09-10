2cfe255abb3640beffc0ca05272be8b61d4d67f8
Ilya Fedin <fedin-ilja2010@ya.ru>
2024-09-10 16:21:37 +0400

    Revert "Fullscreen hack for Windows 11 native resize"
    This reverts commit f7bff01793b622fc7f017fdd102ffbb149c19a83.

--- Telegram/SourceFiles/media/view/media_view_overlay_widget.cpp
+++ Telegram/SourceFiles/media/view/media_view_overlay_widget.cpp
@@ -981,7 +981,7 @@ void OverlayWidget::savePosition() {
 
 void OverlayWidget::updateGeometry(bool inMove) {
 	initFullScreen();
-	if (_fullscreen && (!Platform::IsWindows11OrGreater() || !isHidden())) {
+	if (_fullscreen) {
 		updateGeometryToScreen(inMove);
 	} else if (_windowed && _normalGeometryInited) {
 		DEBUG_LOG(("Viewer Pos: Setting %1, %2, %3, %4")
@@ -3786,9 +3786,6 @@ void OverlayWidget::showAndActivate() {
 		_wasWindowedMode = true;
 	} else if (_fullscreen) {
 		_window->showFullScreen();
-		if (Platform::IsWindows11OrGreater()) {
-			updateGeometry();
-		}
 	} else {
 		_window->showMaximized();
 	}
