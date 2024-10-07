3d98ebff42ae5a13b6651270d865c66e9a697537
John Preston <johnprestonmail@gmail.com>
2024-10-07 13:22:43 +0400

    Hide layer when showing topics list.

--- Telegram/SourceFiles/mainwidget.cpp
+++ Telegram/SourceFiles/mainwidget.cpp
@@ -1538,7 +1538,7 @@ void MainWidget::showForum(
 	_dialogs->showForum(forum, params);
 
 	if (params.activation != anim::activation::background) {
-		_controller->hideLayer();
+		_controller->window().hideSettingsAndLayer();
 	}
 }
 
