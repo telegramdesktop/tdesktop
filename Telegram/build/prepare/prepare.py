0ce4225d45ee48fb389252b9150e75cb3da8f6e7
Ilya Fedin <fedin-ilja2010@ya.ru>
2024-10-08 17:53:50 +0400

    Qt 6.8.0-rc1 -> 6.8.0

--- Telegram/build/prepare/prepare.py
+++ Telegram/build/prepare/prepare.py
@@ -1629,7 +1629,7 @@ mac:
     make install
 """)
 else: # qt > '6'
-    branch = 'v$QT' + ('-lts-lgpl' if qt < '6.3' else '-rc1')
+    branch = 'v$QT' + ('-lts-lgpl' if qt < '6.3' else '')
     stage('qt_' + qt, """
     git clone -b """ + branch + """ https://github.com/qt/qt5.git qt_$QT
     cd qt_$QT
