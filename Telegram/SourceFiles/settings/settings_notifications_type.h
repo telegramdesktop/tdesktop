979973745b58ba1e582ad60e170b1231e5865cbe
23rd <23rd@vivaldi.net>
2024-12-01 14:48:32 +0300

    Fixed build of notifications type included with another Session declare.

--- Telegram/SourceFiles/settings/settings_notifications_type.h
+++ Telegram/SourceFiles/settings/settings_notifications_type.h
@@ -10,10 +10,6 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "settings/settings_common_session.h"
 #include "data/notify/data_notify_settings.h"
 
-namespace Data {
-enum class DefaultNotify;
-} // namespace Data
-
 namespace Settings {
 
 class NotificationsType : public AbstractSection {
@@ -39,11 +35,11 @@ private:
 };
 
 [[nodiscard]] bool NotificationsEnabledForType(
-	not_null<Main::Session*> session,
+	not_null<::Main::Session*> session,
 	Data::DefaultNotify type);
 
 [[nodiscard]] rpl::producer<bool> NotificationsEnabledForTypeValue(
-	not_null<Main::Session*> session,
+	not_null<::Main::Session*> session,
 	Data::DefaultNotify type);
 
 } // namespace Settings
