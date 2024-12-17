telegramdesktop/tdesktop

commit 2d1fb0562db32b64e762407b2ea801ce025c711f

Prepared messages sharing from miniapp.

Telegram/SourceFiles/inline_bots/inline_bot_send_data.cpp
@@ -28,7 +28,7 @@ QString SendData::getLayoutDescription(const Result *owner) const {
 	return owner->_description;
 }
 
-void SendDataCommon::addToHistory(
+not_null<HistoryItem*> SendDataCommon::makeMessage(
 		const Result *owner,
 		not_null<History*> history,
 		HistoryItemCommonFields &&fields) const {
@@ -36,7 +36,7 @@ void SendDataCommon::addToHistory(
 	if (fields.replyTo) {
 		fields.flags |= MessageFlag::HasReplyInfo;
 	}
-	history->addNewLocalMessage(
+	return history->makeMessage(
 		std::move(fields),
 		std::move(distinct.text),
 		std::move(distinct.media));
@@ -96,14 +96,14 @@ QString SendContact::getLayoutDescription(const Result *owner) const {
 	return result;
 }
 
-void SendPhoto::addToHistory(
+not_null<HistoryItem*> SendPhoto::makeMessage(
 		const Result *owner,
 		not_null<History*> history,
 		HistoryItemCommonFields &&fields) const {
-	history->addNewLocalMessage(
+	return history->makeMessage(
 		std::move(fields),
 		_photo,
-		{ _message, _entities });
+		TextWithEntities{ _message, _entities });
 }
 
 QString SendPhoto::getErrorOnSend(
@@ -113,14 +113,14 @@ QString SendPhoto::getErrorOnSend(
 	return Data::RestrictionError(history->peer, type).value_or(QString());
 }
 
-void SendFile::addToHistory(
+not_null<HistoryItem*> SendFile::makeMessage(
 		const Result *owner,
 		not_null<History*> history,
 		HistoryItemCommonFields &&fields) const {
-	history->addNewLocalMessage(
+	return history->makeMessage(
 		std::move(fields),
 		_document,
-		{ _message, _entities });
+		TextWithEntities{ _message, _entities });
 }
 
 QString SendFile::getErrorOnSend(
@@ -130,11 +130,11 @@ QString SendFile::getErrorOnSend(
 	return Data::RestrictionError(history->peer, type).value_or(QString());
 }
 
-void SendGame::addToHistory(
+not_null<HistoryItem*> SendGame::makeMessage(
 		const Result *owner,
 		not_null<History*> history,
 		HistoryItemCommonFields &&fields) const {
-	history->addNewLocalMessage(std::move(fields), _game);
+	return history->addNewLocalMessage(std::move(fields), _game);
 }
 
 QString SendGame::getErrorOnSend(

Telegram/CMakeLists.txt
@@ -1040,6 +1040,8 @@ PRIVATE
     info/info_wrap_widget.h
     inline_bots/bot_attach_web_view.cpp
     inline_bots/bot_attach_web_view.h
+    inline_bots/inline_bot_confirm_prepared.cpp
+    inline_bots/inline_bot_confirm_prepared.h
     inline_bots/inline_bot_layout_internal.cpp
     inline_bots/inline_bot_layout_internal.h
     inline_bots/inline_bot_layout_item.cpp

Telegram/Resources/langs/lang.strings
@@ -3425,6 +3425,9 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 "lng_bot_emoji_status_text" = "Do you want to set this emoji status suggested by {bot}?";
 "lng_bot_emoji_status_access_text" = "{bot} requests access to set your **emoji status**. You will be able to revoke this access in the profile page of {name}.";
 "lng_bot_emoji_status_access_allow" = "Allow";
+"lng_bot_share_prepared_title" = "Share Message";
+"lng_bot_share_prepared_about" = "{bot} mini app suggests you to send this message to a chat you select.";
+"lng_bot_share_prepared_button" = "Share With...";
 
 "lng_bot_status_users#one" = "{count} monthly user";
 "lng_bot_status_users#other" = "{count} monthly users";

Telegram/SourceFiles/apiwrap.cpp
@@ -3947,7 +3947,8 @@ void ApiWrap::sendInlineResult(
 		not_null<UserData*> bot,
 		not_null<InlineBots::Result*> data,
 		const SendAction &action,
-		std::optional<MsgId> localMessageId) {
+		std::optional<MsgId> localMessageId,
+		Fn<void(bool)> done) {
 	sendAction(action);
 
 	const auto history = action.history;
@@ -4027,11 +4028,13 @@ void ApiWrap::sendInlineResult(
 		history->finishSavingCloudDraft(
 			topicRootId,
 			UnixtimeFromMsgId(response.outerMsgId));
+		done(true);
 	}, [=](const MTP::Error &error, const MTP::Response &response) {
 		sendMessageFail(error, peer, randomId, newId);
 		history->finishSavingCloudDraft(
 			topicRootId,
 			UnixtimeFromMsgId(response.outerMsgId));
+		done(false);
 	});
 	finishForwarding(action);
 }

Telegram/SourceFiles/apiwrap.h
@@ -361,7 +361,8 @@ public:
 		not_null<UserData*> bot,
 		not_null<InlineBots::Result*> data,
 		const SendAction &action,
-		std::optional<MsgId> localMessageId);
+		std::optional<MsgId> localMessageId,
+		Fn<void(bool)> done = nullptr);
 	void sendMessageFail(
 		const MTP::Error &error,
 		not_null<PeerData*> peer,

Telegram/SourceFiles/history/history.cpp
@@ -712,6 +712,14 @@ not_null<HistoryItem*> History::addNewLocalMessage(
 		true);
 }
 
+not_null<HistoryItem*> History::addNewLocalMessage(
+		not_null<HistoryItem*> item) {
+	Expects(item->history() == this);
+	Expects(item->isLocal());
+
+	return addNewItem(item, true);
+}
+
 not_null<HistoryItem*> History::addSponsoredMessage(
 		MsgId id,
 		Data::SponsoredFrom from,

Telegram/SourceFiles/history/history.h
@@ -180,6 +180,7 @@ public:
 	not_null<HistoryItem*> addNewLocalMessage(
 		HistoryItemCommonFields &&fields,
 		not_null<GameData*> game);
+	not_null<HistoryItem*> addNewLocalMessage(not_null<HistoryItem*> item);
 
 	not_null<HistoryItem*> addSponsoredMessage(
 		MsgId id,

Telegram/SourceFiles/history/history_item_reply_markup.cpp
@@ -14,28 +14,6 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 namespace {
 
-[[nodiscard]] InlineBots::PeerTypes PeerTypesFromMTP(
-		const MTPvector<MTPInlineQueryPeerType> &types) {
-	using namespace InlineBots;
-	auto result = PeerTypes(0);
-	for (const auto &type : types.v) {
-		result |= type.match([&](const MTPDinlineQueryPeerTypePM &data) {
-			return PeerType::User;
-		}, [&](const MTPDinlineQueryPeerTypeChat &data) {
-			return PeerType::Group;
-		}, [&](const MTPDinlineQueryPeerTypeMegagroup &data) {
-			return PeerType::Group;
-		}, [&](const MTPDinlineQueryPeerTypeBroadcast &data) {
-			return PeerType::Broadcast;
-		}, [&](const MTPDinlineQueryPeerTypeBotPM &data) {
-			return PeerType::Bot;
-		}, [&](const MTPDinlineQueryPeerTypeSameBotPM &data) {
-			return PeerType();
-		});
-	}
-	return result;
-}
-
 [[nodiscard]] RequestPeerQuery RequestPeerQueryFromTL(
 		const MTPDkeyboardButtonRequestPeer &query) {
 	using Type = RequestPeerQuery::Type;
@@ -75,6 +53,28 @@ namespace {
 }
 
 } // namespace
+
+InlineBots::PeerTypes PeerTypesFromMTP(
+		const MTPvector<MTPInlineQueryPeerType> &types) {
+	using namespace InlineBots;
+	auto result = PeerTypes(0);
+	for (const auto &type : types.v) {
+		result |= type.match([&](const MTPDinlineQueryPeerTypePM &data) {
+			return PeerType::User;
+		}, [&](const MTPDinlineQueryPeerTypeChat &data) {
+			return PeerType::Group;
+		}, [&](const MTPDinlineQueryPeerTypeMegagroup &data) {
+			return PeerType::Group;
+		}, [&](const MTPDinlineQueryPeerTypeBroadcast &data) {
+			return PeerType::Broadcast;
+		}, [&](const MTPDinlineQueryPeerTypeBotPM &data) {
+			return PeerType::Bot;
+		}, [&](const MTPDinlineQueryPeerTypeSameBotPM &data) {
+			return PeerType();
+		});
+	}
+	return result;
+}
 
 HistoryMessageMarkupButton::HistoryMessageMarkupButton(
 	Type type,

commit 10f1ae152d82f79d1d0bfd86ee25a96a841797b0

Fix crash in sending games from inline bots.
Regression was introduced in 2d1fb0562d.

Telegram/SourceFiles/inline_bots/inline_bot_send_data.cpp
@@ -134,7 +134,7 @@ not_null<HistoryItem*> SendGame::makeMessage(
 		const Result *owner,
 		not_null<History*> history,
 		HistoryItemCommonFields &&fields) const {
-	return history->addNewLocalMessage(std::move(fields), _game);
+	return history->makeMessage(std::move(fields), _game);
 }
 
 QString SendGame::getErrorOnSend(