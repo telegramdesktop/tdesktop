1b16a8481092f4b16db3c06ef28a4845950bdbf7
John Preston <johnprestonmail@gmail.com>
2025-01-24 17:27:45 +0400

    Return common groups section to bots.
    Fixes #28886.

--- Telegram/SourceFiles/info/profile/info_profile_inner_widget.cpp
+++ Telegram/SourceFiles/info/profile/info_profile_inner_widget.cpp
@@ -247,6 +247,7 @@ object_ptr<Ui::RpWidget> InnerWidget::setupSharedMedia(
 	addMediaButton(MediaType::RoundVoiceFile, st::infoIconMediaVoice);
 	addMediaButton(MediaType::GIF, st::infoIconMediaGif);
 	if (const auto bot = _peer->asBot()) {
+		addCommonGroupsButton(bot, st::infoIconMediaGroup);
 		addSimilarPeersButton(bot, st::infoIconMediaBot);
 	} else if (const auto channel = _peer->asBroadcast()) {
 		addSimilarPeersButton(channel, st::infoIconMediaChannel);
