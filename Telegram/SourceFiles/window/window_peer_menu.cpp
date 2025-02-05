c82fbefcfcc5bc5e4a78fdc37726fcd6a5adcdd9
John Preston <johnprestonmail@gmail.com>
2025-02-05 18:12:34 +0400

    Don't show Send a Gift to service users.

--- Telegram/SourceFiles/window/window_peer_menu.cpp
+++ Telegram/SourceFiles/window/window_peer_menu.cpp
@@ -1236,6 +1236,7 @@ void Filler::addSendGift() {
 		&& (user->isInaccessible()
 			|| user->isSelf()
 			|| user->isBot()
+			|| user->isServiceUser()
 			|| user->isNotificationsUser()
 			|| user->isRepliesChat()
 			|| user->isVerifyCodes()
