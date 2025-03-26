6a2ad4c4b6655710914987be60b1cfc41acc8280
Ilya Fedin <fedin-ilja2010@ya.ru>
2025-03-26 15:44:25 +0000

    Update Qt 6.8.2 -> 6.9.0

--- Telegram/build/qt_version.py
+++ Telegram/build/qt_version.py
@@ -5,7 +5,7 @@ def resolve(arch):
         os.environ['QT'] = '6.2.12'
     elif sys.platform == 'win32':
         if arch == 'arm' or 'qt6' in sys.argv:
-            os.environ['QT'] = '6.8.2'
+            os.environ['QT'] = '6.9.0'
         elif os.environ.get('QT') is None:
             os.environ['QT'] = '5.15.15'
     elif os.environ.get('QT') is None:
