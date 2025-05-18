b3c198f0d270d20955d8e8d38e27467b5cd99585
John Preston <johnprestonmail@gmail.com>
2025-05-18 11:08:19 +0400

    Improve layout of media albums with likes.

--- Telegram/SourceFiles/history/view/history_view_message.cpp
+++ Telegram/SourceFiles/history/view/history_view_message.cpp
@@ -1192,6 +1192,9 @@ void Message::draw(Painter &p, const PaintContext &context) const {
 		}
 		if (!mediaOnBottom && (!_viewButton || !reactionsInBubble)) {
 			localMediaBottom -= st::msgPadding.bottom();
+			if (mediaDisplayed) {
+				localMediaBottom -= st::mediaInBubbleSkip;
+			}
 		}
 		if (check) {
 			localMediaBottom -= check->height();
--- Telegram/SourceFiles/history/view/media/history_view_media_grouped.cpp
+++ Telegram/SourceFiles/history/view/media/history_view_media_grouped.cpp
@@ -172,7 +172,9 @@ QSize GroupedMedia::countOptimalSize() {
 		_parts[i].sides = item.sides;
 	}
 
-	if (_mode == Mode::Column && _parts.back().item->emptyText()) {
+	if (_mode == Mode::Column
+		&& isBubbleBottom()
+		&& _parts.back().item->emptyText()) {
 		const auto item = _parent->data();
 		const auto msgsigned = item->Get<HistoryMessageSigned>();
 		const auto views = item->Get<HistoryMessageViews>();
@@ -236,7 +238,9 @@ QSize GroupedMedia::countCurrentSize(int newWidth) {
 			accumulate_max(newHeight, top + height);
 		}
 	}
-	if (_mode == Mode::Column && _parts.back().item->emptyText()) {
+	if (_mode == Mode::Column
+		&& isBubbleBottom()
+		&& _parts.back().item->emptyText()) {
 		const auto item = _parent->data();
 		const auto msgsigned = item->Get<HistoryMessageSigned>();
 		const auto views = item->Get<HistoryMessageViews>();
