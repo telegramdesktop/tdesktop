08681ac1b999cb8bf99a8e84a02e2ca202c37c1c
John Preston <johnprestonmail@gmail.com>
2025-06-05 13:39:33 +0400

    Show join requests in new forums layout.

--- Telegram/SourceFiles/history/view/history_view_requests_bar.cpp
+++ Telegram/SourceFiles/history/view/history_view_requests_bar.cpp
@@ -109,7 +109,9 @@ rpl::producer<Ui::RequestsBarContent> RequestsBarContentByPeer(
 		auto state = lifetime.make_state<State>(peer);
 
 		const auto pushNext = [=](bool now = false) {
-			if ((!showInForum && peer->isForum())
+			if ((!showInForum
+				&& peer->isForum()
+				&& !peer->asChannel()->useSubsectionTabs())
 				|| (std::min(state->current.count, kRecentRequestsLimit)
 					!= state->users.size())) {
 				return;
