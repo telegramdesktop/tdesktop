0c635a05ff8f32eafd0f9620a398def3557479a1
Ilya Fedin <fedin-ilja2010@ya.ru>
2025-06-06 06:45:43 +0000

    Allow overriding jobs count in Dockerfile

--- Telegram/build/docker/centos_env/gen_dockerfile.py
+++ Telegram/build/docker/centos_env/gen_dockerfile.py
@@ -4,12 +4,15 @@ from os.path import dirname
 from jinja2 import Environment, FileSystemLoader
 
 def checkEnv(envName, defaultValue):
-    return bool(len(environ[envName])) if envName in environ else defaultValue
+    if isinstance(defaultValue, bool):
+        return bool(len(environ[envName])) if envName in environ else defaultValue
+    return environ[envName] if envName in environ else defaultValue
 
 def main():
     print(Environment(loader=FileSystemLoader(dirname(__file__))).get_template("Dockerfile").render(
         DEBUG=checkEnv("DEBUG", True),
         LTO=checkEnv("LTO", True),
+        JOBS=checkEnv("JOBS", ""),
     ))
 
 if __name__ == '__main__':
