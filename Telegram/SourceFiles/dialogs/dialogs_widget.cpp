ca544dbbaeb5f9f45c3f04008ffa0eb595c0b1cb
John Preston <johnprestonmail@gmail.com>
2025-07-25 21:14:05 +0400

    Fix glitch in chats search without animations.

--- Telegram/SourceFiles/dialogs/dialogs_widget.cpp
+++ Telegram/SourceFiles/dialogs/dialogs_widget.cpp
@@ -1748,7 +1748,7 @@ void Widget::updateSuggestions(anim::type animated) {
 			_suggestions = nullptr;
 			_hidingSuggestions.clear();
 			storiesExplicitCollapse();
-			updateStoriesVisibility();
+			updateControlsVisibility();
 			_scroll->show();
 		}
 	} else if (suggest && !_suggestions) {
