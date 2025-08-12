831c84c9f5e64f9d0ce6937ce04839219ef87215
23rd <23rd@vivaldi.net>
2025-08-12 18:26:14 +0300

    Temporary fixed build.

--- Telegram/SourceFiles/api/api_credits_history_entry.cpp
+++ Telegram/SourceFiles/api/api_credits_history_entry.cpp
@@ -153,7 +153,7 @@ Data::CreditsHistoryEntry CreditsHistoryEntryFromTL(
 		.floodSkip = int(tl.data().vfloodskip_number().value_or(0)),
 		.converted = stargift && incoming,
 		.stargift = stargift.has_value(),
-		.postsSearch = tl.data().is_posts_search(),
+		// .postsSearch = tl.data().is_posts_search(),
 		.giftUpgraded = tl.data().is_stargift_upgrade(),
 		.giftResale = tl.data().is_stargift_resale(),
 		.reaction = tl.data().is_reaction(),
