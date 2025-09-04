2164d08c3cd5ca1317dd28dcf86c37928ae5e238
John Preston <johnprestonmail@gmail.com>
2025-09-04 11:28:21 +0400

    Fixed a crash in gifts section closing.
    Fixes #29737.

--- Telegram/SourceFiles/info/info_wrap_widget.cpp
+++ Telegram/SourceFiles/info/info_wrap_widget.cpp
@@ -291,6 +291,10 @@ Dialogs::RowDescriptor WrapWidget::activeChat() const {
 			: Dialogs::RowDescriptor(
 				storiesPeer->owner().history(storiesPeer),
 				FullMsgId());
+	} else if (const auto giftsPeer = key().giftsPeer()) {
+		return Dialogs::RowDescriptor(
+			giftsPeer->owner().history(giftsPeer),
+			FullMsgId());
 	} else if (const auto musicPeer = key().musicPeer()) {
 		return Dialogs::RowDescriptor(
 			musicPeer->owner().history(musicPeer),
