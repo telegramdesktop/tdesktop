c024c830bca23b77f846d373ad582599bc7f3da8
Ilya Fedin <fedin-ilja2010@ya.ru>
2025-10-02 12:50:59 +0400

    Revert "Add error message for webview without OpenGL"
    This reverts commit 20fb73b626bd7628e307969bf87b15298151c2b4.

--- Telegram/SourceFiles/ui/chat/attach/attach_bot_webview.cpp
+++ Telegram/SourceFiles/ui/chat/attach/attach_bot_webview.cpp
@@ -2125,8 +2125,6 @@ TextWithEntities ErrorText(const Webview::Available &info) {
 			Ui::Text::WithEntities);
 	case Error::NoWebKitGTK:
 		return { tr::lng_payments_webview_install_webkit(tr::now) };
-	case Error::NoOpenGL:
-		return { tr::lng_payments_webview_enable_opengl(tr::now) };
 	case Error::OldWindows:
 		return { tr::lng_payments_webview_update_windows(tr::now) };
 	default:
