2276b934d4a8fa01c37e12d5ccd6ad4deaf38e99
InternalProgramError <47084951+InternalProgramError@users.noreply.github.com>
2025-10-17 17:55:10 +0300

    Update menu_send.cpp
    Unification of Send menu (https://bugs.telegram.org/c/55220)

--- Telegram/SourceFiles/menu/menu_send.cpp
+++ Telegram/SourceFiles/menu/menu_send.cpp
@@ -690,11 +690,6 @@ FillMenuResult FillSendMenu(
 		? *iconsOverride
 		: st::defaultComposeIcons;
 
-	if (sending && type != Type::Reminder) {
-		menu->addAction(
-			tr::lng_send_silent_message(tr::now),
-			[=] { action({ Api::SendOptions{ .silent = true } }, details); },
-			&icons.menuMute);
 	}
 	if (sending && type != Type::SilentOnly) {
 		menu->addAction(
@@ -712,6 +707,11 @@ FillMenuResult FillSendMenu(
 				details); },
 			&icons.menuWhenOnline);
 	}
+	if (sending && type != Type::Reminder) {
+		menu->addAction(
+			tr::lng_send_silent_message(tr::now),
+			[=] { action({ Api::SendOptions{ .silent = true } }, details); },
+			&icons.menuMute);
 
 	if ((type != Type::Disabled)
 		&& ((details.spoiler != SpoilerState::None)
