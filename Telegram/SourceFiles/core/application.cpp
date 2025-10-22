92066c63357fa5807c465f5f3d6d40cd2e3f0adb
Reza Bakhshi Laktasaraei <74649066+rezabakhshilaktasaraei@users.noreply.github.com>
2025-10-22 12:50:12 +0330

    adding the entry poin for enabling accessibility

--- Telegram/SourceFiles/core/application.cpp
+++ Telegram/SourceFiles/core/application.cpp
@@ -85,6 +85,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "base/qthelp_regex.h"
 #include "base/qthelp_url.h"
 #include "boxes/premium_limits_box.h"
+#include "ui/accessibility.h"
 #include "ui/boxes/confirm_box.h"
 #include "ui/controls/location_picker.h"
 #include "styles/style_window.h"
@@ -260,6 +261,8 @@ void Application::run() {
 	_notifications = std::make_unique<Window::Notifications::System>();
 
 	startLocalStorage();
+
+	Ui::InstallAccessibleFactory();
 
 	style::SetCustomFont(settings().customFontFamily());
 	style::internal::StartFonts();
