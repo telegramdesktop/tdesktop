bf0612f308552f43f749d3341557de708ce98180
mukthar777 <musthafalmukthar7@gmail.com>
2026-02-04 22:35:45 +0530

    Add screen reader support for silent toggle button as checkbox.

--- Telegram/SourceFiles/ui/controls/silent_toggle.cpp
+++ Telegram/SourceFiles/ui/controls/silent_toggle.cpp
@@ -28,6 +28,7 @@ SilentToggle::SilentToggle(QWidget *parent, not_null<ChannelData*> channel)
 	resize(_st.width, _st.height);
 
 	setMouseTracking(true);
+	setAccessibleName(tr::lng_broadcast_silent(tr::now));
 
 	clicks(
 	) | rpl::on_next([=] {
@@ -65,6 +66,7 @@ void SilentToggle::setChecked(bool checked) {
 	if (_checked != checked) {
 		_checked = checked;
 		update();
+        accessibilityStateChanged({ .checked = true });
 		// _crossLineAnimation.start(
 		// 	[=] { update(); },
 		// 	_checked ? 0. : 1.,
@@ -104,6 +106,14 @@ QPoint SilentToggle::prepareRippleStartPosition() const {
 QImage SilentToggle::prepareRippleMask() const {
 	return RippleAnimation::EllipseMask(
 		QSize(_st.rippleAreaSize, _st.rippleAreaSize));
+}
+
+QAccessible::Role SilentToggle::accessibilityRole() {
+    return QAccessible::Role::CheckBox;
+}
+
+Ui::AccessibilityState SilentToggle::accessibilityState() const {
+	return { .checkable = true, .checked = _checked };
 }
 
 } // namespace Ui
