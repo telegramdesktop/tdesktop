1aaa1712d35df7a8f8dae0be4dadabf63c0de22c
Kosta <makostadev@xyecoc.com>
2026-06-05 16:21:56 +0400

    Fix: ALT+mouse click opens context menu

--- Telegram/SourceFiles/dialogs/dialogs_inner_widget.cpp
+++ Telegram/SourceFiles/dialogs/dialogs_inner_widget.cpp
@@ -3328,6 +3328,9 @@ void InnerWidget::contextMenuEvent(QContextMenuEvent *e) {
 	const auto fromMouse = e->reason() == QContextMenuEvent::Mouse;
 
 	if (fromMouse) {
+		if (e->modifiers() & Qt::AltModifier) {
+			return;
+		}
 		selectByMouse(e->globalPos());
 	}
 
