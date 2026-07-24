d5dddb1068a7d191042cdb9277b6830c15b02208
23rd <23rd@vivaldi.net>
2026-07-24 09:46:49 +0300

    Added muting forever by left click on profile button.

--- Telegram/SourceFiles/info/profile/info_profile_top_bar.cpp
+++ Telegram/SourceFiles/info/profile/info_profile_top_bar.cpp
@@ -43,8 +43,6 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "data/data_star_gift.h"
 #include "data/data_stories.h"
 #include "data/data_user.h"
-#include "data/notify/data_notify_settings.h"
-#include "data/notify/data_peer_notify_settings.h"
 #include "data/stickers/data_custom_emoji.h"
 #include "editor/photo_editor_common.h"
 #include "editor/photo_editor_layer_widget.h"
@@ -983,38 +981,26 @@ void TopBar::setupActions(not_null<Window::SessionController*> controller) {
 		notifications->finishAnimating();
 
 		notifications->setAcceptBoth();
-		const auto notifySettings = &peer->owner().notifySettings();
-			MuteMenu::SetupMuteMenu(
-				notifications,
-				notifications->clicks(
-				) | rpl::filter([=](Qt::MouseButton button) {
-					if (button == Qt::RightButton) {
-						return true;
-					}
-					const auto topic = topicRootId
-						? peer->forumTopicFor(topicRootId)
-						: nullptr;
-					Assert(!topicRootId || topic != nullptr);
-					const auto is = topic
-						? notifySettings->isMuted(topic)
-						: notifySettings->isMuted(peer);
-					if (is) {
-						if (topic) {
-							notifySettings->update(topic, { .unmute = true });
-						} else {
-							notifySettings->update(peer, { .unmute = true });
-						}
-						return false;
-					} else {
-						return true;
-					}
-				}) | rpl::to_empty,
-				makeThread,
-				controller->uiShow(),
-				[=, skip = st::infoProfileTopBarActionMenuSkip] {
-					return notifications->mapToGlobal(
-						QPoint(0, notifications->height() + skip));
-				});
+		notifications->clicks(
+		) | rpl::filter([](Qt::MouseButton button) {
+			return (button == Qt::LeftButton);
+		}) | rpl::on_next([=] {
+			if (const auto thread = makeThread()) {
+				MuteMenu::ToggleMuteForever(thread);
+			}
+		}, notifications->lifetime());
+		MuteMenu::SetupMuteMenu(
+			notifications,
+			notifications->clicks(
+			) | rpl::filter([](Qt::MouseButton button) {
+				return (button == Qt::RightButton);
+			}) | rpl::to_empty,
+			makeThread,
+			controller->uiShow(),
+			[=, skip = st::infoProfileTopBarActionMenuSkip] {
+				return notifications->mapToGlobal(
+					QPoint(0, notifications->height() + skip));
+			});
 		buttons.push_back(notifications);
 		_actions->add(notifications);
 		_edgeColor.value() | rpl::on_next([=](
