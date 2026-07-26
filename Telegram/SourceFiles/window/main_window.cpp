4d5d40de8db0022446c47742976fe25975dbb168
23rd <23rd@vivaldi.net>
2026-07-26 20:22:00 +0300

    Disabled Touch Bar by default on Apple Silicon Macs.

--- Telegram/SourceFiles/window/main_window.cpp
+++ Telegram/SourceFiles/window/main_window.cpp
@@ -85,6 +85,9 @@ base::options::toggle OptionNewWindowsSizeAsFirst({
 base::options::toggle OptionDisableTouchbar({
 	.id = kOptionDisableTouchbar,
 	.name = "Disable Touch Bar (macOS only).",
+#if defined Q_OS_MAC && defined Q_PROCESSOR_ARM
+	.defaultValue = true,
+#endif // Q_OS_MAC && Q_PROCESSOR_ARM
 	.scope = [] {
 #ifdef Q_OS_MAC
 		return true;
