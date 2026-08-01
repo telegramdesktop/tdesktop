b3c68dce3c268ce0bd4bc3130b093a4eb8e9ff30
23rd <23rd@vivaldi.net>
2026-08-01 21:39:11 +0300

    Added experimental option for Vulkan rendering on Linux.

--- Telegram/SourceFiles/settings/settings_experimental.cpp
+++ Telegram/SourceFiles/settings/settings_experimental.cpp
@@ -445,6 +445,7 @@ void SetupExperimental(
 				Core::kOptionFractionalScalingEnabled,
 				Core::kOptionHighDpiDownscale,
 				Ui::GL::kOptionUseQtRhi,
+				Ui::GL::kOptionEnableVulkanRhi,
 				Core::kOptionFreeType,
 				Ui::kOptionQScroller,
 				Window::kOptionDisableTouchbar,
