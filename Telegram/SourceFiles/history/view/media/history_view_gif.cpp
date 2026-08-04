97fe436234b7555987d2ac1d69029a7f350a988b
John Preston <johnprestonmail@gmail.com>
2026-08-04 22:17:14 +0400

    Preserve video progress clipping in slideshows
    Task: 2026/08/04/clip-slideshow-video-progress-during-transitions

--- Telegram/SourceFiles/history/view/media/history_view_gif.cpp
+++ Telegram/SourceFiles/history/view/media/history_view_gif.cpp
@@ -1081,7 +1081,13 @@ void Gif::paintTimestampMark(
 	if (edge > 0) {
 		p.setBrush(st::windowBgActive);
 
-		p.setClipRect(rthumb.x(), top, edge, line);
+		p.save();
+		p.setClipRect(
+			rthumb.x(),
+			top,
+			edge,
+			line,
+			Qt::IntersectClip);
 		p.drawRoundedRect(
 			rthumb.x(),
 			top - 2 * radiusl,
@@ -1089,11 +1095,13 @@ void Gif::paintTimestampMark(
 			line + 2 * radiusl,
 			radiusl,
 			radiusl);
+		p.restore();
 	}
 	if (const auto width = rthumb.width() - edge; width > 0) {
 		const auto left = rthumb.x() + edge;
 		p.setBrush(st::mediaviewPlaybackProgressFg);
-		p.setClipRect(left, top, width, line);
+		p.save();
+		p.setClipRect(left, top, width, line, Qt::IntersectClip);
 		p.drawRoundedRect(
 			left - radiusr,
 			top - 2 * radiusr,
@@ -1101,6 +1109,7 @@ void Gif::paintTimestampMark(
 			line + 2 * radiusr,
 			radiusr,
 			radiusr);
+		p.restore();
 	}
 	p.restore();
 }
