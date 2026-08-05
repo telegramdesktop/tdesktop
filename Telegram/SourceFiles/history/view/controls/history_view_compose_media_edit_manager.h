943501c84c795871e9bbbb288f3bb03d689fdf5d
23rd <23rd@vivaldi.net>
2026-08-05 21:55:31 +0300

    Added video cover editing to media edit manager.

--- Telegram/SourceFiles/history/view/controls/history_view_compose_media_edit_manager.h
+++ Telegram/SourceFiles/history/view/controls/history_view_compose_media_edit_manager.h
@@ -7,7 +7,13 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #pragma once
 
+#include "api/api_common.h"
 #include "base/unique_qptr.h"
+#include "history/view/controls/history_view_video_cover_uploader.h"
+
+namespace ChatHelpers {
+class Show;
+} // namespace ChatHelpers
 
 namespace SendMenu {
 struct Details;
@@ -21,6 +27,7 @@ class PopupMenu;
 
 class Image;
 class HistoryItem;
+class Painter;
 
 namespace HistoryView {
 
@@ -32,18 +39,24 @@ public:
 		not_null<HistoryItem*> item,
 		std::optional<bool> spoilered = {},
 		std::optional<bool> invertCaption = {});
-	void apply(SendMenu::Action action);
+	void apply(
+		SendMenu::Action action,
+		std::shared_ptr<ChatHelpers::Show> show = nullptr);
 	void cancel();
 
 	void showMenu(
 		not_null<Ui::RpWidget*> parent,
 		Fn<void()> finished,
-		bool hasCaptionText);
+		bool hasCaptionText,
+		std::shared_ptr<ChatHelpers::Show> show = nullptr);
 
 	[[nodiscard]] Image *mediaPreview();
+	void paintCoverUpload(Painter &p, QRect to);
 
 	[[nodiscard]] bool spoilered() const;
 	[[nodiscard]] bool invertCaption() const;
+	[[nodiscard]] Api::VideoCoverEdit videoCover();
+	[[nodiscard]] bool videoCoverUploading() const;
 
 	[[nodiscard]] SendMenu::Details sendMenuDetails(
 		bool hasCaptionText) const;
@@ -55,12 +68,21 @@ public:
 	}
 
 	[[nodiscard]] static bool CanBeSpoilered(not_null<HistoryItem*> item);
+	[[nodiscard]] static bool CanEditVideoCover(not_null<HistoryItem*> item);
 
 private:
+	void editVideoCover(std::shared_ptr<ChatHelpers::Show> show);
+	void removeVideoCover();
+	void resetCover();
+
 	base::unique_qptr<Ui::PopupMenu> _menu;
 	HistoryItem *_item = nullptr;
 	bool _spoilered = false;
 	bool _invertCaption = false;
+	bool _coverCleared = false;
+
+	VideoCoverUploader _coverUploader;
+	rpl::lifetime _coverClearedLifetime;
 
 	rpl::event_stream<> _updateRequests;
 
