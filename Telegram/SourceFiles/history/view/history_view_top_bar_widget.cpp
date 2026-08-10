49e7578f03895138125ceb4ceaadc75ea1ed7c47
23rd <23rd@vivaldi.net>
2026-08-10 10:55:47 +0300

    Fixed top bar layout of instantly shown chat sections.

--- Telegram/SourceFiles/history/view/history_view_top_bar_widget.cpp
+++ Telegram/SourceFiles/history/view/history_view_top_bar_widget.cpp
@@ -1280,6 +1280,8 @@ void TopBarWidget::setAnimatingMode(bool enabled) {
 	if (_animatingMode != enabled) {
 		_animatingMode = enabled;
 		setAttribute(Qt::WA_OpaquePaintEvent, !_animatingMode);
+	}
+	if (!_animatingMode) {
 		finishAnimating();
 	} else if (!enabled) {
 		finishAnimating();
