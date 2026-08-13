09650004fd443c504400b5d5749ec457f62d33e6
23rd <23rd@vivaldi.net>
2026-08-13 13:48:57 +0300

    [iv-editor] Moved tag algebra and field chunking to text entities.

--- Telegram/SourceFiles/iv/editor/iv_editor_text_entities.h
+++ Telegram/SourceFiles/iv/editor/iv_editor_text_entities.h
@@ -13,6 +13,32 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 namespace Iv::Editor {
 
+struct TextRange {
+	int offset = 0;
+	int length = 0;
+};
+
+inline constexpr auto kMaxRichTextNodeLength = 16000;
+inline constexpr auto kMaxCommittedFieldLength = 256 * 1024;
+
+[[nodiscard]] bool RangeInsideText(
+	const QString &text,
+	int offset,
+	int length);
+[[nodiscard]] bool TagContains(QStringView tags, QStringView tagId);
+[[nodiscard]] bool HasFullTextTag(
+	const TextWithTags &textWithTags,
+	const QString &tag);
+[[nodiscard]] std::vector<TextWithEntities> SplitFieldText(
+	TextWithEntities text);
+[[nodiscard]] bool SplitTextSpan(
+	const TextWithEntities &text,
+	int from,
+	int till,
+	TextWithEntities *before,
+	TextWithEntities *selected,
+	TextWithEntities *after);
+
 struct RichTextEditorOffsetReplacement {
 	int richOffset = 0;
 	int richLength = 0;
