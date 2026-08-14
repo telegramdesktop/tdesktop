9db910ea2ea7f6753ca3fa803930bf22bdced9c8
23rd <23rd@vivaldi.net>
2026-08-14 11:59:22 +0300

    [iv-editor] Added mutual exclusion for subscript and superscript.

--- Telegram/SourceFiles/iv/editor/iv_editor_state.cpp
+++ Telegram/SourceFiles/iv/editor/iv_editor_state.cpp
@@ -150,14 +150,29 @@ void ExpandInsertContextToActiveLine(State::ActiveTextInsertContext &context) {
 		Ui::InputField::kTagIvMath);
 }
 
-[[nodiscard]] QString TagWithAddedDroppingMath(
+[[nodiscard]] QString TagWithoutOppositeScript(
+		const QString &tag,
+		const QString &added) {
+	if (added == Ui::InputField::kTagIvSubscript) {
+		return TextUtilities::TagWithRemoved(
+			tag,
+			Ui::InputField::kTagIvSuperscript);
+	} else if (added == Ui::InputField::kTagIvSuperscript) {
+		return TextUtilities::TagWithRemoved(
+			tag,
+			Ui::InputField::kTagIvSubscript);
+	}
+	return tag;
+}
+
+[[nodiscard]] QString TagWithAddedDroppingConflicts(
 		const QString &tag,
 		const QString &added) {
 	if (added == Ui::InputField::kTagIvMath) {
 		return Ui::InputField::kTagIvMath;
 	}
 	return TextUtilities::TagWithAdded(
-		TagWithoutInstantViewMath(tag),
+		TagWithoutOppositeScript(TagWithoutInstantViewMath(tag), added),
 		added);
 }
 
@@ -226,7 +241,7 @@ void OverlayTag(
 			result.push_back({
 				.offset = middleFrom,
 				.length = middleTill - middleFrom,
-				.id = TagWithAddedDroppingMath(tag.id, overlay.id),
+				.id = TagWithAddedDroppingConflicts(tag.id, overlay.id),
 			});
 			coveredTill = middleTill;
 		}
