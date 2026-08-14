c1a34401bb68cc5527ad073ab6373dad20597bcd
23rd <23rd@vivaldi.net>
2026-08-14 11:15:26 +0300

    [iv-editor] Fixed last line skip for vertical caret move in field.

--- Telegram/SourceFiles/iv/editor/iv_editor_widget.cpp
+++ Telegram/SourceFiles/iv/editor/iv_editor_widget.cpp
@@ -7297,8 +7297,8 @@ bool Widget::fieldCursorLeavesVisibleRow(bool down) const {
 	const auto nextRect = raw->cursorRect(next);
 	return !nextRect.isValid()
 		|| nextRect.isEmpty()
-		|| (nextRect.top() < viewport.top())
-		|| (nextRect.bottom() > viewport.bottom());
+		|| (nextRect.bottom() < viewport.top())
+		|| (nextRect.top() > viewport.bottom());
 }
 
 int Widget::textEditableSegmentIndex(int ordinal) const {
