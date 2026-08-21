9bed91543d0715243001cb1b5494a6d8c2a46dd7
John Preston <johnprestonmail@gmail.com>
2026-08-21 12:39:18 +0400

    Tear down Layer Info instantly when its sublist dies
    Task: 2026/08/21/resubscribe-info-wrap-teardown-on-replaced-sublist

--- Telegram/SourceFiles/info/info_wrap_widget.cpp
+++ Telegram/SourceFiles/info/info_wrap_widget.cpp
@@ -169,15 +169,19 @@ void WrapWidget::subscribeToThreadDestroyed() {
 		sublist->destroyed(
 		) | rpl::on_next([=] {
 			const auto parent = _controller->parentController();
-			parent->hideSpecialLayer();
-			parent->showBackFromStack(
-				Window::SectionShow(
-					anim::type::instant,
-					anim::activation::background));
-			parent->clearSectionStack(Window::SectionShow(
-				Window::SectionShow::Way::ClearStack,
-				anim::type::instant));
-			_removeRequests.fire({});
+			if (_wrap.current() == Wrap::Layer) {
+				parent->hideSpecialLayer(anim::type::instant);
+			} else if (_wrap.current() == Wrap::Narrow) {
+				parent->showBackFromStack(
+					Window::SectionShow(
+						anim::type::instant,
+						anim::activation::background));
+				parent->clearSectionStack(Window::SectionShow(
+					Window::SectionShow::Way::ClearStack,
+					anim::type::instant));
+			} else {
+				_removeRequests.fire({});
+			}
 		}, _threadDestroyedLifetime);
 	}
 }
