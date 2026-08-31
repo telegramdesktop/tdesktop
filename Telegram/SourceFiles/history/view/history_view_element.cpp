6f040571e755e22c86c24fe55ff5b9bb7fbd3425
23rd <23rd@vivaldi.net>
2026-08-31 21:37:40 +0300

    Fixed sponsored messages hiding userpic of post above them.
    Regression was introduced in 440ebfcbf6.

--- Telegram/SourceFiles/history/view/history_view_element.cpp
+++ Telegram/SourceFiles/history/view/history_view_element.cpp
@@ -2266,6 +2266,7 @@ bool Element::computeIsAttachToPrevious(not_null<Element*> previous) {
 		const auto item = view->data();
 		return !item->isService()
 			&& !item->isEmpty()
+			&& !item->isSponsored()
 			&& !item->isPostHidingAuthor()
 			&& !item->isGuestChatBotMessage()
 			&& (!item->history()->peer->isMegagroup()
