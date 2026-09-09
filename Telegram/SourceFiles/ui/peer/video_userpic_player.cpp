f8811b8aff23c4aaa10daaf74f5ab536a026d0c5
23rd <23rd@vivaldi.net>
2026-09-09 17:31:13 +0300

    Fixed video userpic of profile top bar playing in inactive window.

--- Telegram/SourceFiles/ui/peer/video_userpic_player.cpp
+++ Telegram/SourceFiles/ui/peer/video_userpic_player.cpp
@@ -46,7 +46,10 @@ void VideoUserpicPlayer::clear() {
 	_peer = nullptr;
 }
 
-QImage VideoUserpicPlayer::frame(QSize size, not_null<PeerData*> peer) {
+QImage VideoUserpicPlayer::frame(
+		QSize size,
+		not_null<PeerData*> peer,
+		bool paused) {
 	if (!_streamed
 		|| !_streamed->player().ready()
 		|| _streamed->player().videoSize().isEmpty()
@@ -90,7 +93,9 @@ QImage VideoUserpicPlayer::frame(QSize size, not_null<PeerData*> peer) {
 			_monoforumMask);
 		q.end();
 	}
-	_streamed->markFrameShown();
+	if (!paused) {
+		_streamed->markFrameShown();
+	}
 	return result;
 }
 
