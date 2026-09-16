8316d84d722d3833507a88424b8a258e79811f5d
John Preston <johnprestonmail@gmail.com>
2026-09-16 22:09:16 +0400

    Update tlottie to latest revision.

--- Telegram/build/prepare/prepare.py
+++ Telegram/build/prepare/prepare.py
@@ -1928,7 +1928,7 @@ release:
 stage('tlottie', """
     git clone https://github.com/dkaraush/tlottie.git
     cd tlottie
-    git checkout 4b940c7942
+    git checkout 31f1b542f8
 win:
     SET "RUSTUP_HOME=%THIRDPARTY_DIR%\\rust\\rustup"
     SET "CARGO_HOME=%THIRDPARTY_DIR%\\rust\\cargo"
