telegramdesktop/tdesktop

commit 13d47ae7817a262e321db02c1ea1ae4ba159c7f7

Fixed hidden userpic misaligning sponsored messages.
Regression was introduced in 440ebfcbf6.

Telegram/SourceFiles/history/view/history_view_message.cpp
@@ -3308,7 +3308,9 @@ PointState Message::pointState(QPoint point) const {
 }
 
 bool Message::displayFromPhoto() const {
-	return hasFromPhoto() && !isAttachedToNext();
+	return hasFromPhoto()
+		&& !isAttachedToNext()
+		&& !data()->isSponsored();
 }
 
 void Message::clickHandlerPressedChanged(
@@ -3841,9 +3843,7 @@ bool Message::hasFromPhoto() const {
 	case Context::SavedSublist:
 	case Context::ScheduledTopic: {
 		const auto item = data();
-		if (item->isSponsored()) {
-			return false;
-		} else if (item->isPostHidingAuthor()) {
+		if (item->isPostHidingAuthor()) {
 			return false;
 		} else if (item->isPost()) {
 			return true;

Telegram/SourceFiles/history/admin_log/history_admin_log_inner.cpp
@@ -186,7 +186,10 @@ void InnerWidget::enumerateUserpics(Method method) {
 
 		// Call method on a userpic for all messages that have it and for those who are not showing it
 		// because of their attachment to the next message if they are bottom-most visible.
-		if (view->displayFromPhoto() || (view->hasFromPhoto() && itembottom >= _visibleBottom)) {
+		if (view->displayFromPhoto()
+			|| (view->hasFromPhoto()
+				&& view->isAttachedToNext()
+				&& itembottom >= _visibleBottom)) {
 			if (lowestAttachedItemTop < 0) {
 				lowestAttachedItemTop = itemtop + view->marginTop();
 			}

Telegram/SourceFiles/history/history_inner_widget.cpp
@@ -1073,7 +1073,10 @@ void HistoryInner::enumerateUserpics(Method method) {
 
 		// Call method on a userpic for all messages that have it and for those who are not showing it
 		// because of their attachment to the next message if they are bottom-most visible.
-		if (view->displayFromPhoto() || (view->hasFromPhoto() && itembottom >= _visibleAreaBottom)) {
+		if (view->displayFromPhoto()
+			|| (view->hasFromPhoto()
+				&& view->isAttachedToNext()
+				&& itembottom >= _visibleAreaBottom)) {
 			if (lowestAttachedItemTop < 0) {
 				lowestAttachedItemTop = itemtop + view->marginTop();
 			}

Telegram/SourceFiles/history/view/history_view_list_widget.cpp
@@ -386,7 +386,10 @@ void ListWidget::enumerateUserpics(Method method) {
 
 		// Call method on a userpic for all messages that have it and for those who are not showing it
 		// because of their attachment to the next message if they are bottom-most visible.
-		if (view->displayFromPhoto() || (view->hasFromPhoto() && itembottom >= _visibleBottom)) {
+		if (view->displayFromPhoto()
+			|| (view->hasFromPhoto()
+				&& view->isAttachedToNext()
+				&& itembottom >= _visibleBottom)) {
 			if (lowestAttachedItemTop < 0) {
 				lowestAttachedItemTop = itemtop + view->marginTop();
 			}

commit 74273535ee40046fdc70ce1fe5f0ce7e4a456a79

Fixed left offset of sponsored messages in chats with userpics.
Regression was introduced in 13d47ae781.

Telegram/SourceFiles/history/view/history_view_message.cpp
@@ -3317,9 +3317,7 @@ PointState Message::pointState(QPoint point) const {
 }
 
 bool Message::displayFromPhoto() const {
-	return hasFromPhoto()
-		&& !isAttachedToNext()
-		&& !data()->isSponsored();
+	return hasFromPhoto() && !isAttachedToNext();
 }
 
 void Message::clickHandlerPressedChanged(
@@ -3852,7 +3850,9 @@ bool Message::hasFromPhoto() const {
 	case Context::SavedSublist:
 	case Context::ScheduledTopic: {
 		const auto item = data();
-		if (item->isPostHidingAuthor()) {
+		if (item->isSponsored()) {
+			return false;
+		} else if (item->isPostHidingAuthor()) {
 			return false;
 		} else if (item->isPost()) {
 			return true;